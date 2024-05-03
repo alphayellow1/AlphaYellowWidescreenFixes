117bde83970dc90147376f6abe4bc008b500f8bd
AlphaYellow <diogoccsantos@gmail.com>
2024-05-03 03:25:44 +0100

    renamed:    source/StateofEmergencyFOVFix/StateofEmergencyFOVFix.cpp -> source/StateofEmergencyWSFix/StateofEmergencyWidescreenFix.cpp

--- source/StateofEmergencyWSFix/StateofEmergencyWidescreenFix.cpp
+++ source/StateofEmergencyWSFix/StateofEmergencyWidescreenFix.cpp
@@ -9,12 +9,55 @@ using namespace std;
 
 int main()
 {
-    int choice;
-    cout << "State of Emergency (2003) FOV Fixer v1.1 by AlphaYellow, 2024\n\n----------------\n";
-    float hFOV, desiredWidth, desiredHeight, clippingFix;
+    float hFOV, clippingFix;
+    int16_t currentWidth, currentHeight, desiredWidth, desiredHeight;
+    int choice, fileOpened;
+    bool fileNotFound;
+
+    cout << "State of Emergency (2003) Widescreen Fixer v1.2 by AlphaYellow, 2024\n\n----------------\n";
 
     do
     {
+        fstream file;
+        fileNotFound = false;
+        fileOpened = 0; // Initializes fileOpened to 0
+
+        // Tries to open the file initially
+        file.open("KaosPC.exe", ios::in | ios::out | ios::binary);
+
+        // If the file is not open, sets fileNotFound to true
+        if (!file.is_open())
+        {
+            fileNotFound = true;
+        }
+
+        // Loops until the file is found and opened
+        while (fileNotFound)
+        {
+            cout << "\nFailed to open KaosPC.exe, check if the executable has special permissions allowed that prevent the fixer from opening it (e.g: read-only mode), it's not present in the same directory as the fixer, or if the executable is currently running. Press Enter when all the mentioned problems are solved." << endl;
+            cin.get();
+
+            // Tries to open the file again
+            file.open("KaosPC.exe", ios::in | ios::out | ios::binary);
+
+            if (file.is_open())
+            {
+                if (fileOpened == 0)
+                {
+                    cout << "\nKaosPC.exe opened successfully!" << endl;
+                    fileOpened = 1; // Sets fileOpened to 1 after the file is opened successfully
+                }
+                fileNotFound = false; // Sets fileNotFound to false as the file is found and opened
+            }
+        }
+
+        file.seekg(0x000B77C1);
+        file.read(reinterpret_cast<char *>(&currentWidth), sizeof(currentWidth));
+        file.seekg(0x000B77D1);
+        file.read(reinterpret_cast<char *>(&currentHeight), sizeof(currentHeight));
+
+        cout << "\nYour current resolution is " << currentWidth << "x" << currentHeight << "." << endl;
+
         do
         {
             cout << "\n- Enter the desired width: ";
@@ -54,14 +97,17 @@ int main()
         hFOV = desiredWidth / desiredHeight;
         clippingFix = (4.0f / 3.0f) / (desiredWidth / desiredHeight);
 
-        fstream file("KaosPC.exe", ios::in | ios::out | ios::binary);
+        file.seekp(0x000B77C1);
+        file.write(reinterpret_cast<const char *>(&desiredWidth), sizeof(desiredWidth));
+        file.seekp(0x000B77D1);
+        file.write(reinterpret_cast<const char *>(&desiredHeight), sizeof(desiredHeight));
         file.seekp(0x00086057);
         file.write(reinterpret_cast<const char *>(&hFOV), sizeof(hFOV));
         file.seekp(0x0015F6E0);
         file.write(reinterpret_cast<const char *>(&clippingFix), sizeof(clippingFix));
 
         // Confirmation message
-        cout << "\nSuccessfully changed the field of view." << endl;
+        cout << "\nSuccessfully changed the resolution to " << desiredWidth << "x" << desiredHeight << "." << endl;
 
         // Closes the file
         file.close();
