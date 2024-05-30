8eefbcf28fd1ad1356c3ca1eba3aace1d7b7a8af
AlphaYellow <diogoccsantos@gmail.com>
2024-05-30 16:23:23 +0100

    modified:   source/CrusadersofMightandMagicFOVFix/CoMMFOVFix.cpp 	modified:   source/NinaAgentChroniclesFOVFix/NinaAgentChroniclesFOVFix.cpp 	modified:   source/RequiemAvengingAngelFOVFix/RequiemAvengingAngelFOVFix.cpp

--- source/NinaAgentChroniclesFOVFix/NinaAgentChroniclesFOVFix.cpp
+++ source/NinaAgentChroniclesFOVFix/NinaAgentChroniclesFOVFix.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
-#include <conio.h>
 #include <cstdint>
 #include <cmath>
 #include <limits>
@@ -27,78 +26,123 @@ using namespace std;
 
 int main()
 {
-    double oldWidth = 4.0;
-    double oldHeight = 3.0;
-    double oldAspectRatio = oldWidth / oldHeight;
-    double oldHorizontalFOV = 90.0; // Known horizontal FOV for 4:3 aspect ratio
-    double newHorizontalFOV;
-    double newAspectRatio;
-    double newWidth, newHeight, horizontalFOV;
+    int choice, fileOpened;
+    bool fileNotFound;
+    double oldWidth = 4.0, oldHeight = 3.0, oldAspectRatio = oldWidth / oldHeight, oldHorizontalFOV = 90.0, newHorizontalFOV, newAspectRatio, newWidth, newHeight, horizontalFOV;
     float horizontalFovInRadians;
 
-    cout << "Nina: Agent Chronicles (2002) FOV Fixer by AlphaYellow, 2024\n\n----------------\n\n";
+    cout << "Nina: Agent Chronicles (2002) FOV Fixer v1.3 by AlphaYellow, 2024\n\n----------------\n";
 
     do
     {
-        cout << "\n- Enter the desired width: ";
-        cin >> newWidth;
+        fstream file;
+        fileNotFound = false;
+        fileOpened = 0; // Initializes fileOpened to 0
 
-        if (cin.fail())
+        // Tries to open the file initially
+        file.open("lithtech.exe", ios::in | ios::out | ios::binary);
+
+        // If the file is not open, sets fileNotFound to true
+        if (!file.is_open())
         {
-            cin.clear();                                         // Clears error flags
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignores invalid input
-            newWidth = -1;                                       // Ensures the loop continues
-            cout << "Invalid input. Please enter a numeric value." << endl;
+            fileNotFound = true;
         }
-        else if (newWidth <= 0 || newWidth > 65535)
+
+        // Loops until the file is found and opened
+        while (fileNotFound)
         {
-            cout << "Please enter a positive number for width less than 65536." << endl;
+            cout << "\nFailed to open lithtech.exe, check if the executable has special permissions allowed that prevent the fixer from opening it (e.g: read-only mode), it's not present in the same directory as the fixer, or if the executable is currently running. Press Enter when all the mentioned problems are solved." << endl;
+            cin.get();
+
+            // Tries to open the file again
+            file.open("lithtech.exe", ios::in | ios::out | ios::binary);
+
+            if (file.is_open())
+            {
+                if (fileOpened == 0)
+                {
+                    cout << "\nlithtech.exe opened successfully!" << endl;
+                    fileOpened = 1; // Sets fileOpened to 1 after the file is opened successfully
+                }
+                fileNotFound = false; // Sets fileNotFound to false as the file is found and opened
+            }
         }
-    } while (newWidth <= 0 || newWidth > 65535);
 
-    do
-    {
-        cout << "\n- Enter the desired height: ";
-        cin >> newHeight;
-
-        if (cin.fail())
+        do
         {
-            cin.clear();                                         // Clears error flags
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignores invalid input
-            newHeight = -1;                                      // Ensures the loop continues
-            cout << "Invalid input. Please enter a numeric value." << endl;
-        }
-        else if (newHeight <= 0 || newHeight > 65535)
+            cout << "\n- Enter the desired width: ";
+            cin >> newWidth;
+
+            if (cin.fail())
+            {
+                cin.clear();                                         // Clears error flags
+                cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignores invalid input
+                newWidth = -1;                                       // Ensures the loop continues
+                cout << "Invalid input. Please enter a numeric value." << endl;
+            }
+            else if (newWidth <= 0 || newWidth > 65535)
+            {
+                cout << "Please enter a positive number for width less than 65536." << endl;
+            }
+        } while (newWidth <= 0 || newWidth > 65535);
+
+        do
         {
-            cout << "Please enter a positive number for height less than 65536." << endl;
-        }
-    } while (newHeight <= 0 || newHeight > 65535);
+            cout << "\n- Enter the desired height: ";
+            cin >> newHeight;
 
-    newAspectRatio = newWidth / newHeight;
+            if (cin.fail())
+            {
+                cin.clear();                                         // Clears error flags
+                cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignores invalid input
+                newHeight = -1;                                      // Ensures the loop continues
+                cout << "Invalid input. Please enter a numeric value." << endl;
+            }
+            else if (newHeight <= 0 || newHeight > 65535)
+            {
+                cout << "Please enter a positive number for height less than 65536." << endl;
+            }
+        } while (newHeight <= 0 || newHeight > 65535);
 
-    // Calculates the new horizontal FOV
-    newHorizontalFOV = 2.0 * radToDeg(atan((newAspectRatio / oldAspectRatio) * tan(degToRad(oldHorizontalFOV / 2.0))));
+        newAspectRatio = newWidth / newHeight;
 
-    fstream file("lithtech.exe", ios::in | ios::out | ios::binary);
-    if (!file.is_open())
-    {
-        cout << "Failed to open the file." << endl;
-        return 1;
-    }
+        // Calculates the new horizontal FOV
+        newHorizontalFOV = 2.0 * radToDeg(atan((newAspectRatio / oldAspectRatio) * tan(degToRad(oldHorizontalFOV / 2.0))));
 
-    horizontalFovInRadians = static_cast<float>(newHorizontalFOV * (M_PI / 180.0)); // Converts degrees to radians
+        horizontalFovInRadians = static_cast<float>(newHorizontalFOV * (M_PI / 180.0)); // Converts degrees to radians
 
-    file.seekp(0x000CAF91);
-    file.write(reinterpret_cast<const char *>(&horizontalFovInRadians), sizeof(horizontalFovInRadians));
+        file.seekp(0x000CAF91);
+        file.write(reinterpret_cast<const char *>(&horizontalFovInRadians), sizeof(horizontalFovInRadians));
 
-    // Confirmation message
-    cout << "\nSuccessfully fixed the field view in the executable.\n"
-         << endl;
+        // Confirmation message
+        cout << "\nSuccessfully fixed the field of view."
+             << endl;
 
-    // Closes the file
-    file.close();
+        // Closes the file
+        file.close();
 
-    cout << "Press enter to exit the program...";
+        do
+        {
+            cout << "\n- Do you want to exit the program (1) or try another value (2)?: ";
+            cin >> choice;
+
+            if (cin.fail())
+            {
+                cin.clear();                                         // Clears error flags
+                cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignores invalid input
+                choice = -1;
+                cout << "Invalid input. Please enter 1 to exit the program or 2 to try another value."
+                     << endl;
+            }
+            else if (choice < 1 || choice > 2)
+            {
+                cout << "Please enter a valid number."
+                     << endl;
+            }
+        } while (choice < 1 || choice > 2);
+    } while (choice == 2); // Checks the flag in the loop condition
+
+    cout << "\nPress enter to exit the program...";
     cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clears the input buffer
     cin.get();
 
--- source/RequiemAvengingAngelFOVFix/RequiemAvengingAngelFOVFix.cpp
+++ source/RequiemAvengingAngelFOVFix/RequiemAvengingAngelFOVFix.cpp
@@ -9,69 +9,120 @@ using namespace std;
 
 int main()
 {
-    int language;
-    cout << "Requiem: Avenging Angel FOV Fixer by AlphaYellow, 2024\n\n----------------\n\n";
-    float width, height, result;
-    union
-    {
-        float input;
-        unsigned int output;
-    } data;
+    float width, height, desiredFOV;
+    int choice, fileOpened;
+    bool fileNotFound;
+
+    cout << "Requiem: Avenging Angel (1999) FOV Fixer v1.2 by AlphaYellow, 2024\n\n----------------\n";
 
     do
     {
-        cout << "Enter the desired width: ";
-        cin >> width;
+        fstream file;
+        fileNotFound = false;
+        fileOpened = 0; // Initializes fileOpened to 0
 
-        if (cin.fail())
+        // Tries to open the file initially
+        file.open("D3D.exe", ios::in | ios::out | ios::binary);
+
+        // If the file is not open, sets fileNotFound to true
+        if (!file.is_open())
         {
-            cin.clear();                                                   // Clear error flags
-            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore invalid input
-            width = -1;                                                    // Ensure the loop continues
-            cout << "Invalid input. Please enter a numeric value." << std::endl;
+            fileNotFound = true;
         }
-        else if (width <= 0 || width > 65535)
+
+        // Loops until the file is found and opened
+        while (fileNotFound)
         {
-            cout << "Please enter a positive number for width less than 65536." << std::endl;
+            cout << "\nFailed to open D3D.exe, check if the executable has special permissions allowed that prevent the fixer from opening it (e.g: read-only mode), it's not present in the same directory as the fixer, or if the executable is currently running. Press Enter when all the mentioned problems are solved." << endl;
+            cin.get();
+
+            // Tries to open the file again
+            file.open("D3D.exe", ios::in | ios::out | ios::binary);
+
+            if (file.is_open())
+            {
+                if (fileOpened == 0)
+                {
+                    cout << "\nD3D.exe opened successfully!" << endl;
+                    fileOpened = 1; // Sets fileOpened to 1 after the file is opened successfully
+                }
+                fileNotFound = false; // Sets fileNotFound to false as the file is found and opened
+            }
         }
-    } while (width <= 0 || width > 65535);
 
-    do
-    {
-        cout << "\nEnter the desired height: ";
-        cin >> height;
-
-        if (cin.fail())
+        do
         {
-            cin.clear();                                                   // Clear error flags
-            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore invalid input
-            height = -1;                                                   // Ensure the loop continues
-            cout << "Invalid input. Please enter a numeric value." << endl;
-        }
-        else if (height <= 0 || height > 65535)
+            cout << "\n- Enter the desired width: ";
+            cin >> width;
+
+            if (cin.fail())
+            {
+                cin.clear();                                                   // Clear error flags
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore invalid input
+                width = -1;                                                    // Ensure the loop continues
+                cout << "Invalid input. Please enter a numeric value." << std::endl;
+            }
+            else if (width <= 0 || width > 65535)
+            {
+                cout << "Please enter a positive number for width less than 65536." << std::endl;
+            }
+        } while (width <= 0 || width > 65535);
+
+        do
         {
-            std::cout << "Please enter a positive number for height less than 65536." << endl;
-        }
-    } while (height <= 0 || height > 65535);
+            cout << "\n- Enter the desired height: ";
+            cin >> height;
 
-    fstream file("D3D.exe", ios::in | ios::out | ios::binary);
+            if (cin.fail())
+            {
+                cin.clear();                                                   // Clear error flags
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore invalid input
+                height = -1;                                                   // Ensure the loop continues
+                cout << "Invalid input. Please enter a numeric value." << endl;
+            }
+            else if (height <= 0 || height > 65535)
+            {
+                std::cout << "Please enter a positive number for height less than 65536." << endl;
+            }
+        } while (height <= 0 || height > 65535);
 
-    result = (4.0f / 3.0f) / (width / height);
-    const unsigned char *pf = reinterpret_cast<const unsigned char *>(&result);
-    for (size_t i = 0; i != sizeof(float); ++i)
-    {
-        file.seekp(0x000242E6 + i);
-        file << char(pf[i]);
-        file.seekp(0x0002431D + i);
-        file << char(pf[i]);
-    }
+        desiredFOV = (4.0f / 3.0f) / (width / height);
 
-    // Confirmation message
-    cout << "\nSuccessfully changed field of view in the executable. You can now press Enter to close the program." << endl;
+        file.seekp(0x000242E6); // HFOV
+        file.write(reinterpret_cast<const char *>(&desiredFOV), sizeof(desiredFOV));
+        file.seekp(0x0002431D); // VFOV
+        file.write(reinterpret_cast<const char *>(&desiredFOV), sizeof(desiredFOV));
 
-    // Close the file
-    file.close();
-    getch();
+        // Confirmation message
+        cout << "\nSuccessfully changed the field of view." << endl;
+
+        // Close the file
+        file.close();
+
+        do
+        {
+            cout << "\n- Do you want to exit the program (1) or try another value (2)?: ";
+            cin >> choice;
+
+            if (cin.fail())
+            {
+                cin.clear();                                         // Clears error flags
+                cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignores invalid input
+                choice = -1;
+                cout << "Invalid input. Please enter 1 to exit the program or 2 to try another value."
+                     << endl;
+            }
+            else if (choice < 1 || choice > 2)
+            {
+                cout << "Please enter a valid number."
+                     << endl;
+            }
+        } while (choice < 1 || choice > 2);
+    } while (choice == 2); // Checks the flag in the loop condition
+
+    cout << "\nPress enter to exit the program...";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clears the input buffer
+    cin.get();
 
     return 0;
 }
