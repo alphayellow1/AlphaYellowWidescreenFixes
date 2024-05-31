49227e1f7c166536122e837504ea53712f7d42b2
AlphaYellow <diogoccsantos@gmail.com>
2024-05-31 15:51:10 +0100

    modified:   source/AladdinInNasirasRevengeWSFix/AladdinInNasirasRevengeWidescreenFix.cpp

--- source/AladdinInNasirasRevengeWSFix/AladdinInNasirasRevengeWidescreenFix.cpp
+++ source/AladdinInNasirasRevengeWSFix/AladdinInNasirasRevengeWidescreenFix.cpp
@@ -233,7 +233,7 @@ int main()
             break;
 
         case 2:
-            cout << "\n- Type a custom FOV multiplier value (default for 4:3 aspect ratio is 0,8546222448): " << endl;
+            cout << "\n- Type a custom FOV multiplier value (default for 4:3 aspect ratio is 0,8546222448): ";
             cin >> FOV;
             isFOVKnown = true;
             break;
