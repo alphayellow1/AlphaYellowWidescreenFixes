ef200e84cf362a36896f9a9d59bd7820c8c0e0aa
AlphaYellow <diogoccsantos@gmail.com>
2024-05-31 15:45:53 +0100

    modified:   source/AladdinInNasirasRevengeWSFix/AladdinInNasirasRevengeWidescreenFix.cpp

--- source/AladdinInNasirasRevengeWSFix/AladdinInNasirasRevengeWidescreenFix.cpp
+++ source/AladdinInNasirasRevengeWSFix/AladdinInNasirasRevengeWidescreenFix.cpp
@@ -7,257 +7,268 @@
 
 using namespace std;
 
+// Constants
+const streampos kAspectRatioOffset = 0x000115E9;
+const streampos kFOVOffset = 0x0009C206;
+
+// Variables
+float newAspectRatio, FOV;
+bool fileNotFound, aspectRatioWrittenToFile, isFOVKnown, validKeyPressed;
+int choice, choice2, fileOpened, tempChoice;
+double newCustomResolutionValue, newWidth, newHeight;
+fstream file;
+char ch;
+
+// Function to handle user input in choices
+void HandleUserInput(int &choice)
+{
+    tempChoice = -1;         // Temporary variable to store the input
+    validKeyPressed = false; // Flag to track if a valid key was pressed
+
+    while (true)
+    {
+        ch = _getch(); // Waits for user to press a key
+
+        // Checks if the key is '1' or '2'
+        if ((ch == '1' || ch == '2') && !validKeyPressed)
+        {
+            tempChoice = ch - '0';  // Stores the input temporarily
+            cout << ch;             // Echoes the valid input
+            validKeyPressed = true; // Sets the flag as a valid key has been pressed
+        }
+        else if (ch == '\b' || ch == 127) // Handles backspace or delete keys
+        {
+            if (tempChoice != -1) // Checks if there is something to delete
+            {
+                tempChoice = -1;         // Resets the temporary choice
+                cout << "\b \b";         // Erases the last character from the console
+                validKeyPressed = false; // Resets the flag as the input has been deleted
+            }
+        }
+        else if (ch == '\r') // If 'Enter' is pressed
+        {
+            if (tempChoice != -1) // Checks if a valid input has been made
+            {
+                choice = tempChoice; // Assigns the temporary input to the choice variable
+                cout << endl;        // Moves to a new line
+                break;               // Exits the loop since we have a confirmed input
+            }
+        }
+    }
+}
+
+// Function to handle user input in resolution
+double HandleResolutionInput()
+{
+    do
+    {
+        cin >> newCustomResolutionValue;
+
+        cin.clear();                                         // Clears error flags
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignores invalid input
+
+        if (cin.fail())
+        {
+            cin.clear();                                         // Clears error flags
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignores invalid input
+            cout << "Invalid input. Please enter a numeric value." << endl;
+        }
+        else if (newCustomResolutionValue <= 0 || newCustomResolutionValue >= 65535)
+        {
+            cout << "Please enter a valid number." << endl;
+        }
+    } while (newCustomResolutionValue <= 0 || newCustomResolutionValue > 65535);
+
+    return newCustomResolutionValue;
+}
+
+// Function to open the file
+void OpenFile(fstream &file)
+{
+    fileNotFound = false;
+    fileOpened = 0; // Initializes fileOpened to 0
+
+    file.open("aladdin.exe", ios::in | ios::out | ios::binary);
+
+    // If the file is not open, sets fileNotFound to true
+    if (!file.is_open())
+    {
+        fileNotFound = true;
+    }
+
+    // Loops until the file is found and opened
+    while (fileNotFound)
+    {
+
+        // Tries to open the file again
+        file.open("aladdin.exe", ios::in | ios::out | ios::binary);
+
+        if (!file.is_open())
+        {
+            cout << "\nFailed to open aladdin.exe, check if the executable has special permissions allowed that prevent the fixer from opening it (e.g: read-only mode), it's not present in the same directory as the fixer, or if the executable is currently running. Press Enter when all the mentioned problems are solved." << endl;
+            do
+            {
+                ch = _getch(); // Waits for user to press a key
+            } while (ch != '\r'); // Keeps waiting if the key is not Enter ('\r' is the Enter key in ASCII)
+        }
+        else
+        {
+            cout << "\naladdin.exe opened successfully!" << endl;
+            fileNotFound = false; // Sets fileNotFound to false as the file is found and opened
+        }
+    }
+}
+
 int main()
 {
-    int arWrittenToFile, fovWrittenToFile, choice, fileOpened;
+
     cout << "Aladdin in Nasira's Revenge (2001) Widescreen Fixer v1.3 by AlphaYellow, 2024\n\n----------------\n";
-    float width, height, aspectRatio, FOV;
-    bool fileNotFound;
 
     do
     {
-        fstream file;
-        fileNotFound = false;
-        fileOpened = 0; // Initializes fileOpened to 0
+        OpenFile(file);
 
-        // Tries to open the file initially
-        file.open("aladdin.exe", ios::in | ios::out | ios::binary);
+        aspectRatioWrittenToFile = false;
+        isFOVKnown = false;
 
-        // If the file is not open, sets fileNotFound to true
-        if (!file.is_open())
-        {
-            fileNotFound = true;
-        }
+        cout << "\n- Enter the desired width: ";
+        newWidth = HandleResolutionInput();
 
-        // Loops until the file is found and opened
-        while (fileNotFound)
-        {
-            cout << "\nFailed to open aladdin.exe, check if the executable has special permissions allowed that prevent the fixer from opening it (e.g: read-only mode), it's not present in the same directory as the fixer, or if the executable is currently running. Press Enter when all the mentioned problems are solved." << endl;
-            cin.get();
+        cout << "\n- Enter the desired height: ";
+        newHeight = HandleResolutionInput();
 
-            // Tries to open the file again
-            file.open("aladdin.exe", ios::in | ios::out | ios::binary);
+        newAspectRatio = static_cast<float>(((4.0f / 3.0f) / (newWidth / newHeight)) * 0.75f);
 
-            if (file.is_open())
-            {
-                if (fileOpened == 0)
-                {
-                    cout << "\naladdin.exe opened successfully!" << endl;
-                    fileOpened = 1; // Sets fileOpened to 1 after the file is opened successfully
-                }
-                fileNotFound = false; // Sets fileNotFound to false as the file is found and opened
-            }
-        }
+        file.seekp(kAspectRatioOffset);
+        file.write(reinterpret_cast<const char *>(&newAspectRatio), sizeof(newAspectRatio));
 
-        do
-        {
-            arWrittenToFile = 0;
-            fovWrittenToFile = 0;
-            cout << "\nEnter the desired width: ";
-            cin >> width;
+        aspectRatioWrittenToFile = true;
 
-            if (cin.fail())
-            {
-                cin.clear();                                                   // Clear error flags
-                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore invalid input
-                width = -1;                                                    // Ensure the loop continues
-                cout << "Invalid input. Please enter a numeric value." << std::endl;
-            }
-            else if (width <= 0 || width > 65535)
-            {
-                cout << "Please enter a positive number for width less than 65536." << std::endl;
-            }
-        } while (width <= 0 || width > 65535);
-
-        do
-        {
-            cout << "\nEnter the desired height: ";
-            cin >> height;
-
-            if (cin.fail())
-            {
-                cin.clear();                                                   // Clear error flags
-                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore invalid input
-                height = -1;                                                   // Ensure the loop continues
-                cout << "Invalid input. Please enter a numeric value." << endl;
-            }
-            else if (height <= 0 || height > 65535)
-            {
-                std::cout << "Please enter a positive number for height less than 65536." << endl;
-            }
-        } while (height <= 0 || height > 65535);
-
-        aspectRatio = (4.0f / 3.0f) / (width / height);
-
-        // Adjust the result for the new base FOV
-        aspectRatio *= 0.75f; // Scale the result by the ratio of the new base FOV to the old base FOV
-
-        file.seekp(0x000115E9);
-        file.write(reinterpret_cast<const char *>(&aspectRatio), sizeof(aspectRatio));
-        arWrittenToFile = 1;
-
-        cout << "\nDo you want to fix the FOV automatically based on the resolution typed above (1) or set a custom FOV multiplier value (2)?: ";
-        cin >> choice;
+        cout << "\n- Do you want to fix the FOV automatically based on the resolution typed above (1) or set a custom FOV multiplier value (2)?: ";
+        HandleUserInput(choice);
 
         switch (choice)
         {
         case 1:
-            if (aspectRatio == 0.75)
+            if (newAspectRatio == 0.75)
             {
                 FOV = 0.8546222448; // 4:3
-                file.seekp(0x0009C206);
-                file.write(reinterpret_cast<const char *>(&FOV), sizeof(FOV));
-                fovWrittenToFile = 1;
+                isFOVKnown = true;
             }
-            else if (aspectRatio == 0.5625)
+            else if (newAspectRatio == 0.5625)
             {
                 FOV = 1.139; // 16:9
-                file.seekp(0x0009C206);
-                file.write(reinterpret_cast<const char *>(&FOV), sizeof(FOV));
-                fovWrittenToFile = 1;
+                isFOVKnown = true;
             }
-            else if (aspectRatio == 0.625)
+            else if (newAspectRatio == 0.625)
             {
                 FOV = 1.025; // 16:10
-                file.seekp(0x0009C206);
-                file.write(reinterpret_cast<const char *>(&FOV), sizeof(FOV));
-                fovWrittenToFile = 1;
+                isFOVKnown = true;
             }
-            else if (aspectRatio == 0.8)
+            else if (newAspectRatio == 0.8)
             {
                 FOV = 0.8; // 5:4
-                file.seekp(0x0009C206);
-                file.write(reinterpret_cast<const char *>(&FOV), sizeof(FOV));
-                fovWrittenToFile = 1;
+                isFOVKnown = true;
             }
-            else if (aspectRatio == 0.6)
+            else if (newAspectRatio == 0.6)
             {
                 FOV = 1.0674; // 15:9
-                file.seekp(0x0009C206);
-                file.write(reinterpret_cast<const char *>(&FOV), sizeof(FOV));
-                fovWrittenToFile = 1;
+                isFOVKnown = true;
             }
-            else if (aspectRatio == 0.421875)
+            else if (newAspectRatio == 0.421875)
             {
                 FOV = 1.518; // 21:9 (2560x1080)
-                file.seekp(0x0009C206);
-                file.write(reinterpret_cast<const char *>(&FOV), sizeof(FOV));
-                fovWrittenToFile = 1;
+                isFOVKnown = true;
             }
-            else if (aspectRatio == 0.41860465116)
+            else if (newAspectRatio == 0.41860465116)
             {
                 FOV = 1.53; // 21:9 (3440x1440)
-                file.seekp(0x0009C206);
-                file.write(reinterpret_cast<const char *>(&FOV), sizeof(FOV));
-                fovWrittenToFile = 1;
+                isFOVKnown = true;
             }
-            else if (aspectRatio == 0.4166666)
+            else if (newAspectRatio == 0.4166666)
             {
                 FOV = 1.54; // 21:9 (3840x1600)
-                file.seekp(0x0009C206);
-                file.write(reinterpret_cast<const char *>(&FOV), sizeof(FOV));
-                fovWrittenToFile = 1;
+                isFOVKnown = true;
             }
-            else if (aspectRatio == 0.3125)
+            else if (newAspectRatio == 0.3125)
             {
                 FOV = 2.05; // 32:10
-                file.seekp(0x0009C206);
-                file.write(reinterpret_cast<const char *>(&FOV), sizeof(FOV));
-                fovWrittenToFile = 1;
+                isFOVKnown = true;
             }
-            else if (aspectRatio == 0.28125)
+            else if (newAspectRatio == 0.28125)
             {
                 FOV = 2.28; // 32:9
-                file.seekp(0x0009C206);
-                file.write(reinterpret_cast<const char *>(&FOV), sizeof(FOV));
-                fovWrittenToFile = 1;
+                isFOVKnown = true;
             }
-            else if (aspectRatio == 0.2666666)
+            else if (newAspectRatio == 0.2666666)
             {
                 FOV = 2.4; // 15:4
-                file.seekp(0x0009C206);
-                file.write(reinterpret_cast<const char *>(&FOV), sizeof(FOV));
-                fovWrittenToFile = 1;
+                isFOVKnown = true;
             }
-            else if (aspectRatio == 0.25)
+            else if (newAspectRatio == 0.25)
             {
                 FOV = 2.565; // 12:3
-                file.seekp(0x0009C206);
-                file.write(reinterpret_cast<const char *>(&FOV), sizeof(FOV));
-                fovWrittenToFile = 1;
+                isFOVKnown = true;
             }
-            else if (aspectRatio == 0.2083333)
+            else if (newAspectRatio == 0.2083333)
             {
                 FOV = 3.076; // 48:10
-                file.seekp(0x0009C206);
-                file.write(reinterpret_cast<const char *>(&FOV), sizeof(FOV));
-                fovWrittenToFile = 1;
+                isFOVKnown = true;
             }
-            else if (aspectRatio == 0.2)
+            else if (newAspectRatio == 0.2)
             {
                 FOV = 3.205; // 45:9
-                file.seekp(0x0009C206);
-                file.write(reinterpret_cast<const char *>(&FOV), sizeof(FOV));
-                fovWrittenToFile = 1;
+                isFOVKnown = true;
             }
-            else if (aspectRatio == 0.1875)
+            else if (newAspectRatio == 0.1875)
             {
                 FOV = 3.42; // 48:9
-                file.seekp(0x0009C206);
-                file.write(reinterpret_cast<const char *>(&FOV), sizeof(FOV));
-                fovWrittenToFile = 1;
+                isFOVKnown = true;
             }
             else
             {
                 cout << "\nThe FOV on this aspect ratio can't yet be fixed with this patch, please contact AlphaYellow on the mod's page or through Discord (alphayellow) to add support for it or set a custom FOV multiplier value." << endl;
-                fovWrittenToFile = 0;
+                isFOVKnown = false;
             }
             break;
 
         case 2:
-            cout << "\nType a custom FOV multiplier value (default for 4:3 aspect ratio is 0,8546222448): " << endl;
+            cout << "\n- Type a custom FOV multiplier value (default for 4:3 aspect ratio is 0,8546222448): " << endl;
             cin >> FOV;
-            file.seekp(0x0009C206);
-            file.write(reinterpret_cast<const char *>(&FOV), sizeof(FOV));
-            fovWrittenToFile = 1;
+            isFOVKnown = true;
             break;
         }
 
-        // Confirmation message
-        if (arWrittenToFile == 1 && fovWrittenToFile == 0)
+        if (isFOVKnown = true)
         {
-            cout << "\nSuccessfully changed the aspect ratio in the executable. You can now press Enter to close the program." << endl;
+            file.seekp(kFOVOffset);
+            file.write(reinterpret_cast<const char *>(&FOV), sizeof(FOV));
         }
-        else if (arWrittenToFile == 1 && fovWrittenToFile == 1)
+
+        // Confirmation message
+        if (aspectRatioWrittenToFile && !isFOVKnown)
         {
-            cout << "\nSuccessfully changed the aspect ratio and field of view in the executable. You can now press Enter to close the program." << endl;
+            cout << "\nSuccessfully changed the aspect ratio." << endl;
+        }
+        else if (aspectRatioWrittenToFile && isFOVKnown)
+        {
+            cout << "\nSuccessfully changed the aspect ratio and field of view." << endl;
         }
 
         // Close the file
         file.close();
 
-        do
+        cout << "\n- Do you want to exit the program (1) or try another value (2)?: ";
+        HandleUserInput(choice2);
+
+        if (choice2 == 1)
         {
-            cout << "\n- Do you want to exit the program (1) or try another value (2)?: ";
-            cin >> choice;
-
-            if (cin.fail())
+            cout << "\nPress enter to exit the program...";
+            do
             {
-                cin.clear();                                         // Clears error flags
-                cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignores invalid input
-                choice = -1;
-                cout << "Invalid input. Please enter 1 to exit the program or 2 to try another value." << endl;
-            }
-            else if (choice < 1 || choice > 2)
-            {
-                cout << "Please enter a valid number." << endl;
-            }
-        } while (choice < 1 || choice > 2);
-    } while (choice == 2); // Checks the flag in the loop condition
-
-    cout << "\nPress enter to exit the program...";
-    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clears the input buffer
-    cin.get();
-
-    return 0;
+                ch = _getch(); // Waits for user to press a key
+            } while (ch != '\r'); // Keeps waiting if the key is not Enter ('\r' is the Enter key in ASCII)
+            return 0;
+        }
+    } while (choice2 == 2); // Checks the flag in the loop condition
 }
