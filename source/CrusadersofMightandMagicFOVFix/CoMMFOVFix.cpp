9c0d7065a53e963ae3312df9adee9f5455445b49
AlphaYellow <diogoccsantos@gmail.com>
2024-05-31 14:30:30 +0100

    modified:   source/CrusadersofMightandMagicFOVFix/CoMMFOVFix.cpp

--- source/CrusadersofMightandMagicFOVFix/CoMMFOVFix.cpp
+++ source/CrusadersofMightandMagicFOVFix/CoMMFOVFix.cpp
@@ -7,120 +7,156 @@
 
 using namespace std;
 
+// Constants
+const streampos kHFOVOffset = 0x0002F762;
+const streampos kVFOVOffset = 0x0002F799;
+
+// Variables
+int choice, fileOpened, tempChoice;
+bool fileNotFound, validKeyPressed;
+float width, height, desiredFOV;
+double newCustomResolutionValue, newWidth, newHeight;
+fstream file;
+char ch;
+
+// Function to handle user input in choices
+void HandleUserInput(int &choice)
+{
+    tempChoice = -1;         // Temporary variable to store the input
+    validKeyPressed = false; // Flag to track if a valid key was pressed
+
+    while (true)
+    {
+        ch = _getch(); // Waits for user to press a key
+
+        // Checks if the key is '1' or '2'
+        if ((ch == '1' || ch == '2') && !validKeyPressed)
+        {
+            tempChoice = ch - '0';  // Stores the input temporarily
+            cout << ch;             // Echoes the valid input
+            validKeyPressed = true; // Sets the flag as a valid key has been pressed
+        }
+        else if (ch == '\b' || ch == 127) // Handles backspace or delete keys
+        {
+            if (tempChoice != -1) // Checks if there is something to delete
+            {
+                tempChoice = -1;         // Resets the temporary choice
+                cout << "\b \b";         // Erases the last character from the console
+                validKeyPressed = false; // Resets the flag as the input has been deleted
+            }
+        }
+        else if (ch == '\r') // If 'Enter' is pressed
+        {
+            if (tempChoice != -1) // Checks if a valid input has been made
+            {
+                choice = tempChoice; // Assigns the temporary input to the choice variable
+                cout << endl;        // Moves to a new line
+                break;               // Exits the loop since we have a confirmed input
+            }
+        }
+    }
+}
+
+// Function to handle user input in resolution
+double HandleResolutionInput()
+{
+    do
+    {
+        cin >> newCustomResolutionValue;
+
+        cin.clear();                                         // Clears error flags
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignores invalid input
+
+        if (cin.fail())
+        {
+            cin.clear();                                         // Clears error flags
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignores invalid input
+            cout << "Invalid input. Please enter a numeric value." << endl;
+        }
+        else if (newCustomResolutionValue <= 0 || newCustomResolutionValue >= 65535)
+        {
+            cout << "Please enter a valid number." << endl;
+        }
+    } while (newCustomResolutionValue <= 0 || newCustomResolutionValue > 65535);
+
+    return newCustomResolutionValue;
+}
+
+// Function to open the file
+void OpenFile(fstream &file)
+{
+    fileNotFound = false;
+    fileOpened = 0; // Initializes fileOpened to 0
+
+    file.open("crusaders.exe", ios::in | ios::out | ios::binary);
+
+    // If the file is not open, sets fileNotFound to true
+    if (!file.is_open())
+    {
+        fileNotFound = true;
+    }
+
+    // Loops until the file is found and opened
+    while (fileNotFound)
+    {
+
+        // Tries to open the file again
+        file.open("crusaders.exe", ios::in | ios::out | ios::binary);
+
+        if (!file.is_open())
+        {
+            cout << "\nFailed to open crusaders.exe, check if the executable has special permissions allowed that prevent the fixer from opening it (e.g: read-only mode), it's not present in the same directory as the fixer, or if the executable is currently running. Press Enter when all the mentioned problems are solved." << endl;
+            do
+            {
+                ch = _getch(); // Waits for user to press a key
+            } while (ch != '\r'); // Keeps waiting if the key is not Enter ('\r' is the Enter key in ASCII)
+        }
+        else
+        {
+            cout << "\ncrusaders.exe opened successfully!" << endl;
+            fileNotFound = false; // Sets fileNotFound to false as the file is found and opened
+        }
+    }
+}
+
 int main()
 {
-    float width, height, desiredFOV;
-    int choice, fileOpened;
-    bool fileNotFound;
-
     cout << "Crusaders of Might and Magic (1999) FOV Fixer by AlphaYellow, 2024\n\n----------------\n";
 
     do
     {
-        fstream file;
-        fileNotFound = false;
-        fileOpened = 0; // Initialize fileOpened to 0
+        OpenFile(file);
 
-        // Try to open the file initially
-        file.open("crusaders.exe", ios::in | ios::out | ios::binary);
+        cout << "\nEnter the desired width: ";
+        newWidth = HandleResolutionInput();
 
-        // If the file is not open, set fileNotFound to true
-        if (!file.is_open())
-        {
-            fileNotFound = true;
-        }
+        cout << "\nEnter the desired height: ";
+        newHeight = HandleResolutionInput();
 
-        // Loop until the file is found and opened
-        while (fileNotFound)
-        {
-            cout << "\nFailed to open crusaders.exe, check if the executable has special permissions allowed that prevent the fixer from opening it (e.g: read-only mode), it's not present in the same directory as the fixer, or if the executable is currently running. Press Enter when all the mentioned problems are solved." << endl;
-            getch(); // Wait for user input
+        desiredFOV = static_cast<float>((4.0f / 3.0f) / (newWidth / newHeight));
 
-            // Try to open the file again
-            file.open("crusaders.exe", ios::in | ios::out | ios::binary);
-
-            if (file.is_open())
-            {
-                if (fileOpened == 0)
-                {
-                    cout << "\ncrusaders.exe opened successfully!" << endl;
-                    fileOpened = 1; // Set fileOpened to 1 after the file is opened successfully
-                }
-                fileNotFound = false; // Set fileNotFound to false as the file is found and opened
-            }
-        }
-
-        do
-        {
-            cout << "\nEnter the desired width: ";
-            cin >> width;
-
-            if (cin.fail())
-            {
-                cin.clear();                                                   // Clear error flags
-                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore invalid input
-                width = -1;                                                    // Ensure the loop continues
-                cout << "Invalid input. Please enter a numeric value." << std::endl;
-            }
-            else if (width <= 0 || width > 65535)
-            {
-                cout << "Please enter a positive number for width less than 65536." << std::endl;
-            }
-        } while (width <= 0 || width > 65535);
-
-        do
-        {
-            cout << "\nEnter the desired height: ";
-            cin >> height;
-
-            if (cin.fail())
-            {
-                cin.clear();                                                   // Clear error flags
-                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore invalid input
-                height = -1;                                                   // Ensure the loop continues
-                cout << "Invalid input. Please enter a numeric value." << endl;
-            }
-            else if (height <= 0 || height > 65535)
-            {
-                std::cout << "Please enter a positive number for height less than 65536." << endl;
-            }
-        } while (height <= 0 || height > 65535);
-
-        desiredFOV = (4.0f / 3.0f) / (width / height);
-        
-        file.seekp(0x0002F762); // HFOV
+        file.seekp(kHFOVOffset);
         file.write(reinterpret_cast<const char *>(&desiredFOV), sizeof(desiredFOV));
-        file.seekp(0x0002F799); // VFOV
+        file.seekp(kVFOVOffset);
         file.write(reinterpret_cast<const char *>(&desiredFOV), sizeof(desiredFOV));
 
         // Confirmation message
         cout << "\nSuccessfully changed the field of view." << endl;
 
-        // Close the file
+        // Closes the file
         file.close();
 
-        do
+        cout << "\n- Do you want to exit the program (1) or try another value (2)?: ";
+        HandleUserInput(choice);
+
+        if (choice == 1)
         {
-            cout << "\n- Do you want to exit the program (1) or try another value (2)?: ";
-            cin >> choice;
-
-            if (cin.fail())
+            cout << "\nPress enter to exit the program...";
+            do
             {
-                cin.clear();                                         // Clears error flags
-                cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignores invalid input
-                choice = -1;
-                cout << " Invalid input. Please enter 1 to exit the program or 2 to try another value." << endl;
-            }
-            else if (choice < 1 || choice > 2)
-            {
-                cout << " Please enter a valid number." << endl;
-            }
-        } while (choice < 1 || choice > 2);
+                ch = _getch(); // Waits for user to press a key
+            } while (ch != '\r'); // Keeps waiting if the key is not Enter ('\r' is the Enter key in ASCII)
+            return 0;
+        }
     } while (choice != 1); // Checks the flag in the loop condition
-
-    cout << "\nPress Enter to exit the program...";
-    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clears the input buffer
-    cin.get();
-
-    return 0;
 }
