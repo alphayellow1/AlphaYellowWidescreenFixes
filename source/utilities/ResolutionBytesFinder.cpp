bb7f2a20d52fbb393e198a596ac0f75aaf0db0f1
AlphaYellow <diogoccsantos@gmail.com>
2024-06-03 21:59:39 +0100

    modified:   source/utilities/ResolutionBytesFinder.cpp

--- source/utilities/ResolutionBytesFinder.cpp
+++ source/utilities/ResolutionBytesFinder.cpp
@@ -8,21 +8,23 @@
 int main()
 {
     std::string executableName;
-    std::cout << "Enter the executable name: ";
-    std::cin >> executableName;
+    std::cout << "- Enter the executable name: ";
+    std::getline(std::cin, executableName);
 
     std::ifstream file(executableName, std::ios::binary);
     if (!file)
     {
-        std::cerr << "Could not open the file." << std::endl;
+        std::cerr << "\nCould not open the file." << std::endl;
         return 1;
     }
 
-    int16_t firstValue, secondValue;
-    std::cout << "Enter the first 2-byte integer value: ";
+    int16_t firstValue, secondValue, byteRange;
+    std::cout << "\n- Enter the first 2-byte integer value: ";
     std::cin >> firstValue;
-    std::cout << "Enter the second 2-byte integer value: ";
+    std::cout << "\n- Enter the second 2-byte integer value: ";
     std::cin >> secondValue;
+    std::cout << "\n- Enter the byte search range: ";
+    std::cin >> byteRange;
 
     std::vector<char> buffer(std::istreambuf_iterator<char>(file), {});
     for (size_t i = 0; i < buffer.size(); ++i)
@@ -30,13 +32,13 @@ int main()
         if (i <= buffer.size() - 2 && *reinterpret_cast<int16_t *>(&buffer[i]) == firstValue)
         {
             std::cout << "Found first value at address: " << std::uppercase << std::setfill('0') << std::setw(8) << std::hex << i << std::endl;
-            for (int j = -15; j <= 15; ++j)
+            for (int byteRange = -15; byteRange <= 15; ++byteRange)
             {
                 if (i + j >= 0 && i + j < buffer.size() - 2)
                 {
-                    if (*reinterpret_cast<int16_t *>(&buffer[i + j]) == secondValue)
+                    if (*reinterpret_cast<int16_t *>(&buffer[i + byteRange]) == secondValue)
                     {
-                        std::cout << "Found second value within 15 bytes of first value at address: " << std::uppercase << std::setfill('0') << std::setw(8) << std::hex << i + j << std::endl;
+                        std::cout << "Found second value within 15 bytes of first value at address: " << std::uppercase << std::setfill('0') << std::setw(8) << std::hex << i + byteRange << std::endl;
                     }
                 }
             }
