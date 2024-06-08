35cc1d199ed91e9e161e5fba6bfc20f0f384c932
AlphaYellow <diogoccsantos@gmail.com>
2024-06-08 03:44:27 +0100

    new file:   source/Americas10MostWantedFOVFix/Americas10MostWantedFOVFix.cpp

---
+++ source/Americas10MostWantedFOVFix/Americas10MostWantedFOVFix.cpp
@@ -0,0 +1,207 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <conio.h> // For getch()
+#include <cstdint> // For uint8_t
+#include <limits>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+// Constants
+const streampos kHFOVOffset = 0x00247120;
+const streampos kOverallFOVOffset = 0x001E1640;
+
+// Variables
+int choice1, choice2, fileOpened, tempChoice;
+bool fileNotFound, validKeyPressed;
+float width, height, newHFOV, newOverallFOV, customOverallFOV;
+double newCustomResolutionValue, newWidth, newHeight;
+fstream file;
+string input;
+char ch;
+
+// Function to handle user input in choices
+void HandleChoiceInput(int &choice)
+{
+    tempChoice = -1;         // Temporary variable to store the input
+    validKeyPressed = false; // Flag to track if a valid key was pressed
+
+    while (true)
+    {
+        ch = _getch(); // Waits for user to press a key
+
+        // Checks if the key is '1' or '2'
+        if ((ch == '1' || ch == '2') && !validKeyPressed)
+        {
+            tempChoice = ch - '0';  // Converts char to int and stores the input temporarily
+            cout << ch;             // Echoes the valid input
+            validKeyPressed = true; // Sets the flag as a valid key has been pressed
+        }
+        else if (ch == '\b' || ch == 127) // Handles backspace or delete keys
+        {
+            if (tempChoice != -1) // Checks if there is something to delete
+            {
+                tempChoice = -1;         // Resets the temporary choice
+                cout << "\b \b";         // Erases the last character from the console
+                validKeyPressed = false; // Resets the flag as the input has been deleted
+            }
+        }
+        // If 'Enter' is pressed and a valid key has been pressed prior
+        else if (ch == '\r' && validKeyPressed)
+        {
+            choice = tempChoice; // Assigns the temporary input to the choice variable
+            cout << endl;        // Moves to a new line
+            break;               // Exits the loop since we have a confirmed input
+        }
+    }
+}
+
+float HandleFOVInput()
+{
+    do
+    {
+        // Reads the input as a string
+        cin >> input;
+
+        // Replaces all commas with dots
+        replace(input.begin(), input.end(), ',', '.');
+
+        // Parses the string to a float
+        customOverallFOV = stof(input);
+
+        if (cin.fail())
+        {
+            cin.clear();                                         // Clears error flags
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignores invalid input
+            cout << "Invalid input. Please enter a numeric value." << endl;
+        }
+        else if (customOverallFOV <= 0 || customOverallFOV >= 180)
+        {
+            cout << "Please enter a valid number for the FOV (greater than 0)." << endl;
+        }
+    } while (customOverallFOV <= 0);
+
+    return customOverallFOV;
+}
+
+// Function to handle user input in resolution
+double HandleResolutionInput()
+{
+    do
+    {
+        cin >> newCustomResolutionValue;
+
+        cin.clear();                                         // Clears error flags
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignores invalid input
+
+        if (cin.fail())
+        {
+            cin.clear();                                         // Clears error flags
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignores invalid input
+            cout << "Invalid input. Please enter a numeric value." << endl;
+        }
+        else if (newCustomResolutionValue <= 0 || newCustomResolutionValue >= 65535)
+        {
+            cout << "Please enter a valid number." << endl;
+        }
+    } while (newCustomResolutionValue <= 0 || newCustomResolutionValue > 65535);
+
+    return newCustomResolutionValue;
+}
+
+// Function to open the file
+void OpenFile(fstream &file)
+{
+    fileNotFound = false;
+    fileOpened = 0; // Initializes fileOpened to 0
+
+    file.open("A10MW.exe", ios::in | ios::out | ios::binary);
+
+    // If the file is not open, sets fileNotFound to true
+    if (!file.is_open())
+    {
+        fileNotFound = true;
+    }
+
+    // Loops until the file is found and opened
+    while (fileNotFound)
+    {
+
+        // Tries to open the file again
+        file.open("A10MW.exe", ios::in | ios::out | ios::binary);
+
+        if (!file.is_open())
+        {
+            cout << "\nFailed to open A10MW.exe, check if the executable has special permissions allowed that prevent the fixer from opening it (e.g: read-only mode), it's not present in the same directory as the fixer, or if the executable is currently running. Press Enter when all the mentioned problems are solved." << endl;
+            do
+            {
+                ch = _getch(); // Waits for user to press a key
+            } while (ch != '\r'); // Keeps waiting if the key is not Enter ('\r' is the Enter key in ASCII)
+        }
+        else
+        {
+            cout << "\nA10MW.exe opened successfully!" << endl;
+            fileNotFound = false; // Sets fileNotFound to false as the file is found and opened
+        }
+    }
+}
+
+int main()
+{
+    cout << "America's 10 Most Wanted (2003) FOV Fixer v1.0 by AlphaYellow, 2024\n\n----------------\n";
+
+    do
+    {
+        OpenFile(file);
+
+        cout << "\nEnter the desired width: ";
+        newWidth = HandleResolutionInput();
+
+        cout << "\nEnter the desired height: ";
+        newHeight = HandleResolutionInput();
+
+        newHFOV = (static_cast<float>(newWidth) / static_cast<float>(newHeight)) / (4.0f / 3.0f);
+
+        cout << "\n- Do you want to keep the same FOV value of the automatic one for the aspect ratio (1) or extend it (2)?: ";
+        HandleChoiceInput(choice1);
+
+        switch (choice1)
+        {
+        case 1:
+            newOverallFOV = 1.0f;
+            break;
+
+        case 2:
+            cout << "\n- Enter the custom FOV value (default for the 4:3 aspect ratio is 1.0): ";
+            newOverallFOV = HandleFOVInput();
+            break;
+        }
+
+        file.seekp(kHFOVOffset);
+        file.write(reinterpret_cast<const char *>(&newHFOV), sizeof(newHFOV));
+
+        file.seekp(kOverallFOVOffset);
+        file.write(reinterpret_cast<const char *>(&newOverallFOV), sizeof(newOverallFOV));
+
+        // Confirmation message
+        cout << "\nSuccessfully changed the field of view." << endl;
+
+        // Closes the file
+        file.close();
+
+        cout << "\n- Do you want to exit the program (1) or try another value (2)?: ";
+        HandleChoiceInput(choice2);
+
+        if (choice2 == 1)
+        {
+            cout << "\nPress enter to exit the program...";
+            do
+            {
+                ch = _getch(); // Waits for user to press a key
+            } while (ch != '\r'); // Keeps waiting if the key is not Enter ('\r' is the Enter key in ASCII)
+            return 0;
+        }
+    } while (choice2 != 1); // Checks the flag in the loop condition
+}
