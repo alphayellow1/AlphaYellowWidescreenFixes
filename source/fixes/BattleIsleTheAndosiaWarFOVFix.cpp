ec15c81b5fb82c1966af7f7d7a736c6a6429a3d2
AlphaYellow <diogoccsantos@gmail.com>
2024-06-26 21:01:11 +0100

    modified:   source/fixes/BattleIsleTheAndosiaWarFOVFix.cpp

--- source/fixes/BattleIsleTheAndosiaWarFOVFix.cpp
+++ source/fixes/BattleIsleTheAndosiaWarFOVFix.cpp
@@ -235,5 +235,7 @@ int main()
             } while (ch != '\r'); // Keeps waiting if the key is not Enter ('\r' is the Enter key in ASCII)
             return 0;
         }
+
+        cout << "\n----------------\n";
     } while (choice2 == 2); // Checks the flag in the loop condition
 }
