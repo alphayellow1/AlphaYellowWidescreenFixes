694ea568f7923db1b1f62849ad7434c61688e84b
AlphaYellow <diogoccsantos@gmail.com>
2024-08-08 14:01:08 +0100

    modified:   source/fixes/AladdinInNasirasRevengeWidescreenFix.cpp

--- source/fixes/AladdinInNasirasRevengeWidescreenFix.cpp
+++ source/fixes/AladdinInNasirasRevengeWidescreenFix.cpp
@@ -14,9 +14,9 @@ const streampos kAspectRatioOffset = 0x000115E9;
 const streampos kFOVOffset = 0x0009C206;
 
 // Variables
-float newAspectRatio, FOV;
-bool fileNotFound, aspectRatioWrittenToFile, isFOVKnown, validKeyPressed;
-int choice, choice2, tempChoice;
+float newAspectRatio, desiredFOV, customFOVMultiplier;
+bool fileNotFound, validKeyPressed;
+int choice1, choice2, tempChoice;
 double newCustomResolutionValue, newWidth, newHeight;
 fstream file;
 string input;
@@ -49,13 +49,41 @@ void HandleChoiceInput(int &choice)
             }
         }
         // If 'Enter' is pressed and a valid key has been pressed prior
-        else if (ch == '\r' && validKeyPressed) 
+        else if (ch == '\r' && validKeyPressed)
         {
             choice = tempChoice; // Assigns the temporary input to the choice variable
             cout << endl;        // Moves to a new line
             break;               // Exits the loop since we have a confirmed input
         }
     }
+}
+
+float HandleFOVInput()
+{
+    do
+    {
+        // Reads the input as a string
+        cin >> input;
+
+        // Replaces all commas with dots
+        replace(input.begin(), input.end(), ',', '.');
+
+        // Parses the string to a float
+        customFOVMultiplier = stof(input);
+
+        if (cin.fail())
+        {
+            cin.clear();                                         // Clears error flags
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignores invalid input
+            cout << "Invalid input. Please enter a numeric value." << endl;
+        }
+        else if (customFOVMultiplier <= 0)
+        {
+            cout << "Please enter a number greater than 0 for the FOV." << endl;
+        }
+    } while (customFOVMultiplier <= 0);
+
+    return customFOVMultiplier;
 }
 
 // Function to handle user input in resolution
@@ -87,7 +115,7 @@ double HandleResolutionInput()
 void OpenFile(fstream &file)
 {
     fileNotFound = false;
-    
+
     file.open("aladdin.exe", ios::in | ios::out | ios::binary);
 
     // If the file is not open, sets fileNotFound to true
@@ -120,148 +148,43 @@ void OpenFile(fstream &file)
 
 int main()
 {
-
-    cout << "Aladdin in Nasira's Revenge (2001) Widescreen Fixer v1.3 by AlphaYellow, 2024\n\n----------------\n";
+    cout << "Aladdin in Nasira's Revenge (2001) Widescreen Fixer v1.4 by AlphaYellow, 2024\n\n----------------\n";
 
     do
     {
-        OpenFile(file);
-
-        aspectRatioWrittenToFile = false;
-        isFOVKnown = false;
-
         cout << "\n- Enter the desired width: ";
         newWidth = HandleResolutionInput();
 
         cout << "\n- Enter the desired height: ";
         newHeight = HandleResolutionInput();
 
-        newAspectRatio = static_cast<float>(((4.0f / 3.0f) / (newWidth / newHeight)) * 0.75f);
+        newAspectRatio = 0.75f * ((4.0f / 3.0f) / (static_cast<float>(newWidth) / static_cast<float>(newHeight)));
+
+        OpenFile(file);
 
         file.seekp(kAspectRatioOffset);
         file.write(reinterpret_cast<const char *>(&newAspectRatio), sizeof(newAspectRatio));
 
-        aspectRatioWrittenToFile = true;
-
         cout << "\n- Do you want to fix the FOV automatically based on the resolution typed above (1) or set a custom FOV multiplier value (2)?: ";
-        HandleChoiceInput(choice);
+        HandleChoiceInput(choice1);
 
-        switch (choice)
+        switch (choice1)
         {
         case 1:
-            if (newAspectRatio == 0.75)
-            {
-                FOV = 0.8546222448; // 4:3
-                isFOVKnown = true;
-            }
-            else if (newAspectRatio == 0.5625)
-            {
-                FOV = 1.139; // 16:9
-                isFOVKnown = true;
-            }
-            else if (newAspectRatio == 0.625)
-            {
-                FOV = 1.025; // 16:10
-                isFOVKnown = true;
-            }
-            else if (newAspectRatio == 0.8)
-            {
-                FOV = 0.8; // 5:4
-                isFOVKnown = true;
-            }
-            else if (newAspectRatio == 0.6)
-            {
-                FOV = 1.0674; // 15:9
-                isFOVKnown = true;
-            }
-            else if (newAspectRatio == 0.421875)
-            {
-                FOV = 1.518; // 21:9 (2560x1080)
-                isFOVKnown = true;
-            }
-            else if (newAspectRatio == 0.41860465116)
-            {
-                FOV = 1.53; // 21:9 (3440x1440)
-                isFOVKnown = true;
-            }
-            else if (newAspectRatio == 0.4166666)
-            {
-                FOV = 1.54; // 21:9 (3840x1600)
-                isFOVKnown = true;
-            }
-            else if (newAspectRatio == 0.3125)
-            {
-                FOV = 2.05; // 32:10
-                isFOVKnown = true;
-            }
-            else if (newAspectRatio == 0.28125)
-            {
-                FOV = 2.28; // 32:9
-                isFOVKnown = true;
-            }
-            else if (newAspectRatio == 0.2666666)
-            {
-                FOV = 2.4; // 15:4
-                isFOVKnown = true;
-            }
-            else if (newAspectRatio == 0.25)
-            {
-                FOV = 2.565; // 12:3
-                isFOVKnown = true;
-            }
-            else if (newAspectRatio == 0.2083333)
-            {
-                FOV = 3.076; // 48:10
-                isFOVKnown = true;
-            }
-            else if (newAspectRatio == 0.2)
-            {
-                FOV = 3.205; // 45:9
-                isFOVKnown = true;
-            }
-            else if (newAspectRatio == 0.1875)
-            {
-                FOV = 3.42; // 48:9
-                isFOVKnown = true;
-            }
-            else
-            {
-                cout << "\nThe FOV on this aspect ratio can't yet be fixed with this patch, please contact AlphaYellow on the mod's page or through Discord (alphayellow) to add support for it or set a custom FOV multiplier value." << endl;
-                isFOVKnown = false;
-            }
+            desiredFOV = 0.8546222448f / ((4.0f / 3.0f) / (static_cast<float>(newWidth) / static_cast<float>(newHeight)));
             break;
-
         case 2:
-            cout << "\n- Type a custom FOV multiplier value (default for 4:3 aspect ratio is 0,8546222448): ";
-
-            // Reads the input as a string
-            cin >> input;
-
-            // Replaces all commas with dots
-            replace(input.begin(), input.end(), ',', '.');
-
-            // Parses the string to a float
-            FOV = stof(input);
-
-            isFOVKnown = true;
+            cout << "\n- Type a custom FOV multiplier value (default for 4:3 aspect ratio is 0.8546222448): ";
+            desiredFOV = HandleFOVInput();
             break;
         }
 
-        if (isFOVKnown = true)
-        {
-            file.seekp(kFOVOffset);
-            file.write(reinterpret_cast<const char *>(&FOV), sizeof(FOV));
-        }
+        file.seekp(kFOVOffset);
+        file.write(reinterpret_cast<const char *>(&desiredFOV), sizeof(desiredFOV));
 
         // Confirmation message
-        if (aspectRatioWrittenToFile && !isFOVKnown)
-        {
-            cout << "\nSuccessfully changed the aspect ratio." << endl;
-        }
-        else if (aspectRatioWrittenToFile && isFOVKnown)
-        {
-            cout << "\nSuccessfully changed the aspect ratio and field of view." << endl;
-        }
+
+        cout << "\nSuccessfully changed the aspect ratio and field of view." << endl;
 
         // Closes the file
         file.close();
