9af2d546143df978899f5e0be91426e3c2d6cfbd
AlphaYellow <diogoccsantos@gmail.com>
2024-11-11 15:04:31 +0000

    modified:   source/fixes/F1WorldGrandPrix2000WidescreenFix.cpp

--- source/fixes/F1WorldGrandPrix2000WidescreenFix.cpp
+++ source/fixes/F1WorldGrandPrix2000WidescreenFix.cpp
@@ -161,7 +161,7 @@ double NewCameraFOVCalculation(uint32_t &newWidthValue, uint32_t &newHeightValue
 
 int main()
 {
-    cout << "F1 World Grand Prix 2000 (2001) Widescreen Fixer v1.1 by AlphaYellow and AuToMaNiAk005, 2024\n\n----------------\n";
+    cout << "F1 World Grand Prix 2000 (2001) Widescreen Fixer v1.2 by AlphaYellow and AuToMaNiAk005, 2024\n\n----------------\n";
 
     do
     {
