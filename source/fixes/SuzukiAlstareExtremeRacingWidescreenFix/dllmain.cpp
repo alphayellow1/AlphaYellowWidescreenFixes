0cb9df955d3476e534c5e5b3e11fa4ec308271eb
AlphaYellow <diogoccsantos@gmail.com>
2025-03-29 13:09:59 +0000

    modified:   project/AlphaYellowWidescreenFixes.sln 	new file:   project/SuzukiAlstareExtremeRacingWidescreenFix.vcxproj 	new file:   project/SuzukiAlstareExtremeRacingWidescreenFix.vcxproj.filters 	new file:   source/fixes/SuzukiAlstareExtremeRacingWidescreenFix/dllmain.cpp

---
+++ source/fixes/SuzukiAlstareExtremeRacingWidescreenFix/dllmain.cpp
@@ -0,0 +1,414 @@
+// Include necessary headers
+#include "stdafx.h"
+#include "helper.hpp"
+
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <inipp/inipp.h>
+#include <safetyhook.hpp>
+#include <vector>
+#include <map>
+#include <windows.h>
+#include <psapi.h> // For GetModuleInformation
+#include <fstream>
+#include <filesystem>
+#include <sstream>
+#include <cstring>
+#include <iomanip>
+#include <cstdint>
+#include <iostream>
+
+#define spdlog_confparse(var) spdlog::info("Config Parse: {}: {}", #var, var)
+
+HMODULE exeModule = GetModuleHandle(NULL);
+HMODULE thisModule;
+
+// Fix details
+std::string sFixName = "SuzukiAlstareExtremeRacingWidescreenFix";
+std::string sFixVersion = "1.0";
+std::filesystem::path sFixPath;
+
+// Ini
+inipp::Ini<char> ini;
+std::string sConfigFile = sFixName + ".ini";
+
+// Logger
+std::shared_ptr<spdlog::logger> logger;
+std::string sLogFile = sFixName + ".log";
+std::filesystem::path sExePath;
+std::string sExeName;
+
+// Constants
+constexpr float fOldAspectRatio = 4.0f / 3.0f;
+constexpr float tolerance = 0.0001f;
+
+// Ini variables
+bool bFixActive;
+
+// Variables
+int iCurrentResX;
+int iCurrentResY;
+float fNewCameraFOV;
+float fFOVFactor;
+float fNewAspectRatio;
+float fNewAspectRatio2;
+static float fCurrentCameraHFOV;
+
+// Game detection
+enum class Game
+{
+	SAER,
+	Unknown
+};
+
+struct GameInfo
+{
+	std::string GameTitle;
+	std::string ExeName;
+};
+
+const std::map<Game, GameInfo> kGames = {
+	{Game::SAER, {"Suzuki Alstare Extreme Racing", "SaerPC.exe"}},
+};
+
+const GameInfo* game = nullptr;
+Game eGameType = Game::Unknown;
+
+void Logging()
+{
+	// Get path to DLL
+	WCHAR dllPath[_MAX_PATH] = { 0 };
+	GetModuleFileNameW(thisModule, dllPath, MAX_PATH);
+	sFixPath = dllPath;
+	sFixPath = sFixPath.remove_filename();
+
+	// Get game name and exe path
+	WCHAR exePathW[_MAX_PATH] = { 0 };
+	GetModuleFileNameW(exeModule, exePathW, MAX_PATH);
+	sExePath = exePathW;
+	sExeName = sExePath.filename().string();
+	sExePath = sExePath.remove_filename();
+
+	// Spdlog initialization
+	try
+	{
+		logger = spdlog::basic_logger_st(sFixName.c_str(), sExePath.string() + "\\" + sLogFile, true);
+		spdlog::set_default_logger(logger);
+		spdlog::flush_on(spdlog::level::debug);
+		spdlog::set_level(spdlog::level::debug); // Enable debug level logging
+
+		spdlog::info("----------");
+		spdlog::info("{:s} v{:s} loaded.", sFixName.c_str(), sFixVersion.c_str());
+		spdlog::info("----------");
+		spdlog::info("Log file: {}", sExePath.string() + "\\" + sLogFile);
+		spdlog::info("----------");
+		spdlog::info("Module Name: {0:s}", sExeName.c_str());
+		spdlog::info("Module Path: {0:s}", sExePath.string());
+		spdlog::info("Module Address: 0x{0:X}", (uintptr_t)exeModule);
+		spdlog::info("----------");
+		spdlog::info("DLL has been successfully loaded.");
+	}
+	catch (const spdlog::spdlog_ex& ex)
+	{
+		AllocConsole();
+		FILE* dummy;
+		freopen_s(&dummy, "CONOUT$", "w", stdout);
+		std::cout << "Log initialization failed: " << ex.what() << std::endl;
+		FreeLibraryAndExitThread(thisModule, 1);
+	}
+}
+
+void Configuration()
+{
+	// Inipp initialization
+	std::ifstream iniFile(sFixPath.string() + "\\" + sConfigFile);
+	if (!iniFile)
+	{
+		AllocConsole();
+		FILE* dummy;
+		freopen_s(&dummy, "CONOUT$", "w", stdout);
+		std::cout << sFixName.c_str() << " v" << sFixVersion.c_str() << " loaded." << std::endl;
+		std::cout << "ERROR: Could not locate config file." << std::endl;
+		std::cout << "ERROR: Make sure " << sConfigFile.c_str() << " is located in " << sFixPath.string().c_str() << std::endl;
+		spdlog::shutdown();
+		FreeLibraryAndExitThread(thisModule, 1);
+	}
+	else
+	{
+		spdlog::info("Config file: {}", sFixPath.string() + "\\" + sConfigFile);
+		ini.parse(iniFile);
+	}
+
+	// Parse config
+	ini.strip_trailing_comments();
+	spdlog::info("----------");
+
+	// Load settings from ini
+	inipp::get_value(ini.sections["WidescreenFix"], "Enabled", bFixActive);
+	spdlog_confparse(bFixActive);
+
+	// Load resolution from ini
+	inipp::get_value(ini.sections["Settings"], "Width", iCurrentResX);
+	inipp::get_value(ini.sections["Settings"], "Height", iCurrentResY);
+	inipp::get_value(ini.sections["Settings"], "FOVFactor", fFOVFactor);
+	spdlog_confparse(iCurrentResX);
+	spdlog_confparse(iCurrentResY);
+	spdlog_confparse(fFOVFactor);
+
+	// If resolution not specified, use desktop resolution
+	if (iCurrentResX <= 0 || iCurrentResY <= 0)
+	{
+		spdlog::info("Resolution not specified in ini file. Using desktop resolution.");
+		// Implement Util::GetPhysicalDesktopDimensions() accordingly
+		auto desktopDimensions = Util::GetPhysicalDesktopDimensions();
+		iCurrentResX = desktopDimensions.first;
+		iCurrentResY = desktopDimensions.second;
+		spdlog_confparse(iCurrentResX);
+		spdlog_confparse(iCurrentResY);
+	}
+
+	spdlog::info("----------");
+}
+
+bool DetectGame()
+{
+	for (const auto& [type, info] : kGames)
+	{
+		if (Util::stringcmp_caseless(info.ExeName, sExeName))
+		{
+			spdlog::info("Detected game: {:s} ({:s})", info.GameTitle, sExeName);
+			spdlog::info("----------");
+			eGameType = type;
+			game = &info;
+			return true;
+		}
+	}
+
+	spdlog::error("Failed to detect supported game, {:s} isn't supported by the fix.", sExeName);
+	return false;
+}
+
+float CalculateNewFOV(float fCurrentFOV)
+{
+	return fCurrentFOV * (fNewAspectRatio / fOldAspectRatio);
+}
+
+void WidescreenFix()
+{
+	if (eGameType == Game::SAER && bFixActive == true)
+	{
+		fNewAspectRatio = static_cast<float>(iCurrentResX) / static_cast<float>(iCurrentResY);
+
+		std::uint8_t* Resolution1ScanResult = Memory::PatternScan(exeModule, "68 E0 01 00 00 68 80 02 00 00 E8 ?? ?? ?? ?? 83 C4 10");
+		if (Resolution1ScanResult)
+		{
+			spdlog::info("Resolution 1: Address is {:s}+{:x}", sExeName.c_str(), Resolution1ScanResult - (std::uint8_t*)exeModule);
+
+			Memory::Write(Resolution1ScanResult + 6, iCurrentResX);
+
+			Memory::Write(Resolution1ScanResult + 1, iCurrentResY);
+		}
+		else
+		{
+			spdlog::error("Failed to locate resolution 1 memory address.");
+			return;
+		}
+
+		std::uint8_t* Resolution2ScanResult = Memory::PatternScan(exeModule, "68 58 02 00 00 68 20 03 00 00 EB BA 8B 15 A4 F9 54 00");
+		if (Resolution2ScanResult)
+		{
+			spdlog::info("Resolution 2: Address is {:s}+{:x}", sExeName.c_str(), Resolution2ScanResult - (std::uint8_t*)exeModule);
+
+			Memory::Write(Resolution2ScanResult + 6, iCurrentResX);
+
+			Memory::Write(Resolution2ScanResult + 1, iCurrentResY);
+		}
+		else
+		{
+			spdlog::error("Failed to locate resolution instruction 2 memory address.");
+			return;
+		}
+
+		std::uint8_t* Resolution3ScanResult = Memory::PatternScan(exeModule, "68 00 03 00 00 68 00 04 00 00 EB A5 A1 ?? ?? ?? ??");
+		if (Resolution3ScanResult)
+		{
+			spdlog::info("Resolution 3: Address is {:s}+{:x}", sExeName.c_str(), Resolution3ScanResult - (std::uint8_t*)exeModule);
+
+			Memory::Write(Resolution3ScanResult + 6, iCurrentResX);
+
+			Memory::Write(Resolution3ScanResult + 1, iCurrentResY);
+		}
+		else
+		{
+			spdlog::error("Failed to locate resolution 3 memory address.");
+			return;
+		}
+
+		std::uint8_t* Resolution4ScanResult = Memory::PatternScan(exeModule, "68 60 03 00 00 68 80 04 00 00 EB 91 8B 0D ?? ?? ?? ?? 51");
+		if (Resolution4ScanResult)
+		{
+			spdlog::info("Resolution 4: Address is {:s}+{:x}", sExeName.c_str(), Resolution4ScanResult - (std::uint8_t*)exeModule);
+
+			Memory::Write(Resolution4ScanResult + 6, iCurrentResX);
+
+			Memory::Write(Resolution4ScanResult + 1, iCurrentResY);
+		}
+		else
+		{
+			spdlog::error("Failed to locate resolution instruction 3 memory address.");
+			return;
+		}
+
+		std::uint8_t* Resolution5ScanResult = Memory::PatternScan(exeModule, "68 00 04 00 00 68 00 05 00 00 E9 79 FF FF FF 8B 15 ?? ?? ?? ?? 52");
+		if (Resolution5ScanResult)
+		{
+			spdlog::info("Resolution 5: Address is {:s}+{:x}", sExeName.c_str(), Resolution5ScanResult - (std::uint8_t*)exeModule);
+
+			Memory::Write(Resolution5ScanResult + 6, iCurrentResX);
+
+			Memory::Write(Resolution5ScanResult + 1, iCurrentResY);
+		}
+		else
+		{
+			spdlog::error("Failed to locate resolution instruction 5 memory address.");
+			return;
+		}
+
+		std::uint8_t* Resolution6ScanResult = Memory::PatternScan(exeModule, "68 00 05 00 00 68 40 06 00 00 E9 61 FF FF FF A1 ?? ?? ?? ?? 3B C5");
+		if (Resolution6ScanResult)
+		{
+			spdlog::info("Resolution 6: Address is {:s}+{:x}", sExeName.c_str(), Resolution6ScanResult - (std::uint8_t*)exeModule);
+
+			Memory::Write(Resolution6ScanResult + 6, iCurrentResX);
+
+			Memory::Write(Resolution6ScanResult + 1, iCurrentResY);
+		}
+		else
+		{
+			spdlog::error("Failed to locate resolution 6 memory address.");
+			return;
+		}
+
+		std::uint8_t* CameraHFOVInstructionScanResult = Memory::PatternScan(exeModule, "89 4E 60 8B 50 04");
+		if (CameraHFOVInstructionScanResult)
+		{
+			spdlog::info("Camera HFOV Instruction: Address is {:s}+{:x}", sExeName.c_str(), CameraHFOVInstructionScanResult - (std::uint8_t*)exeModule);
+
+			static SafetyHookMid CameraHFOVInstructionMidHook{};
+
+			static std::vector<float> vComputedHFOVs;
+
+			CameraHFOVInstructionMidHook = safetyhook::create_mid(CameraHFOVInstructionScanResult, [](SafetyHookContext& ctx)
+			{
+				// Convert the ECX register value to float
+				fCurrentCameraHFOV = std::bit_cast<float>(ctx.ecx);
+
+				// Skip processing if a similar HFOV (within tolerance) has already been computed
+				bool bHFOVAlreadyComputed = std::any_of(vComputedHFOVs.begin(), vComputedHFOVs.end(),
+					[&](float computedValue) {
+					return std::fabs(computedValue - fCurrentCameraHFOV) < tolerance;
+				});
+
+				if (bHFOVAlreadyComputed)
+				{
+					return;
+				}
+
+				if (fCurrentCameraHFOV == 1.264000058f)
+				{
+					fCurrentCameraHFOV = CalculateNewFOV(fCurrentCameraHFOV) * fFOVFactor;
+				}
+				else
+				{
+					fCurrentCameraHFOV = CalculateNewFOV(fCurrentCameraHFOV);
+				}
+
+				// Record the computed HFOV for future calls
+				vComputedHFOVs.push_back(fCurrentCameraHFOV);
+
+				// Update the ECX register with the new HFOV value
+				ctx.ecx = std::bit_cast<uintptr_t>(fCurrentCameraHFOV);
+			});
+		}
+		else
+		{
+			spdlog::info("Cannot locate the camera HFOV instruction memory address.");
+			return;
+		}
+
+		std::uint8_t* CameraVFOVInstructionScanResult = Memory::PatternScan(exeModule, "89 56 64 8B 46 04 D9 46 60");
+		if (CameraVFOVInstructionScanResult)
+		{
+			spdlog::info("Camera VFOV Instruction: Address is {:s}+{:x}", sExeName.c_str(), CameraVFOVInstructionScanResult - (std::uint8_t*)exeModule);
+
+			static SafetyHookMid CameraVFOVInstructionMidHook{};
+
+			static std::vector<float> vComputedVFOVs;
+
+			CameraVFOVInstructionMidHook = safetyhook::create_mid(CameraVFOVInstructionScanResult, [](SafetyHookContext& ctx)
+			{
+				// Convert the EDX register value to float
+				float fCurrentCameraVFOV = std::bit_cast<float>(ctx.edx);
+
+				// Skip processing if a similar VFOV (within tolerance) has already been computed
+				bool bVFOVAlreadyComputed = std::any_of(vComputedVFOVs.begin(), vComputedVFOVs.end(),
+					[&](float computedValue) {
+					return std::fabs(computedValue - fCurrentCameraVFOV) < tolerance;
+				});
+
+				if (bVFOVAlreadyComputed)
+				{
+					return;
+				}
+
+				// Compute the new VFOV value
+				fCurrentCameraVFOV = fCurrentCameraHFOV / fNewAspectRatio;
+
+				// Record the computed VFOV for future calls
+				vComputedVFOVs.push_back(fCurrentCameraVFOV);
+
+				// Update the EDX register with the new VFOV value
+				ctx.edx = std::bit_cast<uintptr_t>(fCurrentCameraVFOV);
+			});
+		}
+		else
+		{
+			spdlog::info("Cannot locate the camera VFOV instruction memory address.");
+			return;
+		}
+	}
+}
+
+DWORD __stdcall Main(void*)
+{
+	Logging();
+	Configuration();
+	if (DetectGame())
+	{
+		WidescreenFix();
+	}
+	return TRUE;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	{
+		thisModule = hModule;
+		HANDLE mainHandle = CreateThread(NULL, 0, Main, 0, NULL, 0);
+		if (mainHandle)
+		{
+			SetThreadPriority(mainHandle, THREAD_PRIORITY_HIGHEST);
+			CloseHandle(mainHandle);
+		}
+		break;
+	}
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
