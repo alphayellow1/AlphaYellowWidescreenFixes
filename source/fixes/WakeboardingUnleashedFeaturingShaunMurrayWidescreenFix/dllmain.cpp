7f7793b19b7e9ca3cd21340188e7beec9d96f412
AlphaYellow <diogoccsantos@gmail.com>
2025-04-20 19:35:37 +0100

    new file:   project/AceLightningFOVFix.vcxproj 	new file:   project/AceLightningFOVFix.vcxproj.filters 	modified:   project/AlphaYellowWidescreenFixes.sln     new file:   project/WakeboardingUnleashedFeaturingShaunMurrayWidescreenFix.vcxproj 	new file:   project/WakeboardingUnleashedFeaturingShaunMurrayWidescreenFix.vcxproj.filters 	new file:   source/fixes/AceLightningFOVFix/dllmain.cpp 	modified:   source/fixes/HarleyDavidsonRaceToTheRallyFOVFix/dllmain.cpp 	new file:   source/fixes/WakeboardingUnleashedFeaturingShaunMurrayWidescreenFix/dllmain.cpp

---
+++ source/fixes/WakeboardingUnleashedFeaturingShaunMurrayWidescreenFix/dllmain.cpp
@@ -0,0 +1,381 @@
+// Include necessary headers
+#include "stdafx.h"
+#include "helper.hpp"
+
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <inipp/inipp.h>
+#include <safetyhook.hpp>
+#include <vector>
+#include <map>
+#include <windows.h>
+#include <psapi.h> // For GetModuleInformation
+#include <fstream>
+#include <filesystem>
+#include <sstream>
+#include <cstring>
+#include <iomanip>
+#include <cstdint>
+#include <iostream>
+
+#define spdlog_confparse(var) spdlog::info("Config Parse: {}: {}", #var, var)
+
+HMODULE exeModule = GetModuleHandle(NULL);
+HMODULE dllModule = nullptr;
+HMODULE dllModule2 = nullptr;
+HMODULE thisModule;
+
+// Fix details
+std::string sFixName = "WakeboardingUnleashedFeaturingShaunMurrayWidescreenFix";
+std::string sFixVersion = "1.0";
+std::filesystem::path sFixPath;
+
+// Ini
+inipp::Ini<char> ini;
+std::string sConfigFile = sFixName + ".ini";
+
+// Logger
+std::shared_ptr<spdlog::logger> logger;
+std::string sLogFile = sFixName + ".log";
+std::filesystem::path sExePath;
+std::string sExeName;
+
+// Constants
+constexpr float fPi = 3.14159265358979323846f;
+constexpr float fOriginalCameraFOV = 65.0f;
+constexpr float fOldAspectRatio = 4.0f / 3.0f;
+
+// Ini variables
+bool bFixActive;
+int iCurrentResX;
+int iCurrentResY;
+
+// Variables
+float fNewCameraFOV;
+float fFOVFactor;
+float fNewAspectRatio;
+
+// Function to convert degrees to radians
+float DegToRad(float degrees)
+{
+	return degrees * (fPi / 180.0f);
+}
+
+// Function to convert radians to degrees
+float RadToDeg(float radians)
+{
+	return radians * (180.0f / fPi);
+}
+
+// Game detection
+enum class Game
+{
+	WUFSM,
+	Unknown
+};
+
+struct GameInfo
+{
+	std::string GameTitle;
+	std::string ExeName;
+};
+
+const std::map<Game, GameInfo> kGames = {
+	{Game::WUFSM, {"Wakeboarding Unleashed featuring Shaun Murray", "WBU.exe"}},
+};
+
+const GameInfo* game = nullptr;
+Game eGameType = Game::Unknown;
+
+void Logging()
+{
+	// Get path to DLL
+	WCHAR dllPath[_MAX_PATH] = { 0 };
+	GetModuleFileNameW(thisModule, dllPath, MAX_PATH);
+	sFixPath = dllPath;
+	sFixPath = sFixPath.remove_filename();
+
+	// Get game name and exe path
+	WCHAR exePathW[_MAX_PATH] = { 0 };
+	GetModuleFileNameW(dllModule, exePathW, MAX_PATH);
+	sExePath = exePathW;
+	sExeName = sExePath.filename().string();
+	sExePath = sExePath.remove_filename();
+
+	// Spdlog initialization
+	try
+	{
+		logger = spdlog::basic_logger_st(sFixName.c_str(), sExePath.string() + "\\" + sLogFile, true);
+		spdlog::set_default_logger(logger);
+		spdlog::flush_on(spdlog::level::debug);
+		spdlog::set_level(spdlog::level::debug); // Enable debug level logging
+
+		spdlog::info("----------");
+		spdlog::info("{:s} v{:s} loaded.", sFixName.c_str(), sFixVersion.c_str());
+		spdlog::info("----------");
+		spdlog::info("Log file: {}", sExePath.string() + "\\" + sLogFile);
+		spdlog::info("----------");
+		spdlog::info("Module Name: {0:s}", sExeName.c_str());
+		spdlog::info("Module Path: {0:s}", sExePath.string());
+		spdlog::info("Module Address: 0x{0:X}", (uintptr_t)dllModule);
+		spdlog::info("----------");
+		spdlog::info("DLL has been successfully loaded.");
+	}
+	catch (const spdlog::spdlog_ex& ex)
+	{
+		AllocConsole();
+		FILE* dummy;
+		freopen_s(&dummy, "CONOUT$", "w", stdout);
+		std::cout << "Log initialization failed: " << ex.what() << std::endl;
+		FreeLibraryAndExitThread(thisModule, 1);
+	}
+}
+
+void Configuration()
+{
+	// Inipp initialization
+	std::ifstream iniFile(sFixPath.string() + "\\" + sConfigFile);
+	if (!iniFile)
+	{
+		AllocConsole();
+		FILE* dummy;
+		freopen_s(&dummy, "CONOUT$", "w", stdout);
+		std::cout << sFixName.c_str() << " v" << sFixVersion.c_str() << " loaded." << std::endl;
+		std::cout << "ERROR: Could not locate config file." << std::endl;
+		std::cout << "ERROR: Make sure " << sConfigFile.c_str() << " is located in " << sFixPath.string().c_str() << std::endl;
+		spdlog::shutdown();
+		FreeLibraryAndExitThread(thisModule, 1);
+	}
+	else
+	{
+		spdlog::info("Config file: {}", sFixPath.string() + "\\" + sConfigFile);
+		ini.parse(iniFile);
+	}
+
+	// Parse config
+	ini.strip_trailing_comments();
+	spdlog::info("----------");
+
+	// Load settings from ini
+	inipp::get_value(ini.sections["WidescreenFix"], "Enabled", bFixActive);
+	spdlog_confparse(bFixActive);
+
+	// Load resolution from ini
+	inipp::get_value(ini.sections["Settings"], "Width", iCurrentResX);
+	inipp::get_value(ini.sections["Settings"], "Height", iCurrentResY);
+	inipp::get_value(ini.sections["Settings"], "FOVFactor", fFOVFactor);
+	spdlog_confparse(iCurrentResX);
+	spdlog_confparse(iCurrentResY);
+	spdlog_confparse(fFOVFactor);
+
+	// If resolution not specified, use desktop resolution
+	if (iCurrentResX <= 0 || iCurrentResY <= 0)
+	{
+		spdlog::info("Resolution not specified in ini file. Using desktop resolution.");
+		// Implement Util::GetPhysicalDesktopDimensions() accordingly
+		auto desktopDimensions = Util::GetPhysicalDesktopDimensions();
+		iCurrentResX = desktopDimensions.first;
+		iCurrentResY = desktopDimensions.second;
+		spdlog_confparse(iCurrentResX);
+		spdlog_confparse(iCurrentResY);
+	}
+
+	spdlog::info("----------");
+}
+
+bool DetectGame()
+{
+	for (const auto& [type, info] : kGames)
+	{
+		if (Util::stringcmp_caseless(info.ExeName, sExeName))
+		{
+			spdlog::info("Detected game: {:s} ({:s})", info.GameTitle, sExeName);
+			spdlog::info("----------");
+			eGameType = type;
+			game = &info;
+			return true;
+		}
+	}
+
+	spdlog::error("Failed to detect supported game, {:s} isn't supported by the fix.", sExeName);
+	return false;
+}
+
+void WidescreenFix()
+{
+	if (eGameType == Game::WUFSM && bFixActive == true)
+	{
+		fNewAspectRatio = static_cast<float>(iCurrentResX) / static_cast<float>(iCurrentResY);
+
+		std::uint8_t* ResolutionList1ScanResult = Memory::PatternScan(exeModule, "80 02 00 00 20 03 00 00 00 04 00 00 00 05 00 00 E0 01 00 00 58 02 00 00 00 03 00 00 00 04 00 00 66 66");
+		if (ResolutionList1ScanResult)
+		{
+			spdlog::info("Resolution List 1 Scan: Address is {:s}+{:x}", sExeName.c_str(), ResolutionList1ScanResult - (std::uint8_t*)exeModule);
+			
+			Memory::Write(ResolutionList1ScanResult, iCurrentResX);
+
+			Memory::Write(ResolutionList1ScanResult + 4, iCurrentResX);
+
+			Memory::Write(ResolutionList1ScanResult + 8, iCurrentResX);
+
+			Memory::Write(ResolutionList1ScanResult + 12, iCurrentResX);
+
+			Memory::Write(ResolutionList1ScanResult + 16, iCurrentResY);
+
+			Memory::Write(ResolutionList1ScanResult + 20, iCurrentResY);
+
+			Memory::Write(ResolutionList1ScanResult + 24, iCurrentResY);
+
+			Memory::Write(ResolutionList1ScanResult + 28, iCurrentResY);
+		}
+		else
+		{
+			spdlog::error("Failed to locate resolution list 1 scan memory address.");
+			return;
+		}
+
+		std::uint8_t* ResolutionList2ScanResult = Memory::PatternScan(exeModule, "80 02 00 00 20 03 00 00 00 04 00 00 00 05 00 00 E0 01 00 00 58 02 00 00 00 03 00 00 00 04 00 00 80 02 00 00 20 03 00 00 00 04 00 00 00 05 00 00 E0 01 00 00 58 02 00 00 00 03 00 00 00 04 00 00 80 02 00 00 20 03 00 00 00 04 00 00 00 05 00 00 E0 01 00 00 58 02 00 00 00 03 00 00 00 04 00 00");
+		if (ResolutionList2ScanResult)
+		{
+			spdlog::info("Resolution List 2 Scan: Address is {:s}+{:x}", sExeName.c_str(), ResolutionList2ScanResult - (std::uint8_t*)exeModule);
+
+			Memory::Write(ResolutionList2ScanResult, iCurrentResX);
+
+			Memory::Write(ResolutionList2ScanResult + 4, iCurrentResX);
+
+			Memory::Write(ResolutionList2ScanResult + 8, iCurrentResX);
+
+			Memory::Write(ResolutionList2ScanResult + 12, iCurrentResX);
+
+			Memory::Write(ResolutionList2ScanResult + 16, iCurrentResY);
+
+			Memory::Write(ResolutionList2ScanResult + 20, iCurrentResY);
+
+			Memory::Write(ResolutionList2ScanResult + 24, iCurrentResY);
+
+			Memory::Write(ResolutionList2ScanResult + 28, iCurrentResY);
+
+			Memory::Write(ResolutionList2ScanResult + 32, iCurrentResX);
+
+			Memory::Write(ResolutionList2ScanResult + 36, iCurrentResX);
+
+			Memory::Write(ResolutionList2ScanResult + 40, iCurrentResX);
+
+			Memory::Write(ResolutionList2ScanResult + 44, iCurrentResX);
+
+			Memory::Write(ResolutionList2ScanResult + 48, iCurrentResY);
+
+			Memory::Write(ResolutionList2ScanResult + 52, iCurrentResY);
+
+			Memory::Write(ResolutionList2ScanResult + 56, iCurrentResY);
+
+			Memory::Write(ResolutionList2ScanResult + 60, iCurrentResY);
+
+			Memory::Write(ResolutionList2ScanResult + 64, iCurrentResX);
+
+			Memory::Write(ResolutionList2ScanResult + 68, iCurrentResX);
+
+			Memory::Write(ResolutionList2ScanResult + 72, iCurrentResX);
+
+			Memory::Write(ResolutionList2ScanResult + 76, iCurrentResX);
+
+			Memory::Write(ResolutionList2ScanResult + 80, iCurrentResY);
+
+			Memory::Write(ResolutionList2ScanResult + 84, iCurrentResY);
+
+			Memory::Write(ResolutionList2ScanResult + 88, iCurrentResY);
+
+			Memory::Write(ResolutionList2ScanResult + 92, iCurrentResY);
+		}
+		else
+		{
+			spdlog::error("Failed to locate resolution list 2 scan memory address.");
+			return;
+		}
+
+		std::uint8_t* HUDAspectRatioScanResult = Memory::PatternScan(exeModule, "93 A9 AA 3F 52 EB 16 A1 40 3F 89 00 8B 48 48 8B");
+		if (HUDAspectRatioScanResult)
+		{
+			spdlog::info("HUD Aspect Ratio: Address is {:s}+{:x}", sExeName.c_str(), HUDAspectRatioScanResult - (std::uint8_t*)exeModule);
+
+			Memory::Write(HUDAspectRatioScanResult, fNewAspectRatio);
+		}
+		else
+		{
+			spdlog::error("Failed to locate HUD aspect ratio memory address.");
+			return;
+		}
+
+		std::uint8_t* MainMenuAndGameplayAspectRatioScanResult = Memory::PatternScan(exeModule, "68 93 A9 AA 3F 51 EB 0D 8B 48 44 8B 50 40 51 68 93 A9 AA 3F 52 E8 59 05 03 00 88 1D F5 24 68 00 8B 87 BC 3F 00 00 C1 E8 06 83 C4");
+		if (MainMenuAndGameplayAspectRatioScanResult)
+		{
+			spdlog::info("Main Menu & Gameplay Aspect Ratio: Address is {:s}+{:x}", sExeName.c_str(), MainMenuAndGameplayAspectRatioScanResult - (std::uint8_t*)exeModule);
+
+			Memory::Write(MainMenuAndGameplayAspectRatioScanResult + 1, fNewAspectRatio);
+
+			Memory::Write(MainMenuAndGameplayAspectRatioScanResult + 16, fNewAspectRatio);
+		}
+		else
+		{
+			spdlog::error("Failed to locate main menu and gameplay aspect ratio memory address.");
+			return;
+		}
+
+		std::uint8_t* CameraFOVInstructionScanResult = Memory::PatternScan(exeModule, "8B 48 40 89 0D ?? ?? ?? ?? D9 86 44 19 00 00 D9 E0 D9 9E 44 19 00 00");
+		if (CameraFOVInstructionScanResult)
+		{
+			spdlog::info("Camera FOV Instruction: Address is {:s}+{:x}", sExeName.c_str(), CameraFOVInstructionScanResult - (std::uint8_t*)exeModule);
+
+			static SafetyHookMid CameraFOVInstructionMidHook{};
+
+			CameraFOVInstructionMidHook = safetyhook::create_mid(CameraFOVInstructionScanResult, [](SafetyHookContext& ctx)
+			{
+				float& fCurrentCameraFOV = *reinterpret_cast<float*>(ctx.eax + 0x40);
+
+				if (fCurrentCameraFOV == 67.5f)
+				{
+					fCurrentCameraFOV *= fFOVFactor;
+				}
+			});
+		}
+		else
+		{
+			spdlog::error("Failed to locate camera FOV instruction memory address.");
+			return;
+		}
+	}
+}
+
+DWORD __stdcall Main(void*)
+{
+	Logging();
+	Configuration();
+	if (DetectGame())
+	{
+		WidescreenFix();
+	}
+	return TRUE;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	{
+		thisModule = hModule;
+		HANDLE mainHandle = CreateThread(NULL, 0, Main, 0, NULL, 0);
+		if (mainHandle)
+		{
+			SetThreadPriority(mainHandle, THREAD_PRIORITY_HIGHEST);
+			CloseHandle(mainHandle);
+		}
+		break;
+	}
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
