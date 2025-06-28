955635109f68a5693894ba01b2937471c1e4c975
AlphaYellow <diogoccsantos@gmail.com>
2025-06-28 20:56:37 +0100

    modified:   source/fixes/CrisisTeamAmbulanceDriverFOVFix/dllmain.cpp 	modified:   source/fixes/Croc2FOVFix/dllmain.cpp

--- source/fixes/CrisisTeamAmbulanceDriverFOVFix/dllmain.cpp
+++ source/fixes/CrisisTeamAmbulanceDriverFOVFix/dllmain.cpp
@@ -215,7 +215,7 @@ void FOVFix()
 			
 			static SafetyHookMid CameraHFOVInstructionMidHook{};
 
-			CameraHFOVInstructionMidHook = safetyhook::create_mid(CameraHFOVInstructionScanResult + 6, [](SafetyHookContext& ctx)
+			CameraHFOVInstructionMidHook = safetyhook::create_mid(CameraHFOVInstructionScanResult, [](SafetyHookContext& ctx)
 			{
 				float& fCurrentCameraHFOV = *reinterpret_cast<float*>(ctx.eax + 0x20C);
 
@@ -239,7 +239,7 @@ void FOVFix()
 
 			static SafetyHookMid CameraVFOVInstructionMidHook{};
 
-			CameraVFOVInstructionMidHook = safetyhook::create_mid(CameraVFOVInstructionScanResult + 6, [](SafetyHookContext& ctx)
+			CameraVFOVInstructionMidHook = safetyhook::create_mid(CameraVFOVInstructionScanResult, [](SafetyHookContext& ctx)
 			{
 				float& fCurrentCameraVFOV = *reinterpret_cast<float*>(ctx.eax + 0x210);
 
