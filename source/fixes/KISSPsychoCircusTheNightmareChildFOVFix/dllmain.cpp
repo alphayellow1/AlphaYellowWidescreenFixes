a7006bfce7ca3e86df4c3144789ea42d6a6bb7c4
AlphaYellow <diogoccsantos@gmail.com>
2025-06-28 10:52:31 +0100

    modified:   source/fixes/AngelsVsDevilsFOVFix/dllmain.cpp 	modified:   source/fixes/AnneMcCaffreysFreedomFirstResistanceFOVFix/dllmain.cpp 	modified:   source/fixes/KISSPsychoCircusTheNightmareChildFOVFix/dllmain.cpp 	modified:   source/fixes/SanityAikensArtifactFOVFix/dllmain.cpp

--- source/fixes/KISSPsychoCircusTheNightmareChildFOVFix/dllmain.cpp
+++ source/fixes/KISSPsychoCircusTheNightmareChildFOVFix/dllmain.cpp
@@ -292,7 +292,7 @@ void FOVFix()
 			
 			static SafetyHookMid CameraHFOVInstructionMidHook{};
 
-			CameraHFOVInstructionMidHook = safetyhook::create_mid(CameraHFOVInstructionScanResult + 6, [](SafetyHookContext& ctx)
+			CameraHFOVInstructionMidHook = safetyhook::create_mid(CameraHFOVInstructionScanResult, [](SafetyHookContext& ctx)
 			{
 				// Access the HFOV value at the memory address EAX + 0x150
 				float& fCurrentCameraHFOV = *reinterpret_cast<float*>(ctx.eax + 0x150);
@@ -346,7 +346,7 @@ void FOVFix()
 			
 			static SafetyHookMid CameraVFOVInstructionMidHook{};
 
-			CameraVFOVInstructionMidHook = safetyhook::create_mid(CameraVFOVInstructionScanResult + 6, [](SafetyHookContext& ctx)
+			CameraVFOVInstructionMidHook = safetyhook::create_mid(CameraVFOVInstructionScanResult, [](SafetyHookContext& ctx)
 			{
 				// Access the VFOV value at the memory address EAX + 0x154
 				float& fCurrentCameraVFOV = *reinterpret_cast<float*>(ctx.eax + 0x154);
