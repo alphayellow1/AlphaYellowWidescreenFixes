d85e433fbf26d00b0ccc57731a8463de1a5ee4c7
AlphaYellow <diogoccsantos@gmail.com>
2025-12-22 12:26:25 +0000

    modified:   project/AlphaYellowWidescreenFixes.sln 	new file:   project/NosferatuTheWrathOfMalachiWidescreenFix.vcxproj 	new file:   project/NosferatuTheWrathOfMalachiWidescreenFix.vcxproj.filters 	new file:   source/fixes/NosferatuTheWrathOfMalachiWidescreenFix/dllmain.cpp

---
+++ source/fixes/NosferatuTheWrathOfMalachiWidescreenFix/dllmain.cpp
@@ -0,0 +1,334 @@
+// Include necessary headers
+#include "stdafx.h"
+#include "helper.hpp"
+
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <inipp/inipp.h>
+#include <safetyhook.hpp>
+#include <vector>
+#include <map>
+#include <windows.h>
+#include <psapi.h> // For GetModuleInformation
+#include <fstream>
+#include <filesystem>
+#include <sstream>
+#include <cstring>
+#include <iomanip>
+#include <cstdint>
+#include <iostream>
+
+#define spdlog_confparse(var) spdlog::info("Config Parse: {}: {}", #var, var)
+
+HMODULE exeModule = GetModuleHandle(NULL);
+HMODULE thisModule;
+
+// Fix details
+std::string sFixName = "NosferatuTheWrathOfMalachiWidescreenFix";
+std::string sFixVersion = "1.0";
+std::filesystem::path sFixPath;
+
+// Ini
+inipp::Ini<char> ini;
+std::string sConfigFile = sFixName + ".ini";
+
+// Logger
+std::shared_ptr<spdlog::logger> logger;
+std::string sLogFile = sFixName + ".log";
+std::filesystem::path sExePath;
+std::string sExeName;
+
+// Ini variables
+bool bFixActive;
+int iCurrentResX;
+int iCurrentResY;
+float fFOVFactor;
+
+// Constants
+constexpr float fOldAspectRatio = 4.0f / 3.0f;
+
+// Variables
+float fNewAspectRatio;
+float fAspectRatioScale;
+float fNewCameraFOV1;
+float fNewCameraFOV2;
+
+// Game detection
+enum class Game
+{
+	NTWOM,
+	Unknown
+};
+
+enum ResolutionInstructionsIndices
+{
+	ResolutionWidthsListScan,
+	ResolutionHeightsListScan
+};
+
+enum CameraFOVInstructionsIndices
+{
+	CameraFOV1Scan,
+	CameraFOV2Scan
+};
+
+struct GameInfo
+{
+	std::string GameTitle;
+	std::string ExeName;
+};
+
+const std::map<Game, GameInfo> kGames = {
+	{Game::NTWOM, {"Nosferatu: The Wrath of Malachi", "Nosferatu.exe"}},
+};
+
+const GameInfo* game = nullptr;
+Game eGameType = Game::Unknown;
+
+void Logging()
+{
+	// Get path to DLL
+	WCHAR dllPath[_MAX_PATH] = { 0 };
+	GetModuleFileNameW(thisModule, dllPath, MAX_PATH);
+	sFixPath = dllPath;
+	sFixPath = sFixPath.remove_filename();
+
+	// Get game name and exe path
+	WCHAR exePathW[_MAX_PATH] = { 0 };
+	GetModuleFileNameW(exeModule, exePathW, MAX_PATH);
+	sExePath = exePathW;
+	sExeName = sExePath.filename().string();
+	sExePath = sExePath.remove_filename();
+
+	// Spdlog initialization
+	try
+	{
+		logger = spdlog::basic_logger_st(sFixName.c_str(), sExePath.string() + "\\" + sLogFile, true);
+		spdlog::set_default_logger(logger);
+		spdlog::flush_on(spdlog::level::debug);
+		spdlog::set_level(spdlog::level::debug); // Enable debug level logging
+
+		spdlog::info("----------");
+		spdlog::info("{:s} v{:s} loaded.", sFixName.c_str(), sFixVersion.c_str());
+		spdlog::info("----------");
+		spdlog::info("Log file: {}", sExePath.string() + "\\" + sLogFile);
+		spdlog::info("----------");
+		spdlog::info("Module Name: {0:s}", sExeName.c_str());
+		spdlog::info("Module Path: {0:s}", sExePath.string());
+		spdlog::info("Module Address: 0x{0:X}", (uintptr_t)exeModule);
+		spdlog::info("----------");
+		spdlog::info("DLL has been successfully loaded.");
+	}
+	catch (const spdlog::spdlog_ex& ex)
+	{
+		AllocConsole();
+		FILE* dummy;
+		freopen_s(&dummy, "CONOUT$", "w", stdout);
+		std::cout << "Log initialization failed: " << ex.what() << std::endl;
+		FreeLibraryAndExitThread(thisModule, 1);
+	}
+}
+
+void Configuration()
+{
+	// Inipp initialization
+	std::ifstream iniFile(sFixPath.string() + "\\" + sConfigFile);
+	if (!iniFile)
+	{
+		AllocConsole();
+		FILE* dummy;
+		freopen_s(&dummy, "CONOUT$", "w", stdout);
+		std::cout << sFixName.c_str() << " v" << sFixVersion.c_str() << " loaded." << std::endl;
+		std::cout << "ERROR: Could not locate config file." << std::endl;
+		std::cout << "ERROR: Make sure " << sConfigFile.c_str() << " is located in " << sFixPath.string().c_str() << std::endl;
+		spdlog::shutdown();
+		FreeLibraryAndExitThread(thisModule, 1);
+	}
+	else
+	{
+		spdlog::info("Config file: {}", sFixPath.string() + "\\" + sConfigFile);
+		ini.parse(iniFile);
+	}
+
+	// Parse config
+	ini.strip_trailing_comments();
+	spdlog::info("----------");
+
+	// Load settings from ini
+	inipp::get_value(ini.sections["WidescreenFix"], "Enabled", bFixActive);
+	spdlog_confparse(bFixActive);
+
+	// Load resolution from ini
+	inipp::get_value(ini.sections["Settings"], "Width", iCurrentResX);
+	inipp::get_value(ini.sections["Settings"], "Height", iCurrentResY);
+	inipp::get_value(ini.sections["Settings"], "FOVFactor", fFOVFactor);
+	spdlog_confparse(iCurrentResX);
+	spdlog_confparse(iCurrentResY);
+	spdlog_confparse(fFOVFactor);
+
+	// If resolution not specified, use desktop resolution
+	if (iCurrentResX <= 0 || iCurrentResY <= 0)
+	{
+		spdlog::info("Resolution not specified in ini file. Using desktop resolution.");
+		// Implement Util::GetPhysicalDesktopDimensions() accordingly
+		auto desktopDimensions = Util::GetPhysicalDesktopDimensions();
+		iCurrentResX = desktopDimensions.first;
+		iCurrentResY = desktopDimensions.second;
+		spdlog_confparse(iCurrentResX);
+		spdlog_confparse(iCurrentResY);
+	}
+
+	spdlog::info("----------");
+}
+
+bool DetectGame()
+{
+	for (const auto& [type, info] : kGames)
+	{
+		if (Util::stringcmp_caseless(info.ExeName, sExeName))
+		{
+			spdlog::info("Detected game: {:s} ({:s})", info.GameTitle, sExeName);
+			spdlog::info("----------");
+			eGameType = type;
+			game = &info;
+			return true;
+		}
+	}
+
+	spdlog::error("Failed to detect supported game, {:s} isn't supported by the fix.", sExeName);
+	return false;
+}
+
+static SafetyHookMid AspectRatioInstructionHook{};
+static SafetyHookMid CameraFOVInstruction1Hook{};
+static SafetyHookMid CameraFOVInstruction2Hook{};
+
+void WidescreenFix()
+{
+	if (bFixActive == true && eGameType == Game::NTWOM)
+	{
+		fNewAspectRatio = static_cast<float>(iCurrentResX) / static_cast<float>(iCurrentResY);
+
+		fAspectRatioScale = fNewAspectRatio / fOldAspectRatio;
+
+		std::vector<std::uint8_t*> ResolutionInstructionsScansResult = Memory::PatternScan(exeModule, "68 ?? ?? ?? ?? 52 E8 ?? ?? ?? ?? 66 A1 ?? ?? ?? ?? 8D 4C 24 ?? 8D 94 24 ?? ?? ?? ?? 51 52 C6 84 24 ?? ?? ?? ?? ?? 66 89 44 24 ?? E8 ?? ?? ?? ?? 83 C4 ?? 8B F0 8D 44 24 ?? 8D 4C 24 ?? 50 E8 ?? ?? ?? ?? 3B F5 C6 84 24 ?? ?? ?? ?? ?? 74 ?? 8B FE 83 C9 ?? 33 C0 F2 ?? F7 D1 49 51 56 8D 4C 24 ?? E8 ?? ?? ?? ?? 8B 54 24 ?? 8D 4C 24 ?? 51 52 8D 4C 24 ?? E8 ?? ?? ?? ?? 8D 44 24 ?? 50 55 E8 ?? ?? ?? ?? 8B F0 83 C4 ?? 3B F5 75 ?? 6A ?? 8D 4C 24 ?? 88 9C 24 ?? ?? ?? ?? E8 ?? ?? ?? ?? 8D 4C 24 ?? C6 84 24 ?? ?? ?? ?? ?? 51 8D 8C 24 ?? ?? ?? ?? E8 ?? ?? ?? ?? 8D 4C 24 ?? C6 84 24 ?? ?? ?? ?? ?? E8 ?? ?? ?? ?? 6A ?? 8D 4C 24 ?? C6 84 24 ?? ?? ?? ?? ?? E8 ?? ?? ?? ?? 8D 54 24 ?? 8D 4C 24 ?? 52 E8 ?? ?? ?? ?? 8D 44 24 ?? 8D 4C 24 ?? 50 C6 84 24 ?? ?? ?? ?? ?? E8 ?? ?? ?? ?? BF ?? ?? ?? ?? 83 C9 ?? 33 C0 B3 ?? F2 ?? F7 D1 49 88 9C 24 ?? ?? ?? ?? 51 68 ?? ?? ?? ?? 8D 4C 24 ?? E8 ?? ?? ?? ?? 89 6C 24 ?? BE ?? ?? ?? ?? B8 ?? ?? ?? ?? 8A 10 8A CA 3A 16 75 ?? 84 C9 74 ?? 8A 50 ?? 8A CA 3A 56 ?? 75 ?? 83 C0 ?? 83 C6 ?? 84 C9 75 ?? 33 C0 EB ?? 1B C0 83 D8 ?? 33 C9 3B C5 0F 95 ?? 8B F9 BE ?? ?? ?? ?? 89 7C 24 ?? B8 ?? ?? ?? ?? 8A 10 8A CA 3A 16 75 ?? 84 C9 74 ?? 8A 50 ?? 8A CA 3A 56 ?? 75 ?? 83 C0 ?? 83 C6 ?? 84 C9 75 ?? 33 C0 EB ?? 1B C0 83 D8 ?? 33 C9 3B C5 0F 95 ?? 8B C1 83 FF ?? 89 44 24 ?? 0F 85 ?? ?? ?? ?? 3B C7 0F 85 ?? ?? ?? ?? 68 ?? ?? ?? ?? 8D 94 24 ?? ?? ?? ?? 68 ?? ?? ?? ?? 52 E8 ?? ?? ?? ?? 66 A1 ?? ?? ?? ?? 8D 4C 24 ?? 8D 94 24 ?? ?? ?? ?? 51 52 C6 84 24 ?? ?? ?? ?? ?? 66 89 44 24 ?? E8 ?? ?? ?? ?? 83 C4 ?? 8B F0 8D 44 24 ?? 8D 4C 24 ?? 50 E8 ?? ?? ?? ?? 3B F5 C6 84 24 ?? ?? ?? ?? ?? 74 ?? 8B FE 83 C9 ?? 33 C0 F2 ?? F7 D1 49 51 56 8D 4C 24 ?? E8 ?? ?? ?? ?? 8B 54 24 ?? 8D 4C 24 ?? 51 52 8D 4C 24 ?? E8 ?? ?? ?? ?? 8D 44 24 ?? 50 55 E8 ?? ?? ?? ?? 8B F0 83 C4 ?? 3B F5 75 ?? 6A ?? 8D 4C 24 ?? 88 9C 24 ?? ?? ?? ?? E8 ?? ?? ?? ?? 8D 4C 24 ?? C6 84 24 ?? ?? ?? ?? ?? 51 8D 8C 24 ?? ?? ?? ?? E8 ?? ?? ?? ?? 8D 4C 24 ?? C6 84 24 ?? ?? ?? ?? ?? E8 ?? ?? ?? ?? 6A ?? 8D 4C 24 ?? C6 84 24 ?? ?? ?? ?? ?? E8 ?? ?? ?? ?? 8D 54 24 ?? 8D 4C 24 ?? 52 E8 ?? ?? ?? ?? 8D 44 24 ?? 8D 4C 24 ?? 50 C6 84 24 ?? ?? ?? ?? ?? E8 ?? ?? ?? ?? BF ?? ?? ?? ?? 83 C9 ?? 33 C0 B3 ?? F2 ?? F7 D1 49 88 9C 24 ?? ?? ?? ?? 51 68 ?? ?? ?? ?? 8D 4C 24 ?? E8 ?? ?? ?? ?? C7 44 24", "68 ?? ?? ?? ?? 52 E8 ?? ?? ?? ?? 66 A1 ?? ?? ?? ?? 8D 4C 24 ?? 8D 94 24 ?? ?? ?? ?? 51 52 C6 84 24 ?? ?? ?? ?? ?? 66 89 44 24 ?? E8 ?? ?? ?? ?? 83 C4 ?? 8B F0 8D 44 24 ?? 8D 4C 24 ?? 50 E8 ?? ?? ?? ?? 3B F5 C6 84 24 ?? ?? ?? ?? ?? 74 ?? 8B FE 83 C9 ?? 33 C0 F2 ?? F7 D1 49 51 56 8D 4C 24 ?? E8 ?? ?? ?? ?? 8B 54 24 ?? 8D 4C 24 ?? 51 52 8D 4C 24 ?? E8 ?? ?? ?? ?? 8D 44 24 ?? 50 55 E8 ?? ?? ?? ?? 8B F0 83 C4 ?? 3B F5 75 ?? 6A ?? 8D 4C 24 ?? 88 9C 24 ?? ?? ?? ?? E8 ?? ?? ?? ?? 8D 4C 24 ?? C6 84 24 ?? ?? ?? ?? ?? 51 8D 8C 24 ?? ?? ?? ?? E8 ?? ?? ?? ?? 8D 4C 24 ?? C6 84 24 ?? ?? ?? ?? ?? E8 ?? ?? ?? ?? 6A ?? 8D 4C 24 ?? C6 84 24 ?? ?? ?? ?? ?? E8 ?? ?? ?? ?? 8D 54 24 ?? 8D 4C 24 ?? 52 E8 ?? ?? ?? ?? 8D 44 24 ?? 8D 4C 24 ?? 50 C6 84 24 ?? ?? ?? ?? ?? E8 ?? ?? ?? ?? BF ?? ?? ?? ?? 83 C9 ?? 33 C0 B3 ?? F2 ?? F7 D1 49 88 9C 24 ?? ?? ?? ?? 51 68 ?? ?? ?? ?? 8D 4C 24 ?? E8 ?? ?? ?? ?? C7 44 24 ?? ?? ?? ?? ?? BE ?? ?? ?? ?? B8 ?? ?? ?? ?? 8A 10 8A CA 3A 16 75 ?? 84 C9 74 ?? 8A 50 ?? 8A CA 3A 56 ?? 75 ?? 83 C0 ?? 83 C6 ?? 84 C9 75 ?? 33 C0 EB ?? 1B C0 83 D8 ?? 33 C9 3B C5 0F 95 ?? 8B F9 BE ?? ?? ?? ?? 89 7C 24 ?? B8 ?? ?? ?? ?? 8A 10 8A CA 3A 16 75 ?? 84 C9 74 ?? 8A 50 ?? 8A CA 3A 56 ?? 75 ?? 83 C0 ?? 83 C6 ?? 84 C9 75 ?? 33 C0 EB ?? 1B C0 83 D8 ?? 33 C9 3B C5 0F 95 ?? 8B C1 83 FF ?? 89 44 24 ?? 0F 85 ?? ?? ?? ?? 3B C7 0F 85 ?? ?? ?? ?? 68 ?? ?? ?? ?? 8D 94 24 ?? ?? ?? ?? 68 ?? ?? ?? ?? 52 E8 ?? ?? ?? ?? 66 A1 ?? ?? ?? ?? 8D 4C 24 ?? 8D 94 24 ?? ?? ?? ?? 51 52 C6 84 24 ?? ?? ?? ?? ?? 66 89 44 24 ?? E8 ?? ?? ?? ?? 83 C4 ?? 8B F0 8D 44 24 ?? 8D 4C 24 ?? 50 E8 ?? ?? ?? ?? 3B F5 C6 84 24 ?? ?? ?? ?? ?? 74 ?? 8B FE 83 C9 ?? 33 C0 F2 ?? F7 D1 49 51 56 8D 4C 24 ?? E8 ?? ?? ?? ?? 8B 54 24 ?? 8D 4C 24 ?? 51 52 8D 4C 24 ?? E8 ?? ?? ?? ?? 8D 44 24 ?? 50 55 E8 ?? ?? ?? ?? 8B F0 83 C4 ?? 3B F5 75 ?? 6A ?? 8D 4C 24 ?? 88 9C 24 ?? ?? ?? ?? E8 ?? ?? ?? ?? 8D 4C 24 ?? C6 84 24 ?? ?? ?? ?? ?? 51 8D 8C 24 ?? ?? ?? ?? E8 ?? ?? ?? ?? 8D 4C 24 ?? C6 84 24 ?? ?? ?? ?? ?? E8 ?? ?? ?? ?? 6A ?? 8D 4C 24 ?? C6 84 24 ?? ?? ?? ?? ?? E8 ?? ?? ?? ?? 8D 54 24 ?? 8D 4C 24 ?? 52 E8 ?? ?? ?? ?? 8D 44 24 ?? 8D 4C 24 ?? 50 C6 84 24 ?? ?? ?? ?? ?? E8 ?? ?? ?? ?? BF ?? ?? ?? ?? 83 C9 ?? 33 C0 C6 84 24");
+		if (Memory::AreAllSignaturesValid(ResolutionInstructionsScansResult) == true)
+		{
+			spdlog::info("Resolution Widths List Instruction: Address is {:s}+{:x}", sExeName.c_str(), ResolutionInstructionsScansResult[ResolutionWidthsListScan] - (std::uint8_t*)exeModule);
+
+			spdlog::info("Resolution Heights List Instruction: Address is {:s}+{:x}", sExeName.c_str(), ResolutionInstructionsScansResult[ResolutionHeightsListScan] - (std::uint8_t*)exeModule);
+
+			static std::string sResX = std::to_string(iCurrentResX) + ":" + std::to_string(iCurrentResX) + ":" + std::to_string(iCurrentResX) + ":" + std::to_string(iCurrentResX); // Original string: "800:1024:1280:1600"
+
+			static std::string sResY = std::to_string(iCurrentResY) + ":" + std::to_string(iCurrentResY) + ":" + std::to_string(iCurrentResY) + ":" + std::to_string(iCurrentResY); // Original string: "600:768:960:1200"
+
+			const char* cNewResolutionWidthsList = sResX.c_str();
+
+			const char* cNewResolutionHeightsList = sResY.c_str();
+
+			Memory::Write(ResolutionInstructionsScansResult[ResolutionWidthsListScan] + 1, cNewResolutionWidthsList);
+
+			Memory::Write(ResolutionInstructionsScansResult[ResolutionHeightsListScan] + 1, cNewResolutionHeightsList);
+		}
+
+		std::uint8_t* AspectRatioInstructionScanResult = Memory::PatternScan(exeModule, "D8 81 ?? ?? ?? ?? 8B 44 24");
+		if (AspectRatioInstructionScanResult)
+		{
+			spdlog::info("Aspect Ratio Instruction: Address is {:s}+{:x}", sExeName.c_str(), AspectRatioInstructionScanResult - (std::uint8_t*)exeModule);
+
+			Memory::PatchBytes(AspectRatioInstructionScanResult, "\x90\x90\x90\x90\x90\x90", 6);
+
+			AspectRatioInstructionHook = safetyhook::create_mid(AspectRatioInstructionScanResult, [](SafetyHookContext& ctx)
+			{
+				FPU::FADD(fNewAspectRatio);
+			});
+		}
+		else
+		{
+			spdlog::error("Failed to locate aspect ratio instruction memory address.");
+			return;
+		}
+
+		std::vector<std::uint8_t*> CameraFOVInstructionsScansResult = Memory::PatternScan(exeModule, "8B 8E ?? ?? ?? ?? 51 E8 ?? ?? ?? ?? 8B C8 E8 ?? ?? ?? ?? 8D 94 24 ?? ?? ?? ?? 8D 44 24 ?? 52 50 E8 ?? ?? ?? ?? 8B C8 E8 ?? ?? ?? ?? E9 ?? ?? ?? ?? 8B 9E", "8B 86 ?? ?? ?? ?? 8D 4C 24 ?? 89 44 24");
+		if (Memory::AreAllSignaturesValid(CameraFOVInstructionsScansResult) == true)
+		{
+			spdlog::info("Camera FOV Instruction 1: Address is {:s}+{:x}", sExeName.c_str(), CameraFOVInstructionsScansResult[CameraFOV1Scan] - (std::uint8_t*)exeModule);
+
+			spdlog::info("Camera FOV Instruction 2: Address is {:s}+{:x}", sExeName.c_str(), CameraFOVInstructionsScansResult[CameraFOV2Scan] - (std::uint8_t*)exeModule);
+
+			Memory::PatchBytes(CameraFOVInstructionsScansResult[CameraFOV1Scan], "\x90\x90\x90\x90\x90\x90", 6);
+
+			CameraFOVInstruction1Hook = safetyhook::create_mid(CameraFOVInstructionsScansResult[CameraFOV1Scan], [](SafetyHookContext& ctx)
+			{
+				float& fCurrentCameraFOV1 = *reinterpret_cast<float*>(ctx.esi + 0x2CC);
+
+				if (fCurrentCameraFOV1 == 1.570796371f)
+				{
+					fNewCameraFOV1 = fCurrentCameraFOV1 * fFOVFactor;
+				}
+				else
+				{
+					fNewCameraFOV1 = fCurrentCameraFOV1;
+				}
+
+				ctx.ecx = std::bit_cast<uintptr_t>(fNewCameraFOV1);
+			});
+
+			Memory::PatchBytes(CameraFOVInstructionsScansResult[CameraFOV2Scan], "\x90\x90\x90\x90\x90\x90", 6);
+
+			CameraFOVInstruction2Hook = safetyhook::create_mid(CameraFOVInstructionsScansResult[CameraFOV2Scan], [](SafetyHookContext& ctx)
+			{
+				float& fCurrentCameraFOV2 = *reinterpret_cast<float*>(ctx.esi + 0x300);
+
+				if (fCurrentCameraFOV2 == 1.178097248f)
+				{
+					fNewCameraFOV2 = Maths::CalculateNewFOV_RadBased(fCurrentCameraFOV2, fAspectRatioScale) * fFOVFactor;
+				}
+				else
+				{
+					fNewCameraFOV2 = Maths::CalculateNewFOV_RadBased(fCurrentCameraFOV2, fAspectRatioScale);
+				}
+
+				ctx.eax = std::bit_cast<uintptr_t>(fNewCameraFOV2);
+			});
+		}
+	}
+}
+
+DWORD __stdcall Main(void*)
+{
+
+	return TRUE;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	{
+		thisModule = hModule;
+
+		Logging();
+		Configuration();
+
+		if (DetectGame())
+		{
+			WidescreenFix();
+		}
+
+		HANDLE mainHandle = CreateThread(NULL, 0, Main, 0, NULL, 0);
+		if (mainHandle)
+		{
+			SetThreadPriority(mainHandle, THREAD_PRIORITY_HIGHEST);
+			CloseHandle(mainHandle);
+		}
+		break;
+	}
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
