7b229a0e6c4e5f1af4c146be1d7bb1f0055e2d48
alphayellow1 <diogoccsantos@gmail.com>
2026-06-14 17:46:16 +0100

    PetRacer: - Improved the fix's structure

--- source/fixes/PetRacerWidescreenFix/dllmain.cpp
+++ source/fixes/PetRacerWidescreenFix/dllmain.cpp
@@ -1,236 +1,81 @@
-// Include necessary headers
-#include "stdafx.h"
-#include "helper.hpp"
+#include "..\..\common\FixBase.hpp"
 
-#include <spdlog/spdlog.h>
-#include <spdlog/sinks/basic_file_sink.h>
-#include <inipp/inipp.h>
-#include <safetyhook.hpp>
-#include <vector>
-#include <map>
-#include <windows.h>
-#include <psapi.h> // For GetModuleInformation
-#include <fstream>
-#include <filesystem>
-#include <cmath> // For atanf, tanf
-#include <sstream>
-#include <cstring>
-#include <iomanip>
-#include <cstdint>
-#include <iostream>
-
-#define spdlog_confparse(var) spdlog::info("Config Parse: {}: {}", #var, var)
-
-HMODULE exeModule = GetModuleHandle(NULL);
-HMODULE thisModule;
-HMODULE dllModule2 = nullptr;
-
-// Fix details
-std::string sFixName = "PetRacerWidescreenFix";
-std::string sFixVersion = "1.2";
-std::filesystem::path sFixPath;
-
-// Ini
-inipp::Ini<char> ini;
-std::string sConfigFile = sFixName + ".ini";
-
-// Logger
-std::shared_ptr<spdlog::logger> logger;
-std::string sLogFile = sFixName + ".log";
-std::filesystem::path sExePath;
-std::string sExeName;
-
-// Constants
-constexpr float fOldAspectRatio = 4.0f / 3.0f;
-
-// Ini variables
-bool bFixActive;
-int iCurrentResX;
-int iCurrentResY;
-float fFOVFactor;
-
-// Variables
-float fNewAspectRatio;
-float fAspectRatioScale;
-float fCurrentCameraFOV;
-float fNewCameraHFOV;
-float fNewCameraVFOV;
-
-// Game detection
-enum class Game
+class PetRacerFix final : public FixBase
 {
-	PR,
-	Unknown
-};
-
-enum CameraFOVInstructionsIndices
-{
-	FOV1,
-	FOV2
-};
-
-struct GameInfo
-{
-	std::string GameTitle;
-	std::string ExeName;
-};
-
-const std::map<Game, GameInfo> kGames = {
-	{Game::PR, {"Pet Racer", "PetRacer.exe"}},
-};
-
-const GameInfo* game = nullptr;
-Game eGameType = Game::Unknown;
-
-void Logging()
-{
-	// Get path to DLL
-	WCHAR dllPath[_MAX_PATH] = { 0 };
-	GetModuleFileNameW(thisModule, dllPath, MAX_PATH);
-	sFixPath = dllPath;
-	sFixPath = sFixPath.remove_filename();
-
-	// Get game name and exe path
-	WCHAR exePathW[_MAX_PATH] = { 0 };
-	GetModuleFileNameW(exeModule, exePathW, MAX_PATH);
-	sExePath = exePathW;
-	sExeName = sExePath.filename().string();
-	sExePath = sExePath.remove_filename();
-
-	// Spdlog initialization
-	try
+public:
+	explicit PetRacerFix(HMODULE selfModule) : FixBase(selfModule)
 	{
-		logger = spdlog::basic_logger_st(sFixName.c_str(), sExePath.string() + "\\" + sLogFile, true);
-		spdlog::set_default_logger(logger);
-		spdlog::flush_on(spdlog::level::debug);
-		spdlog::set_level(spdlog::level::debug); // Enable debug level logging
-
-		spdlog::info("----------");
-		spdlog::info("{:s} v{:s} loaded.", sFixName.c_str(), sFixVersion.c_str());
-		spdlog::info("----------");
-		spdlog::info("Log file: {}", sExePath.string() + "\\" + sLogFile);
-		spdlog::info("----------");
-		spdlog::info("Module Name: {0:s}", sExeName.c_str());
-		spdlog::info("Module Path: {0:s}", sExePath.string());
-		spdlog::info("Module Address: 0x{0:X}", (uintptr_t)exeModule);
-		spdlog::info("----------");
-		spdlog::info("DLL has been successfully loaded.");
-	}
-	catch (const spdlog::spdlog_ex& ex)
-	{
-		AllocConsole();
-		FILE* dummy;
-		freopen_s(&dummy, "CONOUT$", "w", stdout);
-		std::cout << "Log initialization failed: " << ex.what() << std::endl;
-		FreeLibraryAndExitThread(thisModule, 1);
-	}
-}
-
-void Configuration()
-{
-	// Inipp initialization
-	std::ifstream iniFile(sFixPath.string() + "\\" + sConfigFile);
-	if (!iniFile)
-	{
-		AllocConsole();
-		FILE* dummy;
-		freopen_s(&dummy, "CONOUT$", "w", stdout);
-		std::cout << sFixName.c_str() << " v" << sFixVersion.c_str() << " loaded." << std::endl;
-		std::cout << "ERROR: Could not locate config file." << std::endl;
-		std::cout << "ERROR: Make sure " << sConfigFile.c_str() << " is located in " << sFixPath.string().c_str() << std::endl;
-		spdlog::shutdown();
-		FreeLibraryAndExitThread(thisModule, 1);
-	}
-	else
-	{
-		spdlog::info("Config file: {}", sFixPath.string() + "\\" + sConfigFile);
-		ini.parse(iniFile);
+		s_instance_ = this;
 	}
 
-	// Parse config
-	ini.strip_trailing_comments();
-	spdlog::info("----------");
-
-	// Load settings from ini
-	inipp::get_value(ini.sections["WidescreenFix"], "Enabled", bFixActive);
-	spdlog_confparse(bFixActive);
-
-	// Load resolution from ini
-	inipp::get_value(ini.sections["Settings"], "Width", iCurrentResX);
-	inipp::get_value(ini.sections["Settings"], "Height", iCurrentResY);
-	inipp::get_value(ini.sections["Settings"], "FOVFactor", fFOVFactor);
-	spdlog_confparse(iCurrentResX);
-	spdlog_confparse(iCurrentResY);
-	spdlog_confparse(fFOVFactor);
-
-	// If resolution not specified, use desktop resolution
-	if (iCurrentResX <= 0 || iCurrentResY <= 0)
+	~PetRacerFix() override
 	{
-		spdlog::info("Resolution not specified in ini file. Using desktop resolution.");
-		// Implement Util::GetPhysicalDesktopDimensions() accordingly
-		auto desktopDimensions = Util::GetPhysicalDesktopDimensions();
-		iCurrentResX = desktopDimensions.first;
-		iCurrentResY = desktopDimensions.second;
-		spdlog_confparse(iCurrentResX);
-		spdlog_confparse(iCurrentResY);
-	}
-
-	spdlog::info("----------");
-}
-
-bool DetectGame()
-{
-	bool bGameFound = false;
-
-	for (const auto& [type, info] : kGames)
-	{
-		if (Util::stringcmp_caseless(info.ExeName, sExeName))
+		if (s_instance_ == this)
 		{
-			spdlog::info("Detected game: {:s} ({:s})", info.GameTitle, sExeName);
-			spdlog::info("----------");
-			eGameType = type;
-			game = &info;
-			bGameFound = true;
-			break;
+			s_instance_ = nullptr;
 		}
 	}
 
-	if (bGameFound == false)
+protected:
+	const char* FixName() const override
 	{
-		spdlog::error("Failed to detect supported game, {:s} isn't supported by the fix.", sExeName);
-		return false;
+		return "PetRacerWidescreenFix";
 	}
 
-	dllModule2 = Memory::GetHandle("ChromeEngine.dll");
-
-	return true;
-}
-
-static SafetyHookMid ResolutionInstructionsHook{};
-static SafetyHookMid CameraFOVInstructionHook{};
-static SafetyHookMid CameraHFOVInstructionHook{};
-static SafetyHookMid CameraVFOVInstructionHook{};
-
-void WidescreenFix()
-{
-	if (eGameType == Game::PR && bFixActive == true)
+	const char* FixVersion() const override
 	{
-		fNewAspectRatio = static_cast<float>(iCurrentResX) / static_cast<float>(iCurrentResY);
+		return "1.3";
+	}
 
-		fAspectRatioScale = fNewAspectRatio / fOldAspectRatio;
+	const char* TargetName() const override
+	{
+		return "Pet Racer";
+	}
 
-		std::uint8_t* ResolutionInstructionsScanResult = Memory::PatternScan(dllModule2, "89 41 ?? 89 51 ?? C2 ?? ?? 90 90 90 90 90 90 90 90 90 90 90 90 90 90 90 8B 54 24");
+	InitMode GetInitMode() const override
+	{
+		// return InitMode::Direct;
+		return InitMode::WorkerThread;
+		// return InitMode::ExportedOnly;
+	}
+
+	bool IsCompatibleExecutable(const std::string& exeName) const override
+	{
+		return Util::stringcmp_caseless(exeName, "PetRacer.exe");
+	}
+
+	void ParseFixConfig(inipp::Ini<char>& ini) override
+	{
+		inipp::get_value(ini.sections["Settings"], "Width", m_newResX);
+		inipp::get_value(ini.sections["Settings"], "Height", m_newResY);
+		inipp::get_value(ini.sections["Settings"], "FOVFactor", m_fovFactor);
+
+		FallbackToDesktopResolution(m_newResX, m_newResY);
+
+		spdlog_confparse(m_newResX);
+		spdlog_confparse(m_newResY);
+		spdlog_confparse(m_fovFactor);
+	}
+
+	void ApplyFix() override
+	{
+		m_newAspectRatio = static_cast<float>(m_newResX) / static_cast<float>(m_newResY);
+		m_aspectRatioScale = m_newAspectRatio / m_oldAspectRatio;
+
+		m_chromeEngineDllModule = Memory::GetHandle("ChromeEngine.dll");
+		m_chromeEngineDllModuleName = Memory::GetModuleName(m_chromeEngineDllModule);
+
+		auto ResolutionInstructionsScanResult = Memory::PatternScan(m_chromeEngineDllModule, "89 41 ?? 89 51 ?? C2 ?? ?? 90 90 90 90 90 90 90 90 90 90 90 90 90 90 90 8B 54 24");
 		if (ResolutionInstructionsScanResult)
 		{
-			spdlog::info("Resolution Instructions Scan: Address is ChromeEngine.dll+{:x}", ResolutionInstructionsScanResult - (std::uint8_t*)dllModule2);
+			spdlog::info("Resolution Instructions Scan: Address is {:s}+{:x}", m_chromeEngineDllModuleName.c_str(), ResolutionInstructionsScanResult - (std::uint8_t*)m_chromeEngineDllModule);
 
 			Memory::WriteNOPs(ResolutionInstructionsScanResult, 6);
 
-			ResolutionInstructionsHook = safetyhook::create_mid(ResolutionInstructionsScanResult, [](SafetyHookContext& ctx)
+			m_resolutionHook = safetyhook::create_mid(ResolutionInstructionsScanResult, [](SafetyHookContext& ctx)
 			{
-				Memory::ReadMem(ctx.ecx + 0xC) = iCurrentResX;
-
-				Memory::ReadMem(ctx.ecx + 0x10) = iCurrentResY;
+				*reinterpret_cast<int*>(ctx.ecx + 0xC) = s_instance_->m_newResX;
+				*reinterpret_cast<int*>(ctx.ecx + 0x10) = s_instance_->m_newResY;
 			});
 		}
 		else
@@ -239,69 +84,81 @@ void WidescreenFix()
 			return;
 		}
 
-		std::vector<std::uint8_t*> CameraFOVInstructionsScansResult = Memory::PatternScan(dllModule2, "D9 44 24 ?? D8 0D ?? ?? ?? ?? 8A 81", "D8 4C 24 ?? D9 5C 24 ?? D8 C9");
-		if (Memory::AreAllSignaturesValid(CameraFOVInstructionsScansResult) == true)
+		auto CameraFOVScansResult = Memory::PatternScan(m_chromeEngineDllModule, "D9 44 24 ?? D8 0D ?? ?? ?? ?? 8A 81", "D8 4C 24 ?? D9 5C 24 ?? D8 C9");
+		if (Memory::AreAllSignaturesValid(CameraFOVScansResult) == true)
 		{
-			spdlog::info("Camera FOV Instruction: Address is ChromeEngine.dll+{:x}", CameraFOVInstructionsScansResult[FOV1] - (std::uint8_t*)dllModule2);
+			spdlog::info("Camera FOV Instruction: Address is {:s}+{:x}", m_chromeEngineDllModuleName.c_str(), CameraFOVScansResult[FOV1] - (std::uint8_t*)m_chromeEngineDllModule);
+			spdlog::info("Camera HFOV Instruction: Address is {:s}+{:x}", m_chromeEngineDllModuleName.c_str(), CameraFOVScansResult[FOV2] - (std::uint8_t*)m_chromeEngineDllModule);
+			spdlog::info("Camera VFOV Instruction: Address is {:s}+{:x}", m_chromeEngineDllModuleName.c_str(), CameraFOVScansResult[FOV2] + 10 - (std::uint8_t*)m_chromeEngineDllModule);
 
-			spdlog::info("Camera HFOV Instruction: Address is ChromeEngine.dll+{:x}", CameraFOVInstructionsScansResult[FOV2] - (std::uint8_t*)dllModule2);
-
-			spdlog::info("Camera VFOV Instruction: Address is ChromeEngine.dll+{:x}", CameraFOVInstructionsScansResult[FOV2] + 10 - (std::uint8_t*)dllModule2);
-
-			CameraFOVInstructionHook = safetyhook::create_mid(CameraFOVInstructionsScansResult[FOV1], [](SafetyHookContext& ctx)
+			m_cameraFOVHook = safetyhook::create_mid(CameraFOVScansResult[FOV1], [](SafetyHookContext& ctx)
 			{
-				fCurrentCameraFOV = Memory::ReadMem(ctx.esp + 0x4);
+				s_instance_->m_currentCameraFOV = Memory::ReadMem(ctx.esp + 0x4);
 			});
 
-			Memory::WriteNOPs(CameraFOVInstructionsScansResult[FOV2], 4); // NOP out the original instruction
+			Memory::WriteNOPs(CameraFOVScansResult[FOV2], 4); // NOP out the original instruction
 
-			CameraHFOVInstructionHook = safetyhook::create_mid(CameraFOVInstructionsScansResult[FOV2], [](SafetyHookContext& ctx)
+			m_cameraHFOVHook = safetyhook::create_mid(CameraFOVScansResult[FOV2], [](SafetyHookContext& ctx)
 			{
 				float& fCurrentCameraHFOV = Memory::ReadMem(ctx.esp + 0x8);
 
-				if (fCurrentCameraFOV == 1.6580627894f)
+				if (s_instance_->m_currentCameraFOV == 1.6580627894f)
 				{
-					fNewCameraHFOV = fCurrentCameraHFOV * fAspectRatioScale * fFOVFactor;
+					s_instance_->m_newCameraHFOV = fCurrentCameraHFOV * s_instance_->m_aspectRatioScale * s_instance_->m_fovFactor;
 				}
 				else
 				{
-					fNewCameraHFOV = fCurrentCameraHFOV * fAspectRatioScale;
+					s_instance_->m_newCameraHFOV = fCurrentCameraHFOV * s_instance_->m_aspectRatioScale;
 				}
 
-				FPU::FMUL(fNewCameraHFOV);
+				FPU::FMUL(s_instance_->m_newCameraHFOV);
 			});
 
-			Memory::WriteNOPs(CameraFOVInstructionsScansResult[FOV2] + 10, 4); // NOP out the original instruction
+			Memory::WriteNOPs(CameraFOVScansResult[FOV2] + 10, 4); // NOP out the original instruction
 
-			CameraVFOVInstructionHook = safetyhook::create_mid(CameraFOVInstructionsScansResult[FOV2] + 10, [](SafetyHookContext& ctx)
+			m_cameraVFOVHook = safetyhook::create_mid(CameraFOVScansResult[FOV2] + 10, [](SafetyHookContext& ctx)
 			{
 				float& fCurrentCameraVFOV = Memory::ReadMem(ctx.esp + 0x8);
 
-				if (fCurrentCameraFOV == 1.6580627894f)
+				if (s_instance_->m_currentCameraFOV == 1.6580627894f)
 				{
-					fNewCameraVFOV = fCurrentCameraVFOV * fAspectRatioScale * fFOVFactor;
+					s_instance_->m_newCameraVFOV = fCurrentCameraVFOV * s_instance_->m_aspectRatioScale * s_instance_->m_fovFactor;
 				}
 				else
 				{
-					fNewCameraVFOV = fCurrentCameraVFOV * fAspectRatioScale;
+					s_instance_->m_newCameraVFOV = fCurrentCameraVFOV * s_instance_->m_aspectRatioScale;
 				}
 
-				FPU::FMUL(fNewCameraVFOV);
+				FPU::FMUL(s_instance_->m_newCameraVFOV);
 			});
 		}
 	}
-}
 
-DWORD __stdcall Main(void*)
-{
-	Logging();
-	Configuration();
-	if (DetectGame())
+private:
+	static constexpr float m_oldAspectRatio = 4.0f / 3.0f;
+
+	HMODULE m_chromeEngineDllModule = nullptr;
+	std::string m_chromeEngineDllModuleName = "";
+
+	float m_currentCameraFOV = 0.0f;
+	float m_newCameraHFOV = 0.0f;
+	float m_newCameraVFOV = 0.0f;
+
+	SafetyHookMid m_resolutionHook{};
+	SafetyHookMid m_cameraFOVHook{};
+	SafetyHookMid m_cameraHFOVHook{};
+	SafetyHookMid m_cameraVFOVHook{};
+
+	enum CameraFOVInstructionsIndices
 	{
-		WidescreenFix();
-	}
-	return TRUE;
-}
+		FOV1,
+		FOV2
+	};
+
+	inline static PetRacerFix* s_instance_ = nullptr;
+};
+
+static std::unique_ptr<PetRacerFix> g_fix;
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
@@ -309,19 +166,24 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 	{
 	case DLL_PROCESS_ATTACH:
 	{
-		thisModule = hModule;
-		HANDLE mainHandle = CreateThread(NULL, 0, Main, 0, NULL, 0);
-		if (mainHandle)
-		{
-			SetThreadPriority(mainHandle, THREAD_PRIORITY_HIGHEST);
-			CloseHandle(mainHandle);
-		}
+		DisableThreadLibraryCalls(hModule);
+		g_fix = std::make_unique<PetRacerFix>(hModule);
+		g_fix->Start();
 		break;
 	}
+
+	case DLL_PROCESS_DETACH:
+	{
+		g_fix->Shutdown();
+		g_fix.reset();
+		break;
+	}
+
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
+	default:
 		break;
 	}
+
 	return TRUE;
 }
