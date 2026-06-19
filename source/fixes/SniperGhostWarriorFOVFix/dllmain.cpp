2d708fb2707ff20d0663a1ed928ee166b248e267
alphayellow1 <diogoccsantos@gmail.com>
2026-06-19 17:45:18 +0100

    SniperGhostWarrior: - Improved the fix's structure - Made the fix calculate cutscenes field of view automatically from the in-game resolution if the current aspect ratio is wider than 16:9

--- source/fixes/SniperGhostWarriorFOVFix/dllmain.cpp
+++ source/fixes/SniperGhostWarriorFOVFix/dllmain.cpp
@@ -1,274 +1,152 @@
-// Include necessary headers
-#include "stdafx.h"
-#include "helper.hpp"
+#include "..\..\common\FixBase.hpp"
 
-#include <spdlog/spdlog.h>
-#include <spdlog/sinks/basic_file_sink.h>
-#include <inipp/inipp.h>
-#include <safetyhook.hpp>
-#include <vector>
-#include <algorithm>
-#include <map>
-#include <windows.h>
-#include <psapi.h> // For GetModuleInformation
-#include <fstream>
-#include <filesystem>
-#include <cmath> // For atanf, tanf
-#include <sstream>
-#include <cstring>
-#include <iomanip>
-#include <cstdint>
-#include <iostream>
-
-#define spdlog_confparse(var) spdlog::info("Config Parse: {}: {}", #var, var)
-
-HMODULE exeModule = GetModuleHandle(NULL);
-HMODULE thisModule;
-HMODULE dllModule2 = nullptr;
-
-// Fix details
-std::string sFixName = "SniperGhostWarriorFOVFix";
-std::string sFixVersion = "1.2";
-std::filesystem::path sFixPath;
-
-// Ini
-inipp::Ini<char> ini;
-std::string sConfigFile = sFixName + ".ini";
-
-// Logger
-std::shared_ptr<spdlog::logger> logger;
-std::string sLogFile = sFixName + ".log";
-std::filesystem::path sExePath;
-std::string sExeName;
-
-// Constants
-constexpr float fOldAspectRatio = 16.0f / 9.0f;
-
-// Ini variables
-bool bFixActive;
-int iCurrentResX;
-int iCurrentResY;
-float fFOVFactor;
-
-// Variables
-float fNewAspectRatio;
-float fAspectRatioScale;
-float fNewGameplayFOV;
-float fNewCutscenesFOV;
-
-// Game detection
-enum class Game
+class SniperGhostWarrior1Fix final : public FixBase
 {
-	SGW,
-	Unknown
-};
-
-enum CameraFOVInstructionsIndices
-{
-	Gameplay,
-	Cutscenes
-};
-
-struct GameInfo
-{
-	std::string GameTitle;
-	std::string ExeName;
-};
-
-const std::map<Game, GameInfo> kGames = {
-	{Game::SGW, {"Sniper: Ghost Warrior", "Sniper_x86.exe"}},
-};
-
-const GameInfo* game = nullptr;
-Game eGameType = Game::Unknown;
-
-void Logging()
-{
-	// Get path to DLL
-	WCHAR dllPath[_MAX_PATH] = { 0 };
-	GetModuleFileNameW(thisModule, dllPath, MAX_PATH);
-	sFixPath = dllPath;
-	sFixPath = sFixPath.remove_filename();
-
-	// Get game name and exe path
-	WCHAR exePathW[_MAX_PATH] = { 0 };
-	GetModuleFileNameW(exeModule, exePathW, MAX_PATH);
-	sExePath = exePathW;
-	sExeName = sExePath.filename().string();
-	sExePath = sExePath.remove_filename();
-
-	// Spdlog initialization
-	try
+public:
+	explicit SniperGhostWarrior1Fix(HMODULE selfModule) : FixBase(selfModule)
 	{
-		logger = spdlog::basic_logger_st(sFixName.c_str(), sExePath.string() + "\\" + sLogFile, true);
-		spdlog::set_default_logger(logger);
-		spdlog::flush_on(spdlog::level::debug);
-		spdlog::set_level(spdlog::level::debug); // Enable debug level logging
-
-		spdlog::info("----------");
-		spdlog::info("{:s} v{:s} loaded.", sFixName.c_str(), sFixVersion.c_str());
-		spdlog::info("----------");
-		spdlog::info("Log file: {}", sExePath.string() + "\\" + sLogFile);
-		spdlog::info("----------");
-		spdlog::info("Module Name: {0:s}", sExeName.c_str());
-		spdlog::info("Module Path: {0:s}", sExePath.string());
-		spdlog::info("Module Address: 0x{0:X}", (uintptr_t)exeModule);
-		spdlog::info("----------");
-		spdlog::info("DLL has been successfully loaded.");
-	}
-	catch (const spdlog::spdlog_ex& ex)
-	{
-		AllocConsole();
-		FILE* dummy;
-		freopen_s(&dummy, "CONOUT$", "w", stdout);
-		std::cout << "Log initialization failed: " << ex.what() << std::endl;
-		FreeLibraryAndExitThread(thisModule, 1);
-	}
-}
-
-void Configuration()
-{
-	// Inipp initialization
-	std::ifstream iniFile(sFixPath.string() + "\\" + sConfigFile);
-	if (!iniFile)
-	{
-		AllocConsole();
-		FILE* dummy;
-		freopen_s(&dummy, "CONOUT$", "w", stdout);
-		std::cout << sFixName.c_str() << " v" << sFixVersion.c_str() << " loaded." << std::endl;
-		std::cout << "ERROR: Could not locate config file." << std::endl;
-		std::cout << "ERROR: Make sure " << sConfigFile.c_str() << " is located in " << sFixPath.string().c_str() << std::endl;
-		spdlog::shutdown();
-		FreeLibraryAndExitThread(thisModule, 1);
-	}
-	else
-	{
-		spdlog::info("Config file: {}", sFixPath.string() + "\\" + sConfigFile);
-		ini.parse(iniFile);
+		s_instance_ = this;
 	}
 
-	// Parse config
-	ini.strip_trailing_comments();
-	spdlog::info("----------");
-
-	// Load settings from ini
-	inipp::get_value(ini.sections["FOVFix"], "Enabled", bFixActive);
-	spdlog_confparse(bFixActive);
-
-	// Load resolution from ini
-	inipp::get_value(ini.sections["Settings"], "Width", iCurrentResX);
-	inipp::get_value(ini.sections["Settings"], "Height", iCurrentResY);
-	inipp::get_value(ini.sections["Settings"], "FOVFactor", fFOVFactor);
-	spdlog_confparse(iCurrentResX);
-	spdlog_confparse(iCurrentResY);
-	spdlog_confparse(fFOVFactor);
-
-	// If resolution not specified, use desktop resolution
-	if (iCurrentResX <= 0 || iCurrentResY <= 0)
+	~SniperGhostWarrior1Fix() override
 	{
-		spdlog::info("Resolution not specified in ini file. Using desktop resolution.");
-		// Implement Util::GetPhysicalDesktopDimensions() accordingly
-		auto desktopDimensions = Util::GetPhysicalDesktopDimensions();
-		iCurrentResX = desktopDimensions.first;
-		iCurrentResY = desktopDimensions.second;
-		spdlog_confparse(iCurrentResX);
-		spdlog_confparse(iCurrentResY);
-	}
-
-	spdlog::info("----------");
-}
-
-bool DetectGame()
-{
-	for (const auto& [type, info] : kGames)
-	{
-		if (Util::stringcmp_caseless(info.ExeName, sExeName))
+		if (s_instance_ == this)
 		{
-			spdlog::info("Detected game: {:s} ({:s})", info.GameTitle, sExeName);
-			spdlog::info("----------");
-			eGameType = type;
-			game = &info;
+			s_instance_ = nullptr;
+		}
+	}
+
+protected:
+	const char* FixName() const override
+	{
+		return "SniperGhostWarriorFOVFix";
+	}
+
+	const char* FixVersion() const override
+	{
+		return "1.3";
+	}
+
+	const char* TargetName() const override
+	{
+		return "Sniper: Ghost Warrior";
+	}
+
+	InitMode GetInitMode() const override
+	{
+		// return InitMode::Direct;
+		return InitMode::WorkerThread;
+		// return InitMode::ExportedOnly;
+	}
+
+	bool IsCompatibleExecutable(const std::string& exeName) const override
+	{
+		return Util::stringcmp_caseless(exeName, "Sniper_x86.exe");
+	}
+
+	void ParseFixConfig(inipp::Ini<char>& ini) override
+	{
+		inipp::get_value(ini.sections["Settings"], "FOVFactor", m_fovFactor);
+		spdlog_confparse(m_fovFactor);
+	}
+
+	void ApplyFix() override
+	{
+		m_enginex86DllModule = Memory::GetHandle("engine_x86.dll");
+		m_enginex86DllModuleName = Memory::GetModuleName(m_enginex86DllModule);
+
+		auto ResolutionScanResult = Memory::PatternScan(m_enginex86DllModule, "89 3D ?? ?? ?? ?? 89 35 ?? ?? ?? ?? 59");
+		if (ResolutionScanResult)
+		{
+			spdlog::info("Resolution Instructions Scan: Address is {:s}+{:x}", m_enginex86DllModuleName.c_str(), ResolutionScanResult - (std::uint8_t*)ExeModule());
+
+			m_resolutionHook = safetyhook::create_mid(ResolutionScanResult, [](SafetyHookContext& ctx)
+			{
+				s_instance_->m_currentWidth = Memory::ReadRegister(ctx.edi);
+				s_instance_->m_currentHeight = Memory::ReadRegister(ctx.esi);
+				s_instance_->m_newAspectRatio = static_cast<float>(s_instance_->m_currentWidth) / static_cast<float>(s_instance_->m_currentHeight);
+				s_instance_->m_aspectRatioScale = s_instance_->m_newAspectRatio / m_oldAspectRatio;
+			});
 		}
 		else
 		{
-			spdlog::error("Failed to detect supported game, {:s} isn't supported by the fix.", sExeName);
-			return false;
+			spdlog::error("Failed to locate resolution instructions scan memory address.");
+			return;
 		}
-	}
-
-	dllModule2 = Memory::GetHandle("engine_x86.dll");
-
-	return true;
-}
-
-static SafetyHookMid GameplayFOVInstructionHook{};
-static SafetyHookMid CutscenesFOVInstructionHook{};
-
-void FOVFix()
-{
-	if (eGameType == Game::SGW && bFixActive == true)
-	{
-		fNewAspectRatio = static_cast<float>(iCurrentResX) / static_cast<float>(iCurrentResY);
-
-		fAspectRatioScale = fNewAspectRatio / fOldAspectRatio;
 
 		// Instructions are located in IBaseCamera::SetFOV and IGSObject::GetStatusEditor
-		std::vector<std::uint8_t*> CameraFOVInstructionsScansResult = Memory::PatternScan(dllModule2, "D9 44 24 ?? D8 0D ?? ?? ?? ?? D9 1C 24 FF D2", "D9 83 ?? ?? ?? ?? 8B 15 ?? ?? ?? ?? D8 0D");
-		if (Memory::AreAllSignaturesValid(CameraFOVInstructionsScansResult) == true)
+		auto CameraFOVScansResult = Memory::PatternScan(m_enginex86DllModule, "D9 44 24 ?? D8 0D ?? ?? ?? ?? D9 1C 24 FF D2", "D9 83 ?? ?? ?? ?? 8B 15 ?? ?? ?? ?? D8 0D");
+		if (Memory::AreAllSignaturesValid(CameraFOVScansResult) == true)
 		{
-			spdlog::info("Gameplay Camera FOV Instruction: Address is engine_x86.dll+{:x}", CameraFOVInstructionsScansResult[Gameplay] - (std::uint8_t*)dllModule2);
+			spdlog::info("Gameplay Camera FOV Instruction: Address is {:s}+{:x}", m_enginex86DllModuleName.c_str(), CameraFOVScansResult[Gameplay] - (std::uint8_t*)m_enginex86DllModule);
+			spdlog::info("Cutscenes Camera FOV Instruction: Address is {:s}+{:x}", m_enginex86DllModuleName.c_str(), CameraFOVScansResult[Cutscenes] - (std::uint8_t*)m_enginex86DllModule);
 
-			spdlog::info("Cutscenes Camera FOV Instruction: Address is engine_x86.dll+{:x}", CameraFOVInstructionsScansResult[Cutscenes] - (std::uint8_t*)dllModule2);
+			Memory::WriteNOPs(CameraFOVScansResult[Gameplay], 4);
 
-			Memory::WriteNOPs(CameraFOVInstructionsScansResult[Gameplay], 4);
-
-			GameplayFOVInstructionHook = safetyhook::create_mid(CameraFOVInstructionsScansResult[Gameplay], [](SafetyHookContext& ctx)
+			m_gameplayFOVHook = safetyhook::create_mid(CameraFOVScansResult[Gameplay], [](SafetyHookContext& ctx)
 			{
-				float& fCurrentGameplayFOV = Memory::ReadMem(ctx.esp + 0x10);
+				s_instance_->m_currentGameplayFOV = Memory::ReadMem(ctx.esp + 0x10);
 
-				if (fCurrentGameplayFOV == 46.799999237061f || fCurrentGameplayFOV == 46.799995422363f)
+				if (s_instance_->m_currentGameplayFOV == 46.799999237061f || s_instance_->m_currentGameplayFOV == 46.799995422363f)
 				{
-					fNewGameplayFOV = fCurrentGameplayFOV * fFOVFactor;
+					s_instance_->m_newGameplayFOV = s_instance_->m_currentGameplayFOV * s_instance_->m_fovFactor;
 				}
 				else
 				{
-					fNewGameplayFOV = fCurrentGameplayFOV;
+					s_instance_->m_newGameplayFOV = s_instance_->m_currentGameplayFOV;
 				}
 
-				FPU::FLD(fNewGameplayFOV);
+				FPU::FLD(s_instance_->m_newGameplayFOV);
 			});
 
-			Memory::WriteNOPs(CameraFOVInstructionsScansResult[Cutscenes], 6);
+			Memory::WriteNOPs(CameraFOVScansResult[Cutscenes], 6);
 
-			CutscenesFOVInstructionHook = safetyhook::create_mid(CameraFOVInstructionsScansResult[Cutscenes], [](SafetyHookContext& ctx)
+			m_cutscenesFOVHook = safetyhook::create_mid(CameraFOVScansResult[Cutscenes], [](SafetyHookContext& ctx)
 			{
-				float& fCurrentCutscenesFOV = Memory::ReadMem(ctx.ebx + 0x150);
+				s_instance_->m_currentCutscenesFOV = Memory::ReadMem(ctx.ebx + 0x150);
 
-				if (fNewAspectRatio > fOldAspectRatio)
+				if (s_instance_->m_newAspectRatio > m_oldAspectRatio)
 				{
-					fNewCutscenesFOV = Maths::CalculateNewFOV_DegBased(fCurrentCutscenesFOV, fAspectRatioScale);
+					s_instance_->m_newCutscenesFOV = Maths::CalculateNewFOV_DegBased(s_instance_->m_currentCutscenesFOV, s_instance_->m_aspectRatioScale);
 				}
-				else if (fNewAspectRatio <= fOldAspectRatio)
+				else if (s_instance_->m_newAspectRatio <= m_oldAspectRatio)
 				{
-					fNewCutscenesFOV = fCurrentCutscenesFOV;
+					s_instance_->m_newCutscenesFOV = s_instance_->m_currentCutscenesFOV;
 				}
 
-				FPU::FLD(fNewCutscenesFOV);
+				FPU::FLD(s_instance_->m_newCutscenesFOV);
 			});
 		}
 	}
-}
 
-DWORD __stdcall Main(void*)
-{
-	Logging();
-	Configuration();
-	if (DetectGame())
+private:
+	HMODULE m_enginex86DllModule = nullptr;
+	std::string m_enginex86DllModuleName = "";
+
+	static constexpr float m_oldAspectRatio = 16.0f / 9.0f;
+
+	int m_currentWidth = 0;
+	int m_currentHeight = 0;
+
+	float m_currentGameplayFOV = 0.0f;
+	float m_currentCutscenesFOV = 0.0f;
+	float m_newGameplayFOV = 0.0f;
+	float m_newCutscenesFOV = 0.0f;
+
+	SafetyHookMid m_resolutionHook{};
+	SafetyHookMid m_gameplayFOVHook{};
+	SafetyHookMid m_cutscenesFOVHook{};
+
+	enum CameraFOVInstructionsIndex
 	{
-		FOVFix();
-	}
-	return TRUE;
-}
+		Gameplay,
+		Cutscenes
+	};
+
+	inline static SniperGhostWarrior1Fix* s_instance_ = nullptr;
+};
+
+static std::unique_ptr<SniperGhostWarrior1Fix> g_fix;
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
@@ -276,19 +154,24 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 	{
 	case DLL_PROCESS_ATTACH:
 	{
-		thisModule = hModule;
-		HANDLE mainHandle = CreateThread(NULL, 0, Main, 0, NULL, 0);
-		if (mainHandle)
-		{
-			SetThreadPriority(mainHandle, THREAD_PRIORITY_HIGHEST);
-			CloseHandle(mainHandle);
-		}
+		DisableThreadLibraryCalls(hModule);
+		g_fix = std::make_unique<SniperGhostWarrior1Fix>(hModule);
+		g_fix->Start();
 		break;
 	}
+
+	case DLL_PROCESS_DETACH:
+	{
+		g_fix->Shutdown();
+		g_fix.reset();
+		break;
+	}
+
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
+	default:
 		break;
 	}
+
 	return TRUE;
 }
