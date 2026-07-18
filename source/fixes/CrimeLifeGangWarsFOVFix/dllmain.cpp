0860ae8be95f18796a764f9272818ca659ba4f96
AlphaYellow <diogoccsantos@gmail.com>
2026-07-18 22:22:23 +0100

    CrimeLifeGangWars: - Improved the fix's structure - Added support for running multiple game instances at once and skipping the intro videos/logos

--- source/fixes/CrimeLifeGangWarsFOVFix/dllmain.cpp
+++ source/fixes/CrimeLifeGangWarsFOVFix/dllmain.cpp
@@ -1,270 +1,214 @@
-// Include necessary headers
-#include "stdafx.h"
-#include "helper.hpp"
+#include "..\..\common\FixBase.hpp"
 
-#include <spdlog/spdlog.h>
-#include <spdlog/sinks/basic_file_sink.h>
-#include <inipp/inipp.h>
-#include <safetyhook.hpp>
-#include <vector>
-#include <map>
-#include <windows.h>
-#include <psapi.h> // For GetModuleInformation
-#include <fstream>
-#include <filesystem>
-#include <sstream>
-#include <cstring>
-#include <iomanip>
-#include <cstdint>
-#include <iostream>
-
-#define spdlog_confparse(var) spdlog::info("Config Parse: {}: {}", #var, var)
-
-HMODULE exeModule = GetModuleHandle(NULL);
-HMODULE thisModule;
-
-// Fix details
-std::string sFixName = "CrimeLifeGangWarsFOVFix";
-std::string sFixVersion = "1.2";
-std::filesystem::path sFixPath;
-
-// Ini
-inipp::Ini<char> ini;
-std::string sConfigFile = sFixName + ".ini";
-
-// Logger
-std::shared_ptr<spdlog::logger> logger;
-std::string sLogFile = sFixName + ".log";
-std::filesystem::path sExePath;
-std::string sExeName;
-
-// Constants
-constexpr float fOldAspectRatio = 4.0f / 3.0f;
-constexpr float fOriginalCameraFOV = 0.5f;
-
-// Ini variables
-bool bFixActive;
-
-// Variables
-int iCurrentResX;
-int iCurrentResY;
-float fNewCameraFOV;
-float fFOVFactor;
-float fNewAspectRatio;
-float fDamping;
-float fEffectiveFOVFactor;
-
-// Game detection
-enum class Game
+class CrimeLifeGangWarsFix final : public FixBase
 {
-	CLGW,
-	Unknown
-};
-
-struct GameInfo
-{
-	std::string GameTitle;
-	std::string ExeName;
-};
-
-const std::map<Game, GameInfo> kGames = {
-	{Game::CLGW, {"Crime Life: Gang Wars", "crimelife.exe"}},
-};
-
-const GameInfo* game = nullptr;
-Game eGameType = Game::Unknown;
-
-void Logging()
-{
-	// Get path to DLL
-	WCHAR dllPath[_MAX_PATH] = { 0 };
-	GetModuleFileNameW(thisModule, dllPath, MAX_PATH);
-	sFixPath = dllPath;
-	sFixPath = sFixPath.remove_filename();
-
-	// Get game name and exe path
-	WCHAR exePathW[_MAX_PATH] = { 0 };
-	GetModuleFileNameW(exeModule, exePathW, MAX_PATH);
-	sExePath = exePathW;
-	sExeName = sExePath.filename().string();
-	sExePath = sExePath.remove_filename();
-
-	// Spdlog initialization
-	try
+public:
+	explicit CrimeLifeGangWarsFix(HMODULE selfModule) : FixBase(selfModule)
 	{
-		logger = spdlog::basic_logger_st(sFixName.c_str(), sExePath.string() + "\\" + sLogFile, true);
-		spdlog::set_default_logger(logger);
-		spdlog::flush_on(spdlog::level::debug);
-		spdlog::set_level(spdlog::level::debug); // Enable debug level logging
-
-		spdlog::info("----------");
-		spdlog::info("{:s} v{:s} loaded.", sFixName.c_str(), sFixVersion.c_str());
-		spdlog::info("----------");
-		spdlog::info("Log file: {}", sExePath.string() + "\\" + sLogFile);
-		spdlog::info("----------");
-		spdlog::info("Module Name: {0:s}", sExeName.c_str());
-		spdlog::info("Module Path: {0:s}", sExePath.string());
-		spdlog::info("Module Address: 0x{0:X}", (uintptr_t)exeModule);
-		spdlog::info("----------");
-		spdlog::info("DLL has been successfully loaded.");
-	}
-	catch (const spdlog::spdlog_ex& ex)
-	{
-		AllocConsole();
-		FILE* dummy;
-		freopen_s(&dummy, "CONOUT$", "w", stdout);
-		std::cout << "Log initialization failed: " << ex.what() << std::endl;
-		FreeLibraryAndExitThread(thisModule, 1);
-	}
-}
-
-void Configuration()
-{
-	// Inipp initialization
-	std::ifstream iniFile(sFixPath.string() + "\\" + sConfigFile);
-	if (!iniFile)
-	{
-		AllocConsole();
-		FILE* dummy;
-		freopen_s(&dummy, "CONOUT$", "w", stdout);
-		std::cout << sFixName.c_str() << " v" << sFixVersion.c_str() << " loaded." << std::endl;
-		std::cout << "ERROR: Could not locate config file." << std::endl;
-		std::cout << "ERROR: Make sure " << sConfigFile.c_str() << " is located in " << sFixPath.string().c_str() << std::endl;
-		spdlog::shutdown();
-		FreeLibraryAndExitThread(thisModule, 1);
-	}
-	else
-	{
-		spdlog::info("Config file: {}", sFixPath.string() + "\\" + sConfigFile);
-		ini.parse(iniFile);
+		s_instance_ = this;
 	}
 
-	// Parse config
-	ini.strip_trailing_comments();
-	spdlog::info("----------");
-
-	// Load settings from ini
-	inipp::get_value(ini.sections["FOVFix"], "Enabled", bFixActive);
-	spdlog_confparse(bFixActive);
-
-	// Load resolution from ini
-	inipp::get_value(ini.sections["Settings"], "Width", iCurrentResX);
-	inipp::get_value(ini.sections["Settings"], "Height", iCurrentResY);
-	inipp::get_value(ini.sections["Settings"], "FOVFactor", fFOVFactor);
-	spdlog_confparse(iCurrentResX);
-	spdlog_confparse(iCurrentResY);
-	spdlog_confparse(fFOVFactor);
-
-	// If resolution not specified, use desktop resolution
-	if (iCurrentResX <= 0 || iCurrentResY <= 0)
+	~CrimeLifeGangWarsFix() override
 	{
-		spdlog::info("Resolution not specified in ini file. Using desktop resolution.");
-		// Implement Util::GetPhysicalDesktopDimensions() accordingly
-		auto desktopDimensions = Util::GetPhysicalDesktopDimensions();
-		iCurrentResX = desktopDimensions.first;
-		iCurrentResY = desktopDimensions.second;
-		spdlog_confparse(iCurrentResX);
-		spdlog_confparse(iCurrentResY);
-	}
-
-	spdlog::info("----------");
-}
-
-bool DetectGame()
-{
-	for (const auto& [type, info] : kGames)
-	{
-		if (Util::stringcmp_caseless(info.ExeName, sExeName))
+		if (s_instance_ == this)
 		{
-			spdlog::info("Detected game: {:s} ({:s})", info.GameTitle, sExeName);
-			spdlog::info("----------");
-			eGameType = type;
-			game = &info;
-			return true;
+			s_instance_ = nullptr;
 		}
 	}
 
-	spdlog::error("Failed to detect supported game, {:s} isn't supported by the fix.", sExeName);
-	return false;
-}
-
-static SafetyHookMid CameraFOVInstructionHook{};
-
-void FOVFix()
-{
-	if (eGameType == Game::CLGW && bFixActive == true)
+protected:
+	const char* FixName() const override
 	{
-		fNewAspectRatio = static_cast<float>(iCurrentResX) / static_cast<float>(iCurrentResY);
+		return "CrimeLifeGangWarsFOVFix";
+	}
 
-		std::uint8_t* AspectRatioScanResult = Memory::PatternScan(exeModule, "68 ?? ?? ?? ?? EB ?? 68 ?? ?? ?? ?? E8 ?? ?? ?? ?? 8B 0D");
+	const char* FixVersion() const override
+	{
+		return "1.3";
+	}
+
+	const char* TargetName() const override
+	{
+		return "Crime Life: Gang Wars";
+	}
+
+	InitMode GetInitMode() const override
+	{
+		return InitMode::Direct;
+		// return InitMode::WorkerThread;
+		// return InitMode::ExportedOnly;
+	}
+
+	bool IsCompatibleExecutable(const std::string& exeName) const override
+	{
+		return Util::stringcmp_caseless(exeName, "crimelife.exe");
+	}
+
+	void ParseFixConfig(inipp::Ini<char>& ini) override
+	{
+		inipp::get_value(ini.sections["Settings"], "FOVFactor", m_fovFactor);
+		inipp::get_value(ini.sections["Settings"], "RunMultipleInstances", m_runMultipleInstances);
+		inipp::get_value(ini.sections["Settings"], "SkipIntroVideos", m_skipIntroVideos);
+		spdlog_confparse(m_fovFactor);
+		spdlog_confparse(m_runMultipleInstances);
+		spdlog_confparse(m_skipIntroVideos);
+	}
+
+	void ApplyFix() override
+	{
+		auto ResolutionScansResult = Memory::PatternScan(ExeModule(), "8B 86 ?? ?? ?? ?? 9C",
+		"A3 ?? ?? ?? ?? E9 ?? ?? ?? ?? 68 ?? ?? ?? ?? 56 FF D7 83 C4 ?? 85 C0 75 ?? 8D 8C 24 ?? ?? ?? ?? E8 ?? ?? ?? ?? A3 ?? ?? ?? ?? E9 ?? ?? ?? ?? 68 ?? ?? ?? ?? 56 FF D7 83 C4 ?? 85 C0 75 ?? 8D 8C 24 ?? ?? ?? ?? E8 ?? ?? ?? ?? 83 F8");
+		if (Memory::AreAllSignaturesValid(ResolutionScansResult) == true)
+		{
+			spdlog::info("Resolution Instructions 1 Scan: Address is {:s}+{:x}", ExeName().c_str(), ResolutionScansResult[OnResChange] - (std::uint8_t*)ExeModule());
+			spdlog::info("Resolution Width 2 Instruction: Address is {:s}+{:x}", ExeName().c_str(), ResolutionScansResult[ConfigRead] - (std::uint8_t*)ExeModule());
+			spdlog::info("Resolution Height 2 Instruction: Address is {:s}+{:x}", ExeName().c_str(), ResolutionScansResult[ConfigRead] + 37 - (std::uint8_t*)ExeModule());
+
+			m_resolution1Hook = safetyhook::create_mid(ResolutionScansResult[OnResChange], [](SafetyHookContext& ctx)
+			{
+				s_instance_->m_newResX = Memory::ReadMem(ctx.esi + 0x880);
+				s_instance_->m_newResY = Memory::ReadMem(ctx.esi + 0x884);
+				s_instance_->UpdateAR();
+			});
+
+			m_resolutionWidth2Hook = safetyhook::create_mid(ResolutionScansResult[ConfigRead], [](SafetyHookContext& ctx)
+			{
+				s_instance_->m_newResX = Memory::ReadRegister(ctx.eax);
+			});
+
+			m_resolutionHeight2Hook = safetyhook::create_mid(ResolutionScansResult[ConfigRead] + 37, [](SafetyHookContext& ctx)
+			{
+				s_instance_->m_newResY = Memory::ReadRegister(ctx.eax);
+				s_instance_->UpdateAR();
+			});
+		}
+
+		AspectRatioScanResult = Memory::PatternScan(ExeModule(), "68 ?? ?? ?? ?? EB ?? 68 ?? ?? ?? ?? E8 ?? ?? ?? ?? 8B 0D");
 		if (AspectRatioScanResult)
 		{
-			spdlog::info("Aspect Ratio Scan: Address is {:s}+{:x}", sExeName.c_str(), AspectRatioScanResult - (std::uint8_t*)exeModule);
-
-			Memory::Write(AspectRatioScanResult + 1, fNewAspectRatio);
-
-			Memory::Write(AspectRatioScanResult + 8, fNewAspectRatio);
+			spdlog::info("Aspect Ratio Instructions Scan: Address is {:s}+{:x}", ExeName().c_str(), AspectRatioScanResult - (std::uint8_t*)ExeModule());
 		}
 		else
 		{
-			spdlog::error("Failed to locate aspect ratio scan memory address.");
+			spdlog::error("Failed to locate aspect ratio instructions scan memory address.");
 			return;
 		}
 
-		std::uint8_t* CameraFOVInstructionScanResult = Memory::PatternScan(exeModule, "D9 40 ?? D8 0D ?? ?? ?? ?? D9 F2");
-		if (CameraFOVInstructionScanResult)
+		auto CameraFOVScanResult = Memory::PatternScan(ExeModule(), "D8 2D ?? ?? ?? ?? 57 D9 05");
+		if (CameraFOVScanResult)
 		{
-			spdlog::info("Camera FOV Instruction: Address is {:s}+{:x}", sExeName.c_str(), CameraFOVInstructionScanResult - (std::uint8_t*)exeModule);
+			spdlog::info("Camera FOV Instruction: Address is {:s}+{:x}", ExeName().c_str(), CameraFOVScanResult - (std::uint8_t*)ExeModule());
 
-			Memory::WriteNOPs(CameraFOVInstructionScanResult, 3);
+			m_newCameraFOV = m_originalCameraFOV * m_fovFactor;
 
-			CameraFOVInstructionHook = safetyhook::create_mid(CameraFOVInstructionScanResult, [](SafetyHookContext& ctx)
-			{
-				float& fCurrentCameraFOV = Memory::ReadMem(ctx.eax + 0x44);
-					
-				fNewCameraFOV = fCurrentCameraFOV * fFOVFactor;
-
-				FPU::FLD(fNewCameraFOV);
-			});
+			Memory::Write(CameraFOVScanResult + 2, &m_newCameraFOV);
 		}
 		else
 		{
 			spdlog::error("Failed to locate camera FOV instruction memory address.");
 			return;
 		}
-	}
-}
 
-DWORD __stdcall Main(void*)
-{
-	Logging();
-	Configuration();
-	if (DetectGame())
-	{
-		FOVFix();
+		if (m_runMultipleInstances == true)
+		{
+			auto RunMultipleInstancesCheckScanResult = Memory::PatternScan(ExeModule(), "75 ?? 6A ?? 68 ?? ?? ?? ?? E8 ?? ?? ?? ?? 8B 0D ?? ?? ?? ?? 8B 51");
+			if (RunMultipleInstancesCheckScanResult)
+			{
+				spdlog::info("Multiple Instance Check Instruction: Address is {:s}+{:x}", ExeName().c_str(), RunMultipleInstancesCheckScanResult - (std::uint8_t*)ExeModule());
+
+				Memory::PatchBytes(RunMultipleInstancesCheckScanResult, "\xEB");
+			}
+			else
+			{
+				spdlog::error("Failed to locate multiple instance check instruction memory address.");
+				return;
+			}
+		}
+
+		if (m_skipIntroVideos == true)
+		{
+			auto SkipIntroVideoScansResult = Memory::PatternScan(ExeModule(), "A0 ?? ?? ?? ?? 84 C0 74 ?? 6A ?? 6A ?? 68 ?? ?? ?? ?? 6A",
+			"68 ?? ?? ?? ?? 8B F1 C6 44 24");
+			if (Memory::AreAllSignaturesValid(SkipIntroVideoScansResult) == true)
+			{
+				spdlog::info("Startup Intro Video Instruction 1: Address is {:s}+{:x}", ExeName().c_str(), SkipIntroVideoScansResult[SkipVids1] - (std::uint8_t*)ExeModule());
+				spdlog::info("Startup Intro Video Instruction 2: Address is {:s}+{:x}", ExeName().c_str(), SkipIntroVideoScansResult[SkipVids2] - (std::uint8_t*)ExeModule());
+
+				Memory::PatchBytes(SkipIntroVideoScansResult[SkipVids1], "\xE9\x6B\x00\x00\x00");
+				Memory::Write(SkipIntroVideoScansResult[SkipVids2] + 1, m_newFilename);
+			}
+		}
 	}
-	return TRUE;
-}
+
+private:
+	static constexpr float m_oldAspectRatio = 4.0f / 3.0f;
+	static constexpr float m_originalCameraFOV = 0.7853980064f;
+	const char* m_newFilename = "cineintro2.bik";
+
+	SafetyHookMid m_resolution1Hook{};
+	SafetyHookMid m_resolutionWidth2Hook{};
+	SafetyHookMid m_resolutionHeight2Hook{};
+	SafetyHookMid m_cameraFOVHook{};
+
+	std::uint8_t* AspectRatioScanResult;
+
+	bool m_runMultipleInstances = false;
+	bool m_skipIntroVideos = false;
+
+	enum ResolutionsInstructionsIndex
+	{
+		OnResChange,
+		ConfigRead
+	};
+
+	enum SkipIntroVideosInstructionsIndex
+	{
+		SkipVids1,
+		SkipVids2
+	};
+
+	void UpdateAR()
+	{
+		m_newAspectRatio = static_cast<float>(m_newResX) / static_cast<float>(m_newResY);
+		m_aspectRatioScale = m_newAspectRatio / m_oldAspectRatio;
+		WriteStaticARs();
+	}
+
+	void WriteStaticARs()
+	{
+		Memory::Write(AspectRatioScanResult + 1, m_newAspectRatio);
+		Memory::Write(AspectRatioScanResult + 8, m_newAspectRatio);
+	}
+
+	inline static CrimeLifeGangWarsFix* s_instance_ = nullptr;
+};
+
+static std::unique_ptr<CrimeLifeGangWarsFix> g_fix;
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
 	switch (ul_reason_for_call)
 	{
-	case DLL_PROCESS_ATTACH:
-	{
-		thisModule = hModule;
-		HANDLE mainHandle = CreateThread(NULL, 0, Main, 0, NULL, 0);
-		if (mainHandle)
+		case DLL_PROCESS_ATTACH:
 		{
-			SetThreadPriority(mainHandle, THREAD_PRIORITY_HIGHEST);
-			CloseHandle(mainHandle);
+			DisableThreadLibraryCalls(hModule);
+			g_fix = std::make_unique<CrimeLifeGangWarsFix>(hModule);
+			g_fix->Start();
+			break;
 		}
-		break;
+
+		case DLL_PROCESS_DETACH:
+		{
+			g_fix->Shutdown();
+			g_fix.reset();
+			break;
+		}
+
+		case DLL_THREAD_ATTACH:
+		case DLL_THREAD_DETACH:
+		default:
+			break;
 	}
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
-		break;
-	}
+
 	return TRUE;
 }
