162505ebb105e4e4dbab8387ef2b70a58b441a97
AlphaYellow <diogoccsantos@gmail.com>
2026-07-22 17:28:17 +0100

    PowerSpikeProBeachVolleyball: - Added support for running multiple game instances at once and skipping the intro splash screen and video

--- source/fixes/PowerSpikeProBeachVolleyballWidescreenFix/dllmain.cpp
+++ source/fixes/PowerSpikeProBeachVolleyballWidescreenFix/dllmain.cpp
@@ -24,7 +24,7 @@ protected:
 
 	const char* FixVersion() const override
 	{
-		return "1.3";
+		return "1.4";
 	}
 
 	const char* TargetName() const override
@@ -34,8 +34,8 @@ protected:
 
 	InitMode GetInitMode() const override
 	{
-		// return InitMode::Direct;
-		return InitMode::WorkerThread;
+		return InitMode::Direct;
+		// return InitMode::WorkerThread;
 		// return InitMode::ExportedOnly;
 	}
 
@@ -47,7 +47,11 @@ protected:
 	void ParseFixConfig(inipp::Ini<char>& ini) override
 	{
 		inipp::get_value(ini.sections["Settings"], "FOVFactor", m_fovFactor);
+		inipp::get_value(ini.sections["Settings"], "RunMultipleInstances", m_runMultipleInstances);
+		inipp::get_value(ini.sections["Settings"], "SkipIntroVideos", m_skipIntroVideos);
 		spdlog_confparse(m_fovFactor);
+		spdlog_confparse(m_runMultipleInstances);
+		spdlog_confparse(m_skipIntroVideos);
 	}
 
 	void ApplyFix() override
@@ -76,16 +80,16 @@ protected:
 			return;
 		}
 
-		std::uint8_t* AspectRatioInstructionScanResult = Memory::PatternScan(ExeModule(), "DC 0D ?? ?? ?? ?? DB 05");
-		if (AspectRatioInstructionScanResult)
+		auto AspectRatioScanResult = Memory::PatternScan(ExeModule(), "DC 0D ?? ?? ?? ?? DB 05");
+		if (AspectRatioScanResult)
 		{
-			spdlog::info("Aspect Ratio Instruction Scan: Address is {:s}+{:x}", ExeName().c_str(), AspectRatioInstructionScanResult - (std::uint8_t*)ExeModule());
+			spdlog::info("Aspect Ratio Instruction: Address is {:s}+{:x}", ExeName().c_str(), AspectRatioScanResult - (std::uint8_t*)ExeModule());
 
-			AspectRatioAddress = Memory::GetPointerFromAddress(AspectRatioInstructionScanResult + 2, Memory::PointerMode::Absolute);
+			m_aspectRatioAddress = Memory::GetPointerFromAddress(AspectRatioScanResult + 2, Memory::PointerMode::Absolute);
 
-			Memory::WriteNOPs(AspectRatioInstructionScanResult, 6);
+			Memory::WriteNOPs(AspectRatioScanResult, 6);
 
-			m_aspectRatioHook = safetyhook::create_mid(AspectRatioInstructionScanResult, [](SafetyHookContext& ctx)
+			m_aspectRatioHook = safetyhook::create_mid(AspectRatioScanResult, [](SafetyHookContext& ctx)
 			{
 				s_instance_->AspectRatioMidHook(ctx);
 			});
@@ -96,16 +100,16 @@ protected:
 			return;
 		}
 
-		std::uint8_t* CameraFOVInstructionScanResult = Memory::PatternScan(ExeModule(), "D9 04 95 ?? ?? ?? ?? DC 0D");
-		if (CameraFOVInstructionScanResult)
+		auto CameraFOVScanResult = Memory::PatternScan(ExeModule(), "D9 04 95 ?? ?? ?? ?? DC 0D");
+		if (CameraFOVScanResult)
 		{
-			spdlog::info("Camera FOV Instruction: Address is {:s}+{:x}", ExeName().c_str(), CameraFOVInstructionScanResult - (std::uint8_t*)ExeModule());
+			spdlog::info("Camera FOV Instruction: Address is {:s}+{:x}", ExeName().c_str(), CameraFOVScanResult - (std::uint8_t*)ExeModule());
 
-			CameraFOVOffset = Memory::GetPointerFromAddress(CameraFOVInstructionScanResult + 3, Memory::PointerMode::Absolute);
+			m_cameraFOVOffset = Memory::GetPointerFromAddress(CameraFOVScanResult + 3, Memory::PointerMode::Absolute);
 
-			Memory::WriteNOPs(CameraFOVInstructionScanResult, 7);
+			Memory::WriteNOPs(CameraFOVScanResult, 7);
 
-			m_cameraFOVHook = safetyhook::create_mid(CameraFOVInstructionScanResult, [](SafetyHookContext& ctx)
+			m_cameraFOVHook = safetyhook::create_mid(CameraFOVScanResult, [](SafetyHookContext& ctx)
 			{
 				s_instance_->CameraFOVMidHook(ctx);
 			});
@@ -115,33 +119,71 @@ protected:
 			spdlog::info("Cannot locate the camera FOV instruction memory address.");
 			return;
 		}
+
+		if (m_runMultipleInstances == true)
+		{
+			auto RunMultipleInstancesCheckScanResult = Memory::PatternScan(ExeModule(), "74 ?? 33 C0 E9 ?? ?? ?? ?? 68");
+			if (RunMultipleInstancesCheckScanResult)
+			{
+				spdlog::info("Multiple Instance Check Instruction: Address is {:s}+{:x}", ExeName().c_str(), RunMultipleInstancesCheckScanResult - (std::uint8_t*)ExeModule());
+
+				Memory::PatchBytes(RunMultipleInstancesCheckScanResult, "\xEB");
+			}
+			else
+			{
+				spdlog::error("Failed to locate multiple instance check instruction memory address.");
+				return;
+			}
+		}
+
+		if (m_skipIntroVideos == true)
+		{
+			auto SkipIntroVideoScanResult = Memory::PatternScan(ExeModule(), "75 ?? 8B 85 ?? ?? ?? ?? 50 B9", "0F 84 ?? ?? ?? ?? 68 ?? ?? ?? ?? 68 ?? ?? ?? ?? 6A ?? 68");
+			if (Memory::AreAllSignaturesValid(SkipIntroVideoScanResult) == true)
+			{
+				spdlog::info("Intro.avi Startup Video Instruction: Address is {:s}+{:x}", ExeName().c_str(), SkipIntroVideoScanResult[AVIVideo] - (std::uint8_t*)ExeModule());
+				spdlog::info("Splash Screen Startup Intro Video Instruction: Address is {:s}+{:x}", ExeName().c_str(), SkipIntroVideoScanResult[SplashScreen] - (std::uint8_t*)ExeModule());
+
+				Memory::PatchBytes(SkipIntroVideoScanResult[AVIVideo], "\xEB");
+				Memory::PatchBytes(SkipIntroVideoScanResult[SplashScreen], "\xE9\x10\x01\x00\x00\x90");
+			}
+		}
 	}
 
 private:
+	static constexpr float m_oldAspectRatio = 4.0f / 3.0f;
+
 	SafetyHookMid m_resolutionHook{};
 	SafetyHookMid m_aspectRatioHook{};
 	SafetyHookMid m_cameraFOVHook{};
 
-	uintptr_t AspectRatioAddress = 0;
-	uintptr_t CameraFOVOffset = 0;
+	uintptr_t m_aspectRatioAddress = 0;
+	uintptr_t m_cameraFOVOffset = 0;
 
-	static constexpr float m_oldAspectRatio = 4.0f / 3.0f;
+	bool m_runMultipleInstances = false;
+	bool m_skipIntroVideos = false;	
 
-	double m_newAspectRatio2 = 0;
+	double m_newAspectRatio2 = 0.0;
 
 	void AspectRatioMidHook(SafetyHookContext& ctx)
 	{
-		double& dCurrentAspectRatio = Memory::ReadMem(AspectRatioAddress);
+		double& dCurrentAspectRatio = Memory::ReadMem(m_aspectRatioAddress);
 		m_newAspectRatio2 = (dCurrentAspectRatio / (double)m_aspectRatioScale) / (double)m_aspectRatioScale;
 		FPU::FMUL(m_newAspectRatio2);
 	}
 
 	void CameraFOVMidHook(SafetyHookContext& ctx)
 	{
-		float& fCurrentCameraFOV = Memory::ReadMem(ctx.edx * 0x4 + CameraFOVOffset);
+		float& fCurrentCameraFOV = Memory::ReadMem(ctx.edx * 0x4 + m_cameraFOVOffset);
 		m_newCameraFOV = fCurrentCameraFOV * m_fovFactor;
 		FPU::FLD(m_newCameraFOV);
 	}
+
+	enum SkipIntroVideoInstructionsIndex
+	{
+		AVIVideo,
+		SplashScreen
+	};
 
 	inline static PowerSpikeFix* s_instance_ = nullptr;
 };
