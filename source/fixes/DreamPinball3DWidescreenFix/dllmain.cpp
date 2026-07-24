41ad1516e1c9e797beead0790cede10be6c476a0
AlphaYellow <diogoccsantos@gmail.com>
2026-07-24 23:00:24 +0100

    DreamPinball3D: - Improved the fix's structure - Added support for skipping the intro logos

--- source/fixes/DreamPinball3DWidescreenFix/dllmain.cpp
+++ source/fixes/DreamPinball3DWidescreenFix/dllmain.cpp
@@ -1,357 +1,197 @@
-// Include necessary headers
-#include "stdafx.h"
-#include "helper.hpp"
+#include "..\..\common\FixBase.hpp"
 
-#include <spdlog/spdlog.h>
-#include <spdlog/sinks/basic_file_sink.h>
-#include <inipp/inipp.h>
-#include <safetyhook.hpp>
-#include <vector>
-#include <map>
-#include <windows.h>
-#include <psapi.h> // For GetModuleInformation
-#include <fstream>
-#include <filesystem>
-#include <sstream>
-#include <cstring>
-#include <iomanip>
-#include <cstdint>
-#include <iostream>
-#include <locale>
-#include <string>
-
-#define spdlog_confparse(var) spdlog::info("Config Parse: {}: {}", #var, var)
-
-HMODULE exeModule = GetModuleHandle(NULL);
-HMODULE thisModule;
-
-// Fix details
-std::string sFixName = "DreamPinball3DWidescreenFix";
-std::string sFixVersion = "1.1";
-std::filesystem::path sFixPath;
-
-// Ini
-inipp::Ini<char> ini;
-std::string sConfigFile = sFixName + ".ini";
-
-// Logger
-std::shared_ptr<spdlog::logger> logger;
-std::string sLogFile = sFixName + ".log";
-std::filesystem::path sExePath;
-std::string sExeName;
-
-// Constants
-constexpr float fOldAspectRatio = 4.0f / 3.0f;
-
-// Ini variables
-bool bFixActive;
-int iCurrentResX;
-int iCurrentResY;
-float fFOVFactor;
-
-// Variables
-float fNewAspectRatio;
-float fNewCameraFOV;
-uint8_t* CameraFOVAddress;
-
-// Game detection
-enum class Game
+class DreamPinball3DFix final : public FixBase
 {
-	DP3D,
-	Unknown
-};
-
-struct GameInfo
-{
-	std::string GameTitle;
-	std::string ExeName;
-};
-
-const std::map<Game, GameInfo> kGames = {
-	{Game::DP3D, {"Dream Pinball 3D", "dp3d.exe"}},
-};
-
-const GameInfo* game = nullptr;
-Game eGameType = Game::Unknown;
-
-void Logging()
-{
-	// Get path to DLL
-	WCHAR dllPath[_MAX_PATH] = { 0 };
-	GetModuleFileNameW(thisModule, dllPath, MAX_PATH);
-	sFixPath = dllPath;
-	sFixPath = sFixPath.remove_filename();
-
-	// Get game name and exe path
-	WCHAR exePathW[_MAX_PATH] = { 0 };
-	GetModuleFileNameW(exeModule, exePathW, MAX_PATH);
-	sExePath = exePathW;
-	sExeName = sExePath.filename().string();
-	sExePath = sExePath.remove_filename();
-
-	// Spdlog initialization
-	try
+public:
+	explicit DreamPinball3DFix(HMODULE selfModule) : FixBase(selfModule)
 	{
-		logger = spdlog::basic_logger_st(sFixName.c_str(), sExePath.string() + "\\" + sLogFile, true);
-		spdlog::set_default_logger(logger);
-		spdlog::flush_on(spdlog::level::debug);
-		spdlog::set_level(spdlog::level::debug); // Enable debug level logging
-
-		spdlog::info("----------");
-		spdlog::info("{:s} v{:s} loaded.", sFixName.c_str(), sFixVersion.c_str());
-		spdlog::info("----------");
-		spdlog::info("Log file: {}", sExePath.string() + "\\" + sLogFile);
-		spdlog::info("----------");
-		spdlog::info("Module Name: {0:s}", sExeName.c_str());
-		spdlog::info("Module Path: {0:s}", sExePath.string());
-		spdlog::info("Module Address: 0x{0:X}", (uintptr_t)exeModule);
-		spdlog::info("----------");
-		spdlog::info("DLL has been successfully loaded.");
-	}
-	catch (const spdlog::spdlog_ex& ex)
-	{
-		AllocConsole();
-		FILE* dummy;
-		freopen_s(&dummy, "CONOUT$", "w", stdout);
-		std::cout << "Log initialization failed: " << ex.what() << std::endl;
-		FreeLibraryAndExitThread(thisModule, 1);
-	}
-}
-
-void Configuration()
-{
-	// Inipp initialization
-	std::ifstream iniFile(sFixPath.string() + "\\" + sConfigFile);
-	if (!iniFile)
-	{
-		AllocConsole();
-		FILE* dummy;
-		freopen_s(&dummy, "CONOUT$", "w", stdout);
-		std::cout << sFixName.c_str() << " v" << sFixVersion.c_str() << " loaded." << std::endl;
-		std::cout << "ERROR: Could not locate config file." << std::endl;
-		std::cout << "ERROR: Make sure " << sConfigFile.c_str() << " is located in " << sFixPath.string().c_str() << std::endl;
-		spdlog::shutdown();
-		FreeLibraryAndExitThread(thisModule, 1);
-	}
-	else
-	{
-		spdlog::info("Config file: {}", sFixPath.string() + "\\" + sConfigFile);
-		ini.parse(iniFile);
+		s_instance_ = this;
 	}
 
-	// Parse config
-	ini.strip_trailing_comments();
-	spdlog::info("----------");
-
-	// Load settings from ini
-	inipp::get_value(ini.sections["WidescreenFix"], "Enabled", bFixActive);
-	spdlog_confparse(bFixActive);
-
-	// Load resolution from ini
-	inipp::get_value(ini.sections["Settings"], "Width", iCurrentResX);
-	inipp::get_value(ini.sections["Settings"], "Height", iCurrentResY);
-	inipp::get_value(ini.sections["Settings"], "FOVFactor", fFOVFactor);
-	spdlog_confparse(iCurrentResX);
-	spdlog_confparse(iCurrentResY);
-	spdlog_confparse(fFOVFactor);
-
-	// If resolution not specified, use desktop resolution
-	if (iCurrentResX <= 0 || iCurrentResY <= 0)
+	~DreamPinball3DFix() override
 	{
-		spdlog::info("Resolution not specified in ini file. Using desktop resolution.");
-		// Implement Util::GetPhysicalDesktopDimensions() accordingly
-		auto desktopDimensions = Util::GetPhysicalDesktopDimensions();
-		iCurrentResX = desktopDimensions.first;
-		iCurrentResY = desktopDimensions.second;
-		spdlog_confparse(iCurrentResX);
-		spdlog_confparse(iCurrentResY);
-	}
-
-	spdlog::info("----------");
-}
-
-bool DetectGame()
-{
-	for (const auto& [type, info] : kGames)
-	{
-		if (Util::stringcmp_caseless(info.ExeName, sExeName))
+		if (s_instance_ == this)
 		{
-			spdlog::info("Detected game: {:s} ({:s})", info.GameTitle, sExeName);
-			spdlog::info("----------");
-			eGameType = type;
-			game = &info;
-			return true;
+			s_instance_ = nullptr;
 		}
 	}
 
-	spdlog::error("Failed to detect supported game, {:s} isn't supported by the fix.", sExeName);
-	return false;
-}
-
-static SafetyHookMid AspectRatioInstructionHook{};
-static SafetyHookMid CameraFOVInstructionHook{};
-
-void WidescreenFix()
-{
-	if (bFixActive == true && eGameType == Game::DP3D)
+protected:
+	const char* FixName() const override
 	{
-		fNewAspectRatio = static_cast<float>(iCurrentResX) / static_cast<float>(iCurrentResY);
+		return "DreamPinball3DWidescreenFix";
+	}
 
-		std::uint8_t* MainMenuResolution1ScanResult = Memory::PatternScan(exeModule, "68 58 02 00 00 68 20 03 00 00 C7 05 48 7D 4A 00 20 03 00 00 C7 05 4C 7D 4A 00 58 02 00 00 E8 AC 7B FB FF 83 C4 08 C7 05 88 33 2C 01");
-		if (MainMenuResolution1ScanResult)
+	const char* FixVersion() const override
+	{
+		return "1.2";
+	}
+
+	const char* TargetName() const override
+	{
+		return "Dream Pinball 3D";
+	}
+
+	InitMode GetInitMode() const override
+	{
+		// return InitMode::Direct;
+		return InitMode::WorkerThread;
+		// return InitMode::ExportedOnly;
+	}
+
+	bool IsCompatibleExecutable(const std::string& exeName) const override
+	{
+		return Util::stringcmp_caseless(exeName, "dp3d.exe");
+	}
+
+	void ParseFixConfig(inipp::Ini<char>& ini) override
+	{
+		inipp::get_value(ini.sections["Settings"], "Width", m_newResX);
+		inipp::get_value(ini.sections["Settings"], "Height", m_newResY);
+		inipp::get_value(ini.sections["Settings"], "FOVFactor", m_fovFactor);
+		inipp::get_value(ini.sections["Settings"], "SkipIntroLogos", m_skipIntroLogos);
+
+		FallbackToDesktopResolution(m_newResX, m_newResY);
+
+		spdlog_confparse(m_newResX);
+		spdlog_confparse(m_newResY);
+		spdlog_confparse(m_fovFactor);
+		spdlog_confparse(m_skipIntroLogos);
+	}
+
+	void ApplyFix() override
+	{
+		auto ResolutionScansResult = Memory::PatternScan(ExeModule(), "68 ?? ?? ?? ?? 68 ?? ?? ?? ?? C7 05 ?? ?? ?? ?? ?? ?? ?? ?? C7 05 ?? ?? ?? ?? ?? ?? ?? ?? E8",
+		"68 ?? ?? ?? ?? 83 F2", "C7 05 ?? ?? ?? ?? ?? ?? ?? ?? C7 05 ?? ?? ?? ?? ?? ?? ?? ?? 89 15 ?? ?? ?? ?? 89 0D ?? ?? ?? ?? 75");
+		if (Memory::AreAllSignaturesValid(ResolutionScansResult) == true)
 		{
-			spdlog::info("Main Menu Resolution 1 Scan: Address is {:s}+{:x}", sExeName.c_str(), MainMenuResolution1ScanResult - (std::uint8_t*)exeModule);
+			spdlog::info("Main Menu Resolution Instructions 1 Scan: Address is {:s}+{:x}", ExeName().c_str(), ResolutionScansResult[MainMenu1] - (std::uint8_t*)ExeModule());
+			spdlog::info("Main Menu Resolution Instructions 2 Scan: Address is {:s}+{:x}", ExeName().c_str(), ResolutionScansResult[MainMenu2] - (std::uint8_t*)ExeModule());
+			spdlog::info("Resolution List Scan: Address is {:s}+{:x}", ExeName().c_str(), ResolutionScansResult[ResList] - (std::uint8_t*)ExeModule());
 
-			Memory::Write(MainMenuResolution1ScanResult + 6, iCurrentResX);
+			Memory::Write(ResolutionScansResult[MainMenu1] + 6, m_newResX);
+			Memory::Write(ResolutionScansResult[MainMenu1] + 1, m_newResY);
+			Memory::Write(ResolutionScansResult[MainMenu1] + 16, m_newResX);
+			Memory::Write(ResolutionScansResult[MainMenu1] + 26, m_newResY);
 
-			Memory::Write(MainMenuResolution1ScanResult + 1, iCurrentResY);
+			Memory::Write(ResolutionScansResult[MainMenu2] + 12, m_newResX);
+			Memory::Write(ResolutionScansResult[MainMenu2] + 1, m_newResY);
+			Memory::Write(ResolutionScansResult[MainMenu2] + 22, m_newResX);
+			Memory::Write(ResolutionScansResult[MainMenu2] + 17, m_newResY);
+			Memory::Write(ResolutionScansResult[MainMenu2] + 32, m_newResX);
+			Memory::Write(ResolutionScansResult[MainMenu2] + 42, m_newResY);
 
-			Memory::Write(MainMenuResolution1ScanResult + 16, iCurrentResX);
+			Memory::Write(ResolutionScansResult[ResList] + 6, m_newResX);
+			Memory::Write(ResolutionScansResult[ResList] + 16, m_newResY);
+			Memory::Write(ResolutionScansResult[ResList] + 40, m_newResX);
+			Memory::Write(ResolutionScansResult[ResList] + 50, m_newResY);
+			Memory::Write(ResolutionScansResult[ResList] + 78, m_newResX);
+			Memory::Write(ResolutionScansResult[ResList] + 88, m_newResY);
+			Memory::Write(ResolutionScansResult[ResList] + 116, m_newResX);
+			Memory::Write(ResolutionScansResult[ResList] + 126, m_newResY);
+			Memory::Write(ResolutionScansResult[ResList] + 154, m_newResX);
+			Memory::Write(ResolutionScansResult[ResList] + 164, m_newResY);
+			Memory::Write(ResolutionScansResult[ResList] + 192, m_newResX);
+			Memory::Write(ResolutionScansResult[ResList] + 202, m_newResY);
+			Memory::Write(ResolutionScansResult[ResList] + 230, m_newResX);
+			Memory::Write(ResolutionScansResult[ResList] + 240, m_newResY);
+		}
 
-			Memory::Write(MainMenuResolution1ScanResult + 26, iCurrentResY);
+		auto AspectRatioScanResult = Memory::PatternScan(ExeModule(), "74 ?? D9 05 ?? ?? ?? ?? D9 1D");
+		if (AspectRatioScanResult)
+		{
+			spdlog::info("Aspect Ratio Instruction: Address is {:s}+{:x}", ExeName().c_str(), AspectRatioScanResult - (std::uint8_t*)ExeModule());
+
+			Memory::PatchBytes(AspectRatioScanResult, "\xEB");
 		}
 		else
 		{
-			spdlog::error("Failed to locate main menu resolution 1 scan memory address.");
+			spdlog::info("Failed to locate the aspect ratio instruction memory address.");
 			return;
 		}
 
-		std::uint8_t* MainMenuResolution2ScanResult = Memory::PatternScan(exeModule, "68 58 02 00 00 83 F2 01 0F 95 C0 68 20 03 00 00 68 58 02 00 00 68 20 03 00 00 C7 05 48 7D 4A 00 20 03 00 00 C7 05 4C 7D 4A 00 58 02 00 00");
-		if (MainMenuResolution2ScanResult)
+		auto CameraFOVScanResult = Memory::PatternScan(ExeModule(), "D9 05 ?? ?? ?? ?? D9 15 ?? ?? ?? ?? 8B 46");
+		if (CameraFOVScanResult)
 		{
-			spdlog::info("Main Menu Resolution 2 Scan: Address is {:s}+{:x}", sExeName.c_str(), MainMenuResolution2ScanResult - (std::uint8_t*)exeModule);
+			spdlog::info("Camera FOV Instruction: Address is {:s}+{:x}", ExeName().c_str(), CameraFOVScanResult - (std::uint8_t*)ExeModule());
 
-			Memory::Write(MainMenuResolution2ScanResult + 12, iCurrentResX);
+			m_cameraFOVAddress = Memory::GetPointerFromAddress(CameraFOVScanResult + 2, Memory::PointerMode::Absolute);
 
-			Memory::Write(MainMenuResolution2ScanResult + 1, iCurrentResY);
+			Memory::WriteNOPs(CameraFOVScanResult, 6);
 
-			Memory::Write(MainMenuResolution2ScanResult + 22, iCurrentResX);
-
-			Memory::Write(MainMenuResolution2ScanResult + 17, iCurrentResY);
-
-			Memory::Write(MainMenuResolution2ScanResult + 32, iCurrentResX);
-
-			Memory::Write(MainMenuResolution2ScanResult + 42, iCurrentResY);
-		}
-		else
-		{
-			spdlog::error("Failed to locate main menu resolution 2 scan memory address.");
-			return;
-		}
-
-		std::uint8_t* ResolutionListScanResult = Memory::PatternScan(exeModule, "C7 05 2C 24 5C 00 80 02 00 00 C7 05 28 24 5C 00 E0 01 00 00 89 15 20 24 5C 00 89 0D 24 24 5C 00 75 21 C7 05 2C 24 5C 00 80 02 00 00 C7 05 28 24 5C 00 E0 01 00 00 89 15 20 24 5C 00 89 0D 24 24 5C 00 C3 83 F8 01 75 21 C7 05 2C 24 5C 00 20 03 00 00 C7 05 28 24 5C 00 58 02 00 00 89 15 20 24 5C 00 89 0D 24 24 5C 00 C3 83 F8 02 75 21 C7 05 2C 24 5C 00 00 04 00 00 C7 05 28 24 5C 00 00 03 00 00 89 15 20 24 5C 00 89 0D 24 24 5C 00 C3 83 F8 03 75 21 C7 05 2C 24 5C 00 00 05 00 00 C7 05 28 24 5C 00 C0 03 00 00 89 15 20 24 5C 00 89 0D 24 24 5C 00 C3 83 F8 04 75 21 C7 05 2C 24 5C 00 00 05 00 00 C7 05 28 24 5C 00 00 04 00 00 89 15 20 24 5C 00 89 0D 24 24 5C 00 C3 83 F8 05 75 20 C7 05 2C 24 5C 00 40 06 00 00 C7 05 28 24 5C 00 B0 04 00 00");
-		if (ResolutionListScanResult)
-		{
-			spdlog::info("Resolution List: Address is {:s}+{:x}", sExeName.c_str(), ResolutionListScanResult - (std::uint8_t*)exeModule);
-
-			Memory::Write(ResolutionListScanResult + 6, iCurrentResX);
-
-			Memory::Write(ResolutionListScanResult + 16, iCurrentResY);
-
-			Memory::Write(ResolutionListScanResult + 40, iCurrentResX);
-
-			Memory::Write(ResolutionListScanResult + 50, iCurrentResY);
-
-			Memory::Write(ResolutionListScanResult + 78, iCurrentResX);
-
-			Memory::Write(ResolutionListScanResult + 88, iCurrentResY);
-
-			Memory::Write(ResolutionListScanResult + 116, iCurrentResX);
-
-			Memory::Write(ResolutionListScanResult + 126, iCurrentResY);
-
-			Memory::Write(ResolutionListScanResult + 154, iCurrentResX);
-
-			Memory::Write(ResolutionListScanResult + 164, iCurrentResY);
-
-			Memory::Write(ResolutionListScanResult + 192, iCurrentResX);
-
-			Memory::Write(ResolutionListScanResult + 202, iCurrentResY);
-
-			Memory::Write(ResolutionListScanResult + 230, iCurrentResX);
-
-			Memory::Write(ResolutionListScanResult + 240, iCurrentResY);
-		}
-		else
-		{
-			spdlog::error("Failed to locate resolution list scan memory address.");
-			return;
-		}
-
-		std::uint8_t* AspectRatioInstructionScanResult = Memory::PatternScan(exeModule, "D9 05 ?? ?? ?? ?? D9 5C 24 ?? D9 05 ?? ?? ?? ?? D9 1C ?? E8");
-		if (AspectRatioInstructionScanResult)
-		{
-			spdlog::info("Aspect Ratio Instruction: Address is {:s}+{:x}", sExeName.c_str(), AspectRatioInstructionScanResult - (std::uint8_t*)exeModule);
-			
-			Memory::WriteNOPs(AspectRatioInstructionScanResult, 6);
-
-			AspectRatioInstructionHook = safetyhook::create_mid(AspectRatioInstructionScanResult, [](SafetyHookContext& ctx)
+			m_cameraFOVHook = safetyhook::create_mid(CameraFOVScanResult, [](SafetyHookContext& ctx)
 			{
-				FPU::FLD(fNewAspectRatio);
+				float& fCurrentCameraFOV = Memory::ReadMem(s_instance_->m_cameraFOVAddress);
+				s_instance_->m_newCameraFOV = fCurrentCameraFOV * s_instance_->m_fovFactor;
+				FPU::FLD(s_instance_->m_newCameraFOV);
 			});
 		}
 		else
 		{
-			spdlog::info("Cannot locate the aspect ratio instruction memory address.");
+			spdlog::info("Failed to locate the camera FOV instruction memory address.");
 			return;
 		}
 
-		std::uint8_t* CameraFOVInstructionScanResult = Memory::PatternScan(exeModule, "D9 05 ?? ?? ?? ?? D9 15 ?? ?? ?? ?? 8B 46");
-		if (CameraFOVInstructionScanResult)
+		if (m_skipIntroLogos == true)
 		{
-			spdlog::info("Camera FOV Instruction: Address is {:s}+{:x}", sExeName.c_str(), CameraFOVInstructionScanResult - (std::uint8_t*)exeModule);
-
-			CameraFOVAddress = Memory::GetPointerFromAddress(CameraFOVInstructionScanResult + 2, Memory::PointerMode::Absolute);
-
-			Memory::WriteNOPs(CameraFOVInstructionScanResult, 6);
-
-			CameraFOVInstructionHook = safetyhook::create_mid(CameraFOVInstructionScanResult, [](SafetyHookContext& ctx)
+			auto SkipIntroLogosScanResult = Memory::PatternScan(ExeModule(), "68 ?? ?? ?? ?? 8D 44 24 ?? 68 ?? ?? ?? ?? 50 89 2D");
+			if (SkipIntroLogosScanResult)
 			{
-				float& fCurrentCameraFOV = Memory::ReadMem(CameraFOVAddress);
+				spdlog::info("Skip Intro Logos Instruction: Address is {:s}+{:x}", ExeName().c_str(), SkipIntroLogosScanResult - (std::uint8_t*)ExeModule());
 
-				fNewCameraFOV = fCurrentCameraFOV * fFOVFactor;
-
-				FPU::FLD(fNewCameraFOV);
-			});
-		}
-		else
-		{
-			spdlog::info("Cannot locate the camera FOV and aspect ratio scan memory address.");
-			return;
+				Memory::PatchBytes(SkipIntroLogosScanResult, "\xE9\xCE\x02\x00\x00");
+			}
+			else
+			{
+				spdlog::error("Failed to locate skip intro logos instruction memory address.");
+				return;
+			}
 		}
 	}
-}
 
-DWORD __stdcall Main(void*)
-{
-	Logging();
-	Configuration();
-	if (DetectGame())
+private:
+	bool m_skipIntroLogos = false;
+
+	uintptr_t m_cameraFOVAddress = 0;
+
+	SafetyHookMid m_cameraFOVHook{};
+
+	enum ResolutionInstructionsIndices
 	{
-		WidescreenFix();
-	}
-	return TRUE;
-}
+		MainMenu1,
+		MainMenu2,
+		ResList
+	};
+
+	inline static DreamPinball3DFix* s_instance_ = nullptr;
+};
+
+static std::unique_ptr<DreamPinball3DFix> g_fix;
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
 	switch (ul_reason_for_call)
 	{
-	case DLL_PROCESS_ATTACH:
-	{
-		thisModule = hModule;
-		HANDLE mainHandle = CreateThread(NULL, 0, Main, 0, NULL, 0);
-		if (mainHandle)
+		case DLL_PROCESS_ATTACH:
 		{
-			SetThreadPriority(mainHandle, THREAD_PRIORITY_HIGHEST);
-			CloseHandle(mainHandle);
+			DisableThreadLibraryCalls(hModule);
+			g_fix = std::make_unique<DreamPinball3DFix>(hModule);
+			g_fix->Start();
+			break;
 		}
-		break;
+
+		case DLL_PROCESS_DETACH:
+		{
+			g_fix->Shutdown();
+			g_fix.reset();
+			break;
+		}
+
+		case DLL_THREAD_ATTACH:
+		case DLL_THREAD_DETACH:
+		default:
+			break;
 	}
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
-		break;
-	}
+
 	return TRUE;
 }
