011250a27175a44c2adee57d5cf157029328c5f6
AlphaYellow <diogoccsantos@gmail.com>
2026-08-02 04:04:11 +0100

    Frogger2SwampysRevenge: - Fixed the bink videos playing in a black screen at higher resolutions - Added support for skipping the intro videos

--- source/fixes/Frogger2SwampysRevengeWidescreenFix/dllmain.cpp
+++ source/fixes/Frogger2SwampysRevengeWidescreenFix/dllmain.cpp
@@ -24,7 +24,7 @@ protected:
 
 	const char* FixVersion() const override
 	{
-		return "1.3";
+		return "1.4";
 	}
 
 	const char* TargetName() const override
@@ -47,49 +47,107 @@ protected:
 	void ParseFixConfig(inipp::Ini<char>& ini) override
 	{
 		inipp::get_value(ini.sections["Settings"], "FOVFactor", m_fovFactor);
+		inipp::get_value(ini.sections["Settings"], "SkipIntroVideos", m_skipIntroVideos);
 		spdlog_confparse(m_fovFactor);
+		spdlog_confparse(m_skipIntroVideos);
 	}
 
 	void ApplyFix() override
 	{
-		auto ResolutionScansResult = Memory::PatternScan(ExeModule(), "BB ?? ?? ?? ?? 8B 16", "8B 8C 24 ?? ?? ?? ?? 8B 94 24");
-		if (Memory::AreAllSignaturesValid(ResolutionScansResult) == true)
+		m_resolutionScansResult = Memory::PatternScan(ExeModule(), "BB ?? ?? ?? ?? 8B 16", "8B 8C 24 ?? ?? ?? ?? 8B 94 24",
+		"A1 ?? ?? ?? ?? 33 F6 56", "A1 ?? ?? ?? ?? 81 EC ?? ?? ?? ?? 53");
+		if (Memory::AreAllSignaturesValid(m_resolutionScansResult) == true)
 		{
-			spdlog::info("Resolution List Unlock Scan: Address is {:s}+{:x}", ExeName().c_str(), ResolutionScansResult[ResListUnlock] - (std::uint8_t*)ExeModule());
-			spdlog::info("Resolution Instructions Scan: Address is {:s}+{:x}", ExeName().c_str(), ResolutionScansResult[ResWidthHeight] - (std::uint8_t*)ExeModule());
+			spdlog::info("Resolution List Unlock Scan: Address is {:s}+{:x}", ExeName().c_str(), m_resolutionScansResult[ListUnlock] - (std::uint8_t*)ExeModule());
+			spdlog::info("Resolution Instructions Scan: Address is {:s}+{:x}", ExeName().c_str(), m_resolutionScansResult[WidthHeight] - (std::uint8_t*)ExeModule());
+			spdlog::info("Bink Video Rectangle Instruction: Address is {:s}+{:x}", ExeName().c_str(), m_resolutionScansResult[BinkVideoRect] - (std::uint8_t*)ExeModule());
+			spdlog::info("Bink Video Handle Instruction: Address is {:s}+{:x}", ExeName().c_str(), m_resolutionScansResult[BinkVideoHandle] - (std::uint8_t*)ExeModule());
 
-			Memory::PatchBytes(ResolutionScansResult[ResListUnlock] + 1, "\x01");
-			Memory::PatchBytes(ResolutionScansResult[ResListUnlock] + 57, "\x00");
+			Memory::PatchBytes(m_resolutionScansResult[ListUnlock] + 1, "\x01");
+			Memory::PatchBytes(m_resolutionScansResult[ListUnlock] + 57, "\x00");
 			
-			m_resolutionHook = safetyhook::create_mid(ResolutionScansResult[ResWidthHeight], [](SafetyHookContext& ctx)
+			m_resolutionHook = safetyhook::create_mid(m_resolutionScansResult[WidthHeight], [](SafetyHookContext& ctx)
 			{
-				uint32_t& iCurrentWidth = Memory::ReadMem(ctx.esp + 0x88);
-				uint32_t& iCurrentHeight = Memory::ReadMem(ctx.esp + 0x8C);
-				s_instance_->m_newAspectRatio = static_cast<float>(iCurrentWidth) / static_cast<float>(iCurrentHeight);
+				s_instance_->m_newResX = Memory::ReadMem(ctx.esp + 0x88);
+				s_instance_->m_newResY = Memory::ReadMem(ctx.esp + 0x8C);
+				s_instance_->m_newAspectRatio = static_cast<float>(s_instance_->m_newResX) / static_cast<float>(s_instance_->m_newResY);
 				s_instance_->m_aspectRatioScale = s_instance_->m_newAspectRatio / m_oldAspectRatio;
+			});
+
+			m_binkVideoRectHook = safetyhook::create_mid(m_resolutionScansResult[BinkVideoRect], [](SafetyHookContext & ctx)
+			{
+				const uintptr_t binkHandleAddress = Memory::GetPointerFromAddress(s_instance_->m_resolutionScansResult[BinkVideoHandle] + 1, Memory::PointerMode::Absolute);
+				const uintptr_t binkHandle = Memory::ReadMem(binkHandleAddress);
+
+				if (binkHandle == 0 || s_instance_->m_newResX <= 0 || s_instance_->m_newResY <= 0)
+				{
+					return;
+				}
+
+				const std::int32_t screenWidth = s_instance_->m_newResX;
+				const std::int32_t screenHeight = s_instance_->m_newResY;
+
+				const std::uint32_t videoWidth = Memory::ReadMem(binkHandle);
+				const std::uint32_t videoHeight = Memory::ReadMem(binkHandle + 0x4);
+
+				std::int32_t drawWidth = 0;
+				std::int32_t drawHeight = 0;
+
+				if (static_cast<std::int64_t>(screenWidth) * videoHeight <=static_cast<std::int64_t>(screenHeight) * videoWidth)
+				{
+					drawWidth = screenWidth;
+					drawHeight = static_cast<std::int32_t>(static_cast<std::int64_t>(drawWidth) * videoHeight / videoWidth);
+				}
+				else
+				{
+					drawHeight = screenHeight;
+					drawWidth = static_cast<std::int32_t>(static_cast<std::int64_t>(drawHeight) * videoWidth / videoHeight);
+				}
+
+				RECT& destinationRect = Memory::ReadMem(ctx.esp + 0x10);
+				destinationRect.left = (screenWidth - drawWidth) / 2;
+				destinationRect.top = (screenHeight - drawHeight) / 2;
+				destinationRect.right = destinationRect.left + drawWidth;
+				destinationRect.bottom = destinationRect.top + drawHeight;
 			});
 		}
 
-		auto CameraFOVScanResult = Memory::PatternScan(ExeModule(), "C7 05 ?? ?? ?? ?? ?? ?? ?? ?? D9 1D");
-		if (CameraFOVScanResult)
+		m_cameraFOVScanResult = Memory::PatternScan(ExeModule(), "C7 05 ?? ?? ?? ?? ?? ?? ?? ?? D9 1D");
+		if (m_cameraFOVScanResult)
 		{
-			spdlog::info("Camera FOV Instruction: Address is {:s}+{:x}", ExeName().c_str(), CameraFOVScanResult - (std::uint8_t*)ExeModule());
+			spdlog::info("Camera FOV Instruction: Address is {:s}+{:x}", ExeName().c_str(), m_cameraFOVScanResult - (std::uint8_t*)ExeModule());
 
-			CameraFOVAddress = Memory::GetPointerFromAddress(CameraFOVScanResult + 2, Memory::PointerMode::Absolute);
+			m_cameraFOVAddress = Memory::GetPointerFromAddress(m_cameraFOVScanResult + 2, Memory::PointerMode::Absolute);
 
-			Memory::WriteNOPs(CameraFOVScanResult, 10);
+			Memory::WriteNOPs(m_cameraFOVScanResult, 10);
 
-			m_cameraFOVHook = safetyhook::create_mid(CameraFOVScanResult, [](SafetyHookContext& ctx)
+			m_cameraFOVHook = safetyhook::create_mid(m_cameraFOVScanResult, [](SafetyHookContext& ctx)
 			{
 				s_instance_->m_newCameraFOV = (uint32_t)((m_originalCameraFOV / s_instance_->m_aspectRatioScale) / s_instance_->m_fovFactor);
 
-				Memory::Write(s_instance_->CameraFOVAddress, s_instance_->m_newCameraFOV);
+				Memory::Write(s_instance_->m_cameraFOVAddress, s_instance_->m_newCameraFOV);
 			});
 		}
 		else
 		{
-			spdlog::error("Failed to locate resolution instructions scan memory address.");
+			spdlog::error("Failed to locate camera FOV instruction memory address.");
 			return;
+		}
+
+		if (m_skipIntroVideos == true)
+		{
+			m_skipIntroVideosScanResult = Memory::PatternScan(ExeModule(), "6A ?? E8 ?? ?? ?? ?? A1 ?? ?? ?? ?? 83 C4 ?? 85 C0 75 ?? 6A ?? E8 ?? ?? ?? ?? A1");
+			if (m_skipIntroVideosScanResult)
+			{
+				spdlog::info("Skip Intro Videos Instruction: Address is {:s}+{:x}", ExeName().c_str(), m_skipIntroVideosScanResult - (std::uint8_t*)ExeModule());
+
+				Memory::PatchBytes(m_skipIntroVideosScanResult, "\xEB\x2E");
+			}
+			else
+			{
+				spdlog::error("Failed to locate skip intro videos instruction memory address.");
+				return;
+			}
 		}
 	}
 
@@ -97,24 +155,26 @@ private:
 	static constexpr float m_oldAspectRatio = 4.0f / 3.0f;
 	static constexpr uint32_t m_originalCameraFOV = 450;
 
+	bool m_skipIntroVideos = true;
+
+	std::vector<std::uint8_t*> m_resolutionScansResult{};
+	std::uint8_t* m_cameraFOVScanResult{};
+	std::uint8_t* m_skipIntroVideosScanResult{};
+
 	SafetyHookMid m_resolutionHook{};
 	SafetyHookMid m_cameraFOVHook{};
+	SafetyHookMid m_binkVideoRectHook{};
 
-	uintptr_t CameraFOVAddress = 0;
+	uintptr_t m_cameraFOVAddress = 0;
 	uint32_t m_newCameraFOV = 0;
 
 	enum ResolutionListsIndices
 	{
-		ResListUnlock,
-		ResWidthHeight
-	};	
-
-	void CameraFOVMidHook(SafetyHookContext& ctx)
-	{
-		float& fCurrentCameraFOV = Memory::ReadMem(ctx.esp + 0x30);
-		m_newCameraFOV = fCurrentCameraFOV * m_aspectRatioScale * m_fovFactor;
-		ctx.edx = std::bit_cast<uintptr_t>(m_newCameraFOV);
-	}
+		ListUnlock,
+		WidthHeight,
+		BinkVideoRect,
+		BinkVideoHandle
+	};
 
 	inline static Frogger2Fix* s_instance_ = nullptr;
 };
@@ -123,29 +183,27 @@ static std::unique_ptr<Frogger2Fix> g_fix;
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
-	UNREFERENCED_PARAMETER(lpReserved);
-
 	switch (ul_reason_for_call)
 	{
-	case DLL_PROCESS_ATTACH:
-	{
-		DisableThreadLibraryCalls(hModule);
-		g_fix = std::make_unique<Frogger2Fix>(hModule);
-		g_fix->Start();
-		break;
-	}
+		case DLL_PROCESS_ATTACH:
+		{
+			DisableThreadLibraryCalls(hModule);
+			g_fix = std::make_unique<Frogger2Fix>(hModule);
+			g_fix->Start();
+			break;
+		}
 
-	case DLL_PROCESS_DETACH:
-	{
-		g_fix->Shutdown();
-		g_fix.reset();
-		break;
-	}
+		case DLL_PROCESS_DETACH:
+		{
+			g_fix->Shutdown();
+			g_fix.reset();
+			break;
+		}
 
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	default:
-		break;
+		case DLL_THREAD_ATTACH:
+		case DLL_THREAD_DETACH:
+		default:
+			break;
 	}
 
 	return TRUE;
